f88ca8a69e4836b55b2def517ec75c282a6ecd74
Greg Littlefield <greg.littlefield@webfilings.com>
2015-05-06 16:49:32 -0700

    iQIcBAABCAAGBQJYXA0/AAoJEJ5mu3zHWF1PRH4P/16GjZ+ho/Mh3Zxqk52Isea8  /phhxv5mWRAS/QOWaE2KrkZVyPEoGKKsGiuTAtWQGxetCtTJQ2rKPC596lHjVO0x  9KtSq41cHkEaQPx/b+U/uHakso0+60QDQWHZLy1Ou9G8y4Czwr+N2zZS+w2G7k9J  m/9pfk5Q24TGIMh7g5Z+BXJgbyayx8Jm7vb69ggPoY7sWwkg7chZX1qrCRYJ1ia/  r/H68mOSjZq9TAmbL3G967PXUAdUf0lYhVyLf7sMEHxZLeSUA483gHLaek8PBzb3  4ilITaHFETqjj1tR0l+qUN/Sb+koUQc1nSqyeT0PsOUHYtoOUVGt1ObwMsqclI0L  yYYeZM9OcjHuPkJhDmkE6Azx29RHUezJhdTeV/tmT6WYPaGxcxHk5qt2DD3dmmjM  4suMQwVQAcTPLcOgNnE1NDoMNngUfCJxmMrGKAno5e93qcvzE4Auuze93xqmi1fw  jGaGghZIJ0fkC66uAacWtX8Ub5ks1mjtBNpVsZpXXcT+W1eT2EkHPDdeXZj4MyCK  HfxbqxOJzOjwp6ldtz/xZbAABVMc5GB6koVR3NRyl+FJp7FVW75zehSXh6Yg4RqX  +MDSzUsce+h3psKpHeLB37Jwym3PefBKeRmIQkOdevXNEWmgymj3p7yQkhSPWoUT  pC2wyqdEJ8qKUmwcQKeE  =GvVj  -----END PGP SIGNATURE-----
    Typed mixins: work around bug, add convenience methods; dynamic key/ref
    
    Mixing both TypedPropsGetter and TypedStateGetter led to issues as a
    result of this bug in the Dart VM:
    https://code.google.com/p/dart/issues/detail?id=18713
    
    To work around it, the Expando parameters have been removed.
    
    UIP-280

--- lib/src/ui_core/component/typed_mixins.dart
+++ lib/src/ui_core/component/typed_mixins.dart
@@ -3,12 +3,13 @@ part of w_ui_platform.ui_core;
 
 /// Mixin for easy use of generic typed props within a component.
 /// Designed for MapView-like typed props that are backed by Maps.
-abstract class TypedPropsGetter<P> extends Object {
-  Expando<P> _typedPropsCache = new Expando<P>();
+abstract class TypedPropsGetter<TPropsMap> extends Object {
+  // Keep this Expando unparameterized to work around this bug: https://code.google.com/p/dart/issues/detail?id=18713
+  Expando _typedPropsCache = new Expando();
 
   /// Create, or get from cache, a typed props object corresponding to the current props Map.
-  P get tProps {
-    P typedProps = _typedPropsCache[props];
+  TPropsMap get tProps {
+    TPropsMap typedProps = _typedPropsCache[props];
     if (typedProps == null) {
       typedProps = typedPropsFactory(props);
       _typedPropsCache[props] = typedProps;
@@ -22,18 +23,21 @@ abstract class TypedPropsGetter<P> extends Object {
 
   /// Returns a typed props object backed by the specified Map.
   /// Required to properly instantiate the generic class parameter.
-  P typedPropsFactory(Map propsMap);
+  TPropsMap typedPropsFactory(Map propsMap);
+
+  TPropsMap newProps() => typedPropsFactory({});
 }
 
 
 /// Mixin for easy use of generic typed state within a component.
 /// Designed for MapView-like typed state that are backed by Maps.
-abstract class TypedStateGetter<P> extends Object {
-  Expando<P> _typedStateCache = new Expando<P>();
+abstract class TypedStateGetter<TStateMap> extends Object {
+  // Keep this Expando unparameterized to work around this bug: https://code.google.com/p/dart/issues/detail?id=18713
+  Expando _typedStateCache = new Expando();
 
   /// Create, or get from cache, a typed props object corresponding to the current state Map.
-  P get tState {
-    P typedState = _typedStateCache[state];
+  TStateMap get tState {
+    TStateMap typedState = _typedStateCache[state];
     if (typedState == null) {
       typedState = typedStateFactory(state);
       _typedStateCache[state] = typedState;
@@ -47,5 +51,7 @@ abstract class TypedStateGetter<P> extends Object {
 
   /// Returns a typed props object backed by the specified Map.
   /// Required to properly instantiate the generic class parameter.
-  P typedStateFactory(Map propsMap);
+  TStateMap typedStateFactory(Map stateMap);
+
+  TStateMap newState() => typedStateFactory({});
 }
