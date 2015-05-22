c0efff13564b9a9ba0714d06849e482f194fa44f
Greg Littlefield <greg.littlefield@webfilings.com>
2015-05-22 13:15:36 -0700

    Fix namespacing, allow fields in templates, generate prop mixins

---
+++ lib/src/ui_core/component/callback_typedefs.dart
@@ -0,0 +1,11 @@
+part of w_ui_platform.ui_core;
+
+// React Event callbacks
+typedef ClipboardEventCallback(react.SyntheticClipboardEvent event);
+typedef KeyboardEventCallback(react.SyntheticKeyboardEvent event);
+typedef FocusEventCallback(react.SyntheticFocusEvent event);
+typedef FormEventCallback(react.SyntheticFormEvent event);
+typedef MouseEventCallback(react.SyntheticMouseEvent event);
+typedef TouchEventCallback(react.SyntheticTouchEvent event);
+typedef UIEventCallback(react.SyntheticUIEvent event);
+typedef WheelEventCallback(react.SyntheticWheelEvent event);
