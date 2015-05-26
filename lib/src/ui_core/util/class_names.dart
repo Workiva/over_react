e5a62f03e87672d081518b927017fe4a1abf6e93
Greg Littlefield <greg.littlefield@webfilings.com>
2015-05-26 16:40:00 -0700

    iQIcBAABCAAGBQJYXA1EAAoJEJ5mu3zHWF1PgQ0P/jRnqXdZvYde2pdjbnuYILBo  aaY6MhTZ02WtI1NDSUAmoN7wocG7cmf3OrDHK3ICMdvWbbBfMCDRkILdGQPPngpz  jbz1D/tGcBZjs47ZYb6fAV5NAKlxcgk6dEBWCLIPICmArNh1KGaaMsmBeIZ2D2+L  n6kgOXUhyizZIArxgh0Crd2mSAnwmDoJGpBSGmqELDvQak8C9NxZjl+y4ZSb3xRl  ulqx+Qwy5Hi6jSjXn9WVjNlGRfaxVXGl0DdQzp8hwzOtOmdNeTbfjt52/+0AnOHt  vK227uxBPpciMBr4FD8RYKt7JoB1ikonPQBec/TJiSsi9UaB/rNeOGjgwrvtlXFS  hGxnHvSr89eSVgfOubllD8YOZDew7M0EQO8260LPRmVHG8+u5LGMfibFzrTQZLx4  ALgzAOrpyN1pG1IGHtOyRqcKrO4Auzpj2XutWRmCJ6ypRlImZd5v8e3+nmNJ/LlL  Mam/QD2gOhA27IWhp07kIaxmFDPpO2zAsSygMN1mcxu6Q0p8Qw48EFc5Wep+dVB9  QBcIrdyrSJMz58FUmILTElCWxTzedRYyd0U0BHBz0Iq+mqsOZI/GLptGSn/9p9l+  wYNn4WfcCNgj90TTmIp+gpodnuEUkNvsWNspuiAhN8qoTvV7G1GNHWz72O2vlANF  ClfAfdsJ51xnaCiiiq0O  =1qw/  -----END PGP SIGNATURE-----
    Switch over to ClassNameBuilder for faster/simpler className forwarding

--- lib/src/ui_core/util/class_names.dart
+++ lib/src/ui_core/util/class_names.dart
@@ -3,12 +3,14 @@ part of w_ui_platform.ui_core;
 
 /// Converts a map of className->bool entries to a className, similar to React.addons.classSet.
 /// See <http://facebook.github.io/react/docs/class-name-manipulation.html>.
+@deprecated
 String classNameFromMap(Map<String, bool> classMap) {
   return classMap.keys.where((key) => classMap[key]).join(' ');
 }
 
 /// Merges className definitions of different formats together.
 /// Accepts single classes, className strings, and Map<String, bool> class maps.
+@deprecated
 Map<String, bool> mergeClassDefinitions(Iterable<dynamic> classes) {
   Map<String, bool> mergedClassMap = {};
   classes.forEach((dynamic classDefinition) {
@@ -28,6 +30,109 @@ Map<String, bool> mergeClassDefinitions(Iterable<dynamic> classes) {
 /// For the sake of convenience, 1-6 definitions can be specified, as single classes, className strings, and Map<String, bool> class maps.
 /// Similar to <https://github.com/JedWatson/classnames>.
 /// See [classNameFromMap], [mergeClassDefinitions].
+@deprecated
 String classNames(dynamic classDefinition, [dynamic classDefinition2, dynamic classDefinition3, dynamic classDefinition4, dynamic classDefinition5]) {
   return classNameFromMap(mergeClassDefinitions([classDefinition, classDefinition2, classDefinition3, classDefinition4, classDefinition5]));
+}
+
+/// StringBuffer-backed className builder optimized for adding classNames, with support for blacklisting CSS classes.
+class ClassNameBuilder {
+  StringBuffer _classNamesBuffer = new StringBuffer();
+  StringBuffer _blacklistBuffer = new StringBuffer();
+
+  /// Creates a new, empty ClassNameBuilder.
+  ClassNameBuilder();
+
+  /// Creates a new ClassNameBuilder with className and blacklist values added from [CssClassProps.className] and
+  /// [CssClassProps.classNameBlackList], if they are specified.
+  ClassNameBuilder.fromProps(Map props) {
+    this
+      ..add(props[CssClassPropsKey.CLASS_NAME])
+      ..blacklist(props[CssClassPropsKey.CLASS_NAME_BLACKLIST]);
+  }
+
+  /// Add a className string. May be a single CSS class 'token', or multiple space-delimited classes.
+  ///
+  /// There is no checking for duplicate CSS classes.
+  void add(String className, [bool should = true]) {
+    if (!should || className == null) {
+      return;
+    }
+
+    if (_classNamesBuffer.isNotEmpty) {
+      _classNamesBuffer.write(' ');
+    }
+    _classNamesBuffer.write(className);
+  }
+
+  /// Adds all of the CSS classes represented by [className] (a space-delimited list) to the blacklist.
+  ///
+  /// Classes added to the blacklist will not appear in the result of [toClassName].
+  void blacklist(String className, [bool should = true]) {
+    if (!should || className == null) {
+      return;
+    }
+
+    if (_blacklistBuffer == null) {
+      _blacklistBuffer = new StringBuffer();
+    } else {
+      if (_blacklistBuffer.isNotEmpty) {
+        _blacklistBuffer.write(' ');
+      }
+    }
+    _blacklistBuffer.write(className);
+  }
+
+  /// Returns a String representation of the built className, which includes any added classes, and none of the blacklisted classes.
+  ///
+  /// Duplicate classes will be added.
+  String toClassName() {
+    String className = _classNamesBuffer.toString();
+
+    if (_blacklistBuffer != null && _blacklistBuffer.isNotEmpty) {
+      List blacklistedClasses = splitClassName(_blacklistBuffer.toString());
+
+      className = splitClassName(className)
+        .where((String cssClass) => !blacklistedClasses.contains(cssClass))
+        .join(' ');
+    }
+
+    return className;
+  }
+
+  /// Returns a List of CSS class token Strings efficiently split from the specified [className].
+  static List<String> splitClassName(String className) {
+    List<String> classNames = [];
+
+    int start = 0;
+
+    while (start != className.length) {
+      while (className.codeUnitAt(start) == 32) {
+        start++;
+        if (start == className.length) {
+          return classNames;
+        }
+      }
+
+      int end = start;
+      while (className.codeUnitAt(end) != 32) {
+        end++;
+        if (end == className.length) {
+          classNames.add(className.substring(start, end));
+          return classNames;
+        }
+      }
+
+      classNames.add(className.substring(start, end));
+
+      start = end;
+    }
+
+    return classNames;
+  }
+
+  @override
+  String toString() {
+    return '${runtimeType} _classNamesBuffer: ${_classNamesBuffer}, _blacklistBuffer: ${_blacklistBuffer}, toClassName(): ${toClassName()}';
+  }
 }
