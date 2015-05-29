7aa8838bbcf3f5ae84fe2606c8fea058290e1b8f
Greg Littlefield <greg.littlefield@webfilings.com>
2015-05-29 16:41:01 -0700

    Add key/ref tests, add comments, minor local variable renaming
    UIP-369

--- lib/src/ui_core/util/react_wrappers.dart
+++ lib/src/ui_core/util/react_wrappers.dart
@@ -3,44 +3,57 @@ part of w_ui_platform.ui_core;
 JsObject _React = context['React'];
 Map _getInternal(JsObject jsThis) => jsThis[PROPS][INTERNAL];
 
+/// Helper with logic borrowed from react-dart that returns a JsObject version of props,
+/// preprocessed for consumption by React JS and prepared for consumption by the react-dart wrapper internals/
 JsObject _convertDartProps(Map extendedProps) {
-  var convertedArgs = newJsObjectEmpty();
+  var convertedProps = newJsObjectEmpty();
 
+  // Transfer over key and ref if they're specified so React JS knows about them.
   if (extendedProps.containsKey('key')) {
-    convertedArgs['key'] = extendedProps['key'];
+    convertedProps['key'] = extendedProps['key'];
   }
   if (extendedProps.containsKey('ref')) {
-    convertedArgs['ref'] = extendedProps['ref'];
+    convertedProps['ref'] = extendedProps['ref'];
   }
 
-  convertedArgs[INTERNAL] = {PROPS: extendedProps};
+  // Put Dart props inside the internal object, which will be accessed and manipulated by the react-dart wrapper.
+  convertedProps[INTERNAL] = {PROPS: extendedProps};
 
-  return convertedArgs;
+  return convertedProps;
 }
-JsObject cloneElement(JsObject element, [Map config, List children]) {
-  JsObject convertedConfig;
+
+/// Dart wrapper for React.cloneElement.
+///
+/// _From the JS docs:_
+/// > Clone and return a new ReactElement using element as the starting point.
+/// > The resulting element will have the original element's props with the new props merged in shallowly.
+/// > New children will replace existing children.
+/// > Unlike React.addons.cloneWithProps, key and ref from the original element will be preserved.
+/// > There is no special behavior for merging any props (unlike cloneWithProps).
+/// > See the [v0.13 RC2 blog post](https://facebook.github.io/react/blog/2015/03/03/react-v0.13-rc2.html) for additional details.
+JsObject cloneElement(JsObject element, [Map props, List children]) {
+  JsObject convertedProps;
 
   Map internal = _getInternal(element);
   if (internal == null) {
     // Plain JS component
-    convertedConfig = config != null ? newJsMap(config) : null;
+    convertedProps = props != null ? newJsMap(props) : null;
   } else {
     // react-dart component
     Map oldConfig = internal[PROPS];
 
     Map extendedProps = new Map.from(oldConfig);
-    if (config != null) {
-      extendedProps.addAll(config);
+    if (props != null) {
+      extendedProps.addAll(props);
     }
     if (children != null) {
       extendedProps['children'] = children;
     }
 
-    convertedConfig = _convertDartProps(extendedProps);
+    convertedProps = _convertDartProps(extendedProps);
   }
 
-
-  List jsMethodArgs = [element, convertedConfig];
+  List jsMethodArgs = [element, convertedProps];
   if (children != null) {
     jsMethodArgs.add(new JsArray.from(children));
   }
