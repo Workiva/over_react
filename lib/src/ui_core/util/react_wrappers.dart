60fab3850816ff92efa1202ecb7b364b142d1ae1
Jace Hensley <jace.hensley@workiva.com>
2015-06-18 15:02:30 -0700

    Address CR comments

--- lib/src/ui_core/util/react_wrappers.dart
+++ lib/src/ui_core/util/react_wrappers.dart
@@ -61,6 +61,11 @@ JsObject cloneElement(JsObject element, [Map props, List children]) {
   return _React.callMethod('cloneElement', jsMethodArgs);
 }
 
+/// Dart wrapper for React.isValidElement.
+///
+/// _From the JS docs:_
+/// > Verifies the object is a ReactElement
+
 bool isValidElement(dynamic object) {
   return _React.callMethod('isValidElement', [object]);
 }
