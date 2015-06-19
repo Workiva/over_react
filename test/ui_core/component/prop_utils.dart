96fc1c360ffc4e118253875874c5935400a9a8db
Jace Hensley <jace.hensley@workiva.com>
2015-06-19 08:09:55 -0700

    Update prop mixing tests to generate their own list of keys
    UIP-383

--- test/ui_core/component/prop_utils.dart
+++ test/ui_core/component/prop_utils.dart
@@ -1,5 +1,7 @@
 import 'dart:mirrors';
 import 'package:test/test.dart';
+import 'package:w_ui_platform/ui_core.dart';
+
 
 
 void testProp (Symbol name, dynamic expectedKey, instance, testValue) {
@@ -10,7 +12,37 @@ void testProp (Symbol name, dynamic expectedKey, instance, testValue) {
   expect(mirror.getField(name).reflectee, equals(testValue));
 }
 
-void testKeys (List keys, dynamic instanceBuilder()) {
+//void testKeys (testClass, instance) {
+//  ClassMirror classMirror = reflectClass(testClass);
+//
+//  for (var element in classMirror.declarations.values) {
+//    test('', (){
+//      var name = MirrorSystem.getName(element.simpleName);
+//      if (!name.contains('=')) {
+//        testProp(element.simpleName, name, testClass, null);
+//      }
+//    });
+//
+//  }
+//}
+
+List getKeys (testClass) {
+  List keys = [];
+
+  ClassMirror classMirror = reflectClass(testClass);
+  for (var element in classMirror.declarations.values) {
+    var name = MirrorSystem.getName(element.simpleName);
+    if (!name.contains('=') && !name.toLowerCase().contains('props')) {
+      keys.add(name);
+    }
+  }
+
+  return keys;
+}
+
+void testKeys (testClass, dynamic instanceBuilder()) {
+  List keys = getKeys(testClass);
+
   test('set up', () {
     var instance = instanceBuilder();
     expect(() {instance['notThere'];}, throws);
@@ -21,4 +53,5 @@ void testKeys (List keys, dynamic instanceBuilder()) {
       testProp(new Symbol(propKey), propKey, instance, null);
     });
   }
+  keys = [];
 }
