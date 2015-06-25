6316fbba057325b8ef7d6e27b01bfe4c651a1712
Jace Hensley <jace.hensley@workiva.com>
2015-06-25 10:19:10 -0700

    Add doc comments remove imports
    UIP-422

--- lib/ui_core.dart
+++ lib/ui_core.dart
@@ -1,5 +1,5 @@
+/// Base classes for UI components and related utilities.
 library w_ui_platform.public.ui_core;
 
 // Importing and exporting this library so we can hide the generated files
-import  'src/ui_core.dart';
 export  'src/ui_core.dart';
