20e6a7378a431a4cc46eabf008b42d6a1bd942ca
Greg Littlefield <greg.littlefield@webfilings.com>
2015-07-09 16:11:10 -0700

    UIP-281: Update CssValue comments

--- lib/src/ui_core/util/css_value_util.dart
+++ lib/src/ui_core/util/css_value_util.dart
@@ -1,30 +1,46 @@
 part of w_ui_platform.ui_core;
 
-/// Class for handling CSS values
-class CssValue implements Comparable {
+/// A CSS length value, with a number and unit component, for use in CSS properties such as `width`, `top`, `padding`, etc.
+class CssValue implements Comparable<CssValue> {
+  /// The number component of this CSS value.
+  ///
+  /// E.g., 1 for '1px'
   final num number;
+
+  /// The unit component of this CSS value.
+  ///
+  /// E.g., 'px' for '1px'
   final String unit;
 
-  /// Constructor
+  /// Creates a new [CssValue]. If no [unit] is specified, `'px'` is used instead.
   const CssValue(this.number, [this.unit = 'px']);
 
-  /// Parse a value into a CssValue.
-  /// If the value provided is not a valid [CssValue] the 'onError' function will
-  /// be called.
-  /// If no 'onError' function was specified then null will be returned.
-  factory CssValue.parse(dynamic value, {CssValue onError(value, error)}) {
+  /// Parse [source] and return its [CssValue] representation.
+  ///
+  /// Accepts a number optionally followed by a CSS length unit. If no unit is present, `'px'` is used as the unit instead.
+  ///
+  /// If `source` is not a valid CSS value, the [onError] callback is called with [source] and an error object, and its return value is used instead. If no `onError` is provided, `null` is returned.
+  ///
+  /// Examples of accepted values:
+  ///
+  ///     '2px'
+  ///     '10'
+  ///     20
+  ///     '1.25em'
+  ///     '-15%'
+  factory CssValue.parse(dynamic source, {CssValue onError(value, error)}) {
     num number;
     String unit;
 
     var error = null;
 
-    if (value == null) {
+    if (source == null) {
       error = new ArgumentError.notNull('value');
-    } else if (value is num) {
-      number = value;
+    } else if (source is num) {
+      number = source;
       unit = 'px';
     } else {
-      var unitMatch = new RegExp(r'(?:rem|em|ex|vh|vw|vmin|vmax|%|px|cm|mm|in|pt|pc|ch)?$').firstMatch(value.toString());
+      var unitMatch = new RegExp(r'(?:rem|em|ex|vh|vw|vmin|vmax|%|px|cm|mm|in|pt|pc|ch)?$').firstMatch(source.toString());
       try {
         number = double.parse(unitMatch.input.substring(0, unitMatch.start));
         unit = unitMatch.group(0);
@@ -32,13 +48,13 @@ class CssValue implements Comparable {
           unit = 'px';
         }
       } catch(e) {
-        error = new ArgumentError.value(value, 'value', 'Invalid number/unit for CSS value');
+        error = new ArgumentError.value(source, 'value', 'Invalid number/unit for CSS value');
       }
     }
 
     if (number != null && !number.isFinite) {
       // Rule out -Infinity, Infinity, and NaN.
-      error = new ArgumentError.value(number, 'value', 'Number portion of CSS value ($value) must be finite');
+      error = new ArgumentError.value(number, 'value', 'Number portion of CSS value ($source) must be finite');
     }
 
     var result;
@@ -46,7 +62,7 @@ class CssValue implements Comparable {
       if (onError == null) {
         result = null;
       } else {
-        result = onError(value, error);
+        result = onError(source, error);
       }
     } else {
       result = new CssValue(number, unit);
@@ -55,80 +71,94 @@ class CssValue implements Comparable {
     return result;
   }
 
-  /// Internal function to check that units match
+  /// Throws an error if this value's [unit] does not match that of [other].
+  ///
+  /// Used internally to prevent calculations between incompatible units.
   void _checkMatchingUnits(CssValue other) {
     if (unit != other.unit) {
       throw new ArgumentError('Cannot compare CSS unit values of units $unit and ${other.unit}');
     }
   }
 
-  /// Get the remainder [CssValue] divided by a number.
+  /// Returns the remainder of dividing this value's [number] by [other].
   CssValue operator %(num other) => new CssValue(number % other, unit);
 
-  /// Multiply the [CssValue] by a number.
+  /// Returns the result of multiplying this value's [number] by [other].
   CssValue operator *(num other) => new CssValue(number * other, unit);
 
-  /// Divide the [CssValue] by a number.
+  /// Returns the result of dividing this value's [number] by [other].
   CssValue operator /(num other) => new CssValue(number / other, unit);
 
-  /// Add two [CssValue] objects.
+  /// Returns a new [CssValue] with the sum of the [number]s of this value and [other].
+  ///
+  /// Throws an error if the [unit] of this value and [other] do not match.
   CssValue operator +(CssValue other) {
     _checkMatchingUnits(other);
     return new CssValue(number + other.number, unit);
   }
 
-  /// Subtract two [CssValue] objects.
-  /// If the units of the two [CssValue] object doesn't match an ArgumentError will be thrown.
+  /// Returns a new [CssValue] with the difference between the [number]s of this value and [other].
+  ///
+  /// Throws an error if the [unit] of this value and [other] do not match.
   CssValue operator -(CssValue other) {
     _checkMatchingUnits(other);
     return new CssValue(number - other.number, unit);
   }
 
-  /// Check if this [CssValue] is less than the provided [CssValue].
-  /// If the units of the two [CssValue] object doesn't match an ArgumentError will be thrown.
+  /// Returns whether this value's [number] is less than that of [other].
+  ///
+  /// Throws an error if the [unit] of this value and [other] do not match.
   bool operator <(CssValue other) {
     _checkMatchingUnits(other);
     return number < other.number;
   }
 
-  /// Check if this [CssValue] is less than or equal to the provided [CssValue].
-  /// If the units of the two [CssValue] object doesn't match an ArgumentError will be thrown.
+  /// Returns whether this value's [number] is less than or equal to that of [other].
+  ///
+  /// Throws an error if the [unit] of this value and [other] do not match.
   bool operator <=(CssValue other) {
     _checkMatchingUnits(other);
     return number <= other.number;
   }
 
-  /// Check if this [CssValue] is equal to the provided [CssValue].
-  /// Compares both the value and the units.
+  /// Returns whether this value's [number] and [unit] are equal to that of [other].
   bool operator ==(dynamic other) {
     return identical(this, other) || (other is CssValue && number == other.number && unit == other.unit);
   }
 
-  /// Check if this [CssValue] is less than or equal to the provided [CssValue].
-  /// If the units of the two [CssValue] object doesn't match an ArgumentError will be thrown.
+  /// Returns whether this value's [number] is greater than that of [other].
+  ///
+  /// Throws an error if the [unit] of this value and [other] do not match.
   bool operator >(CssValue other) {
     _checkMatchingUnits(other);
     return number > other.number;
   }
 
-  /// Check if this [CssValue] is less than or equal to the provided [CssValue].
-  /// If the units of the two [CssValue] object doesn't match an ArgumentError will be thrown.
+  /// Returns whether this value's [number] is greater than or equal to that of [other].
+  ///
+  /// Throws an error if the [unit] of this value and [other] do not match.
   bool operator >=(CssValue other) {
     _checkMatchingUnits(other);
     return number >= other.number;
   }
 
-  /// Negate this [CssValue].
+  /// Returns a new [CssValue] with this value's [unit] and the negation of this value's [number].
   CssValue operator -() {
     return new CssValue(-number, unit);
   }
 
+  /// Returns the result of comparing this value's [number] to that of [other].
+  ///
+  /// Throws an error if the [unit] of this value and [other] do not match.
   @override
-  int compareTo(other) {
+  int compareTo(CssValue other) {
     _checkMatchingUnits(other);
-    return number.compareTo(other);
+    return number.compareTo(other.number);
   }
 
+  /// Returns the String representation of this value, which can be used as CSS style values.
+  ///
+  /// If [number] is 0, then the [unit] is omitted.
   @override
   String toString() => number == 0 ? '0' : '$number${unit}';
 }
