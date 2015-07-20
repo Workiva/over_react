1e66ad90114f3adbfc3cae1080e491c4ecc7049e
Jace Hensley <jace.hensley@workiva.com>
2015-07-20 11:53:45 -0700

    Initial CR comments addressed.
    UIP-451

--- lib/src/ui_core/component/block_mixin.dart
+++ lib/src/ui_core/component/block_mixin.dart
@@ -1,6 +1,6 @@
 part of w_ui_platform.ui_core;
 
-/// Mixin for [GridBlock] props
+/// Mixin for [Block] props
 /// To use:
 /// - add [BlockProps] mixin class to component definition.
 /// - add [BlockProps] mixin class to component class.
@@ -20,93 +20,92 @@ abstract class _$template_BlockProps {
   static const String _BLOCK_MD = '-md';
   static const String _BLOCK_LG = '-lg';
 
-  /// The size of the [GridBlock] at any screen size.
+  /// The size of the [Block] at any screen size.
   @Prop("'$_SIZE'") int get size;
 
-  /// The size of the [GridBlock] above the 'sm' responsive breakpoint.
+  /// The size of the [Block] above the 'sm' responsive breakpoint.
   @Prop("'$_SIZE$_BLOCK_SM'") int get smSize;
 
-  /// The size of the [GridBlock] above the 'md' responsive breakpoint.
+  /// The size of the [Block] above the 'md' responsive breakpoint.
   @Prop("'$_SIZE$_BLOCK_MD'") int get mdSize;
 
-  /// The size of the [GridBlock] above the 'lg' responsive breakpoint.
+  /// The size of the [Block] above the 'lg' responsive breakpoint.
   @Prop("'$_SIZE$_BLOCK_LG'") int get lgSize;
 
-  /// The order that the [GridBlock] should display in at any screen size.
+  /// The order that the [Block] should display in at any screen size.
   @Prop("'$_ORDER'") int get order;
 
-  /// The order that the [GridBlock] should display in above the 'sm' responsive breakpoint.
+  /// The order that the [Block] should display in above the 'sm' responsive breakpoint.
   @Prop("'$_ORDER$_BLOCK_SM'") int get smOrder;
 
-  /// The order that the [GridBlock] should display in above the 'md' responsive breakpoint.
+  /// The order that the [Block] should display in above the 'md' responsive breakpoint.
   @Prop("'$_ORDER$_BLOCK_MD'") int get mdOrder;
 
-  /// The order that the [GridBlock] should display in above the 'lg' responsive breakpoint.
+  /// The order that the [Block] should display in above the 'lg' responsive breakpoint.
   @Prop("'$_ORDER$_BLOCK_LG'") int get lgOrder;
 
-  /// The offset of the [GridBlock] at any screen size.
-  /// Move a [GridBlock] to the right using the Offset prop.
-  /// This prop increases the left margin of a [GridBlock] by n blocks. For example, Offset=4 moves it over four blocks.
+  /// The offset of the [Block] at any screen size.
+  /// Move a [Block] to the right using the Offset prop.
+  /// This prop increases the left margin of a [Block] by n blocks. For example, Offset=4 moves it over four blocks.
   /// See: https://api.atl.workiva.net/WebSkinReact/docs/build/html/components.html#grid-v2-options-offsets
   @Prop("'$_OFFSET'") int get offset;
 
-  /// The offset of the [GridBlock] above the 'sm' responsive breakpoint.
-  /// Move a [GridBlock] to the right using the Offset prop.
-  /// This prop increases the left margin of a small [GridBlock] by n blocks. For example, smOffset=4 moves it over four blocks.
+  /// The offset of the [Block] above the 'sm' responsive breakpoint.
+  /// Move a [Block] to the right using the Offset prop.
+  /// This prop increases the left margin of a small [Block] by n blocks. For example, smOffset=4 moves it over four blocks.
   /// See: https://api.atl.workiva.net/WebSkinReact/docs/build/html/components.html#grid-v2-options-offsets
   @Prop("'$_OFFSET$_BLOCK_SM'") int get smOffset;
 
-  /// The offset of the [GridBlock] above the 'md' responsive breakpoint.
-  /// Move a [GridBlock] to the right using the Offset prop.
-  /// This prop increases the left margin of a medium [GridBlock] by n blocks. For example, mdOffset=4 moves it over four blocks.
+  /// The offset of the [Block] above the 'md' responsive breakpoint.
+  /// Move a [Block] to the right using the Offset prop.
+  /// This prop increases the left margin of a medium [Block] by n blocks. For example, mdOffset=4 moves it over four blocks.
   /// See: https://api.atl.workiva.net/WebSkinReact/docs/build/html/components.html#grid-v2-options-offsets
   @Prop("'$_OFFSET$_BLOCK_MD'") int get mdOffset;
 
-  /// The offset of the [GridBlock] above the 'lg' responsive breakpoint.
-  /// Move a [GridBlock] to the right using the Offset prop.
-  /// This prop increases the left margin of a large [GridBlock] by n blocks. For example, lgOffset=4 moves it over four blocks.
+  /// The offset of the [Block] above the 'lg' responsive breakpoint.
+  /// Move a [Block] to the right using the Offset prop.
+  /// This prop increases the left margin of a large [Block] by n blocks. For example, lgOffset=4 moves it over four blocks.
   /// See: https://api.atl.workiva.net/WebSkinReact/docs/build/html/components.html#grid-v2-options-offsets
   @Prop("'$_OFFSET$_BLOCK_LG'") int get lgOffset;
 
-  /// Whether this [GridBlock] is a content [GridBlock] at any screen size
+  /// Whether this [Block] is a content [Block] at any screen size
   /// See: https://api.atl.workiva.net/WebSkinReact/docs/build/html/components.html#grid-v2-block-content
   @Prop("'$_CONTENT'") bool get content;
 
-  /// Whether this [GridBlock] is a content [GridBlock] above the 'sm' responsive breakpoint.
+  /// Whether this [Block] is a content [Block] above the 'sm' responsive breakpoint.
   /// See: https://api.atl.workiva.net/WebSkinReact/docs/build/html/components.html#grid-v2-block-content
   @Prop("'$_CONTENT$_BLOCK_SM'") bool get smContent;
 
-  /// Whether this [GridBlock] is a content [GridBlock] above the 'md' responsive breakpoint.
+  /// Whether this [Block] is a content [Block] above the 'md' responsive breakpoint.
   /// See: https://api.atl.workiva.net/WebSkinReact/docs/build/html/components.html#grid-v2-block-content
   @Prop("'$_CONTENT$_BLOCK_MD'") bool get mdContent;
 
-  /// Whether this [GridBlock] is a content [GridBlock] above the 'lg' responsive breakpoint.
+  /// Whether this [Block] is a content [Block] above the 'lg' responsive breakpoint.
   /// See: https://api.atl.workiva.net/WebSkinReact/docs/build/html/components.html#grid-v2-block-content
   @Prop("'$_CONTENT$_BLOCK_LG'") bool get lgContent;
 
-  /// Whether this [GridBlock] should shrink to the size of its content at any screen size.
+  /// Whether this [Block] should shrink to the size of its content at any screen size.
   @Prop("'$_SHRINK'") bool get shrink;
 
-  /// Whether this [GridBlock] should shrink to the size of its content above the 'sm' responsive breakpoint.
+  /// Whether this [Block] should shrink to the size of its content above the 'sm' responsive breakpoint.
   @Prop("'$_SHRINK$_BLOCK_SM'") bool get smShrink;
 
-  /// Whether this [GridBlock] should shrink to the size of its content above the 'md' responsive breakpoint.
+  /// Whether this [Block] should shrink to the size of its content above the 'md' responsive breakpoint.
   @Prop("'$_SHRINK$_BLOCK_MD'") bool get mdShrink;
 
-  /// Whether this [GridBlock] should shrink to the size of its content above the 'lg' responsive breakpoint.
+  /// Whether this [Block] should shrink to the size of its content above the 'lg' responsive breakpoint.
   @Prop("'$_SHRINK$_BLOCK_LG'") bool get lgShrink;
 
-  /// Whether to collapse some or all of the sides of the [GridBlock].
+  /// Determines what sides of the [Block] to collapse.
   /// Valid values are:
-  ///    1. false - same as BlockCollapse.NONE (default).
-  ///    2. BlockCollapse.ALL, BlockCollapse.HORIZONTAL or BlockCollapse.VERTICAL.
-  ///    3. BlockCollapse.LEFT, BlockCollapse.RIGHT, BlockCollapse.TOP, BlockCollapse.BOTTOM.
-  ///    4. List with any combination of the four [BlockCollapse] sides.
+  /// > 1. Any option from BlockCollapse
+  /// > 2. Any combination of options from BlockCollapse
+  /// >> - To combine BlockCollapse options or them together, bit-wise.
   ///
   /// Default: BlockCollapse.NONE
-  dynamic get collapse;
+  int get collapse;
 
-  /// Whether to add a gutter around the [GridBlock]
+  /// Whether to add a gutter around the [Block]
   /// Valid values are:
   ///    1. BlockGutter.NONE (default).
   ///    2. BlockGutter.ALL.
@@ -115,14 +114,14 @@ abstract class _$template_BlockProps {
   /// Default: BlockCollapse.NONE
   BlockGutter get gutter;
 
-  /// Whether the [GridBlock] should enable scrolling.
+  /// Whether the [Block] should enable scrolling.
   ///
   /// Default: true
   bool get scroll;
 }
 
-abstract class GridBlockMixin<P extends BlockProps> {
-  /// Standard HitAreaProps defaults.
+abstract class BlockMixin<P extends BlockProps> {
+  /// Standard BlockMixin defaults.
   static const Map defaultProps = const {
     BlockProps.Z_$KEY__COLLAPSE: BlockCollapse.NONE,
     BlockProps.Z_$KEY__CONTENT: false,
@@ -157,13 +156,16 @@ abstract class GridBlockMixin<P extends BlockProps> {
     // - For bool props add the key directly.
     for (String classPrefix in keys) {
       var value = props[classPrefix];
+
       if (value != null) {
         var className;
+
         if (value is int && value >= 0) {
           className = (classPrefix + '-' + value.toString());
-        } else if (value is bool && value == true) {
+        } else if (value) {
           className = classPrefix;
         }
+
         if (className != null) {
           blockClasses.add(className);
         }
@@ -171,7 +173,7 @@ abstract class GridBlockMixin<P extends BlockProps> {
     }
 
     blockClasses.add(_getCollapseClasses());
-    blockClasses.add(_getGutterClasses());
+    blockClasses.add(tProps.gutter.className);
 
     if (!tProps.scroll) {
       blockClasses.add('no-scroll');
@@ -180,67 +182,67 @@ abstract class GridBlockMixin<P extends BlockProps> {
     return blockClasses.toClassName();
   }
 
-  /// Get the classNames for the collapsing the padding of the [GridBlock].
+  /// Get the classNames for the collapsing the padding of the [Block].
   String _getCollapseClasses() {
-    if (tProps.collapse == false || tProps.collapse == BlockCollapse.NONE) {
+    if (tProps.collapse == BlockCollapse.NONE) {
       return null;
     }
 
     var collapseClasses = new ClassNameBuilder();
 
     // Determine the type for the collapse prop
-    if (tProps.collapse is List) {
+    if ((tProps.collapse & BlockCollapse.ALL) != 0) {
+      collapseClasses.add('grid-collapse');
+    } else {
+      if ((tProps.collapse & BlockCollapse.VERTICAL) != 0) {
+        collapseClasses
+          ..add('grid-collapse-vertical');
+      } else {
+        if ((tProps.collapse & BlockCollapse.RIGHT) != 0) {
+          collapseClasses.add('grid-collapse-right');
+        }
 
-      // Collapse was provided a list so run through the list and
-      // ensure the values are valid BlockCollapse values
-      for (var item in tProps.collapse) {
-        if (item is! BlockCollapse
-            || item == BlockCollapse.NONE
-            || item == BlockCollapse.ALL
-            || item == BlockCollapse.HORIZONTAL
-            || item == BlockCollapse.VERTICAL) {
-          assert(ValidationUtil.warn('Invalid collapse option given to Block collapse List.'));
-        } else {
-          collapseClasses.add((item as BlockCollapse).className);
+        if ((tProps.collapse & BlockCollapse.LEFT) != 0) {
+          collapseClasses.add('grid-collapse-left');
         }
       }
-    } else if (tProps.collapse is BlockCollapse) {
-      // collapse was provided a BlockCollapse object so use that directly
-      collapseClasses.add((tProps.collapse as BlockCollapse).className);
-    } else {
-      // Anything other type of value is unsupported
-      assert(ValidationUtil.warn('Invalid value provided for collapse prop.'));
+
+      if ((tProps.collapse & BlockCollapse.HORIZONTAL) != 0) {
+        collapseClasses
+          ..add('grid-collapse-horizontal');
+      } else {
+        if ((tProps.collapse & BlockCollapse.TOP) != 0) {
+          collapseClasses.add('grid-collapse-top');
+        }
+
+        if ((tProps.collapse & BlockCollapse.BOTTOM) != 0) {
+          collapseClasses.add('grid-collapse-bottom');
+        }
+      }
     }
+
     return collapseClasses.toClassName();
   }
-
-  /// Get the classNames for the gutters of the [GridBlock]
-  String _getGutterClasses() {
-    if (tProps.gutter == BlockGutter.NONE) {
-      return null;
-    }
-
-    return tProps.gutter.className;
-  }
-
 }
 
-/// The collapse options for the [GridBlock]
-@GenerateConstants(#BlockCollapse, #className)
-class _$template_GridBlockCollapse {
-  static const NONE = null;
-  static const ALL = 'grid-collapse';
-  static const HORIZONTAL = 'grid-collapse-horizontal';
-  static const VERTICAL = 'grid-collapse-vertical';
-  static const LEFT = 'grid-collapse-left';
-  static const RIGHT = 'grid-collapse-right';
-  static const TOP = 'grid-collapse-top';
-  static const BOTTOM = 'grid-collapse-bottom';
+///// The collapse options for the [Block]
+abstract class BlockCollapse {
+  static const int NONE = 0;
+
+  static const int TOP    = 1 << 0;
+  static const int LEFT   = 1 << 1;
+  static const int BOTTOM = 1 << 2;
+  static const int RIGHT  = 1 << 3;
+
+  static const int HORIZONTAL = (TOP | BOTTOM);
+  static const int VERTICAL   = (LEFT | RIGHT);
+
+  static const int ALL = (HORIZONTAL | VERTICAL);
 }
 
-/// The gutter options for the [GridBlock]
+/// The gutter options for the [Block]
 @GenerateConstants(#BlockGutter, #className)
-class _$template_GridBlockGutter {
+class _$template_BlockGutter {
   static const NONE = null;
   static const ALL = 'grid-gutter';
   static const HORIZONTAL = 'grid-gutter-horizontal';
