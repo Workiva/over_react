131c2e918e69ecd546e97c0ed2d0acb19b0e881c
Claire Sarsam <claire.sarsam@workiva.com>
2015-08-12 15:31:18 -0700

    iQIcBAABCAAGBQJYXA2sAAoJEJ5mu3zHWF1PL80QAIBfdroPvl01E7pY95uW1Fe+  /GR3xNLHJ4lozmboBqfZ139v0Vav2xRVK6Js7vN+q0zxcx27l49VHdIpoxKn5YBs  Y73fYyeOwBcnOKvMFmGxXTWV/yJ/KK1cihAZSrwoDfAXvRuIcFZmfrK7u+uvqGIK  5j9n+TncE5GBtrFfQ22AkVmNNP02sypwwdhyPY8vp/cR/BxyVvlaL5lQpG914ZyY  dz9/yfvLN5T08tO0MfFr7HjRv7dRKHhmdUZml7gFKAGkZkRNLKxWa7SkFuGZkeyZ  3dLmu7nzZGkys+BDfoydMqCKG3NoXRpWO43W+DNRgbNzI4bOQmJSzsM5WB+1d8Jn  YHYBfMD/hkyJ+mtTO2mGkUFnqyUHsZUGd5OM5qwRF4eSYgYPyslxC0Hlb7Nl+AmY  +1Krcoi7njAkZ8/2xcUjHroKGood3VI/05ey18mrPqcpRD8K8UCkcP+P7vqgRM0F  pJUu9iRgXamjhgn991hsPDRcjohhKzaismMtW9ctkTlFtem+oHc+YQRcqT+nrS7Y  McGcvDpl8dRt4Qa44srsfnWLgAfwPJDBk6i/cjpaG0cCTZ0Lclx9mMMDyarn+tMT  LNz+GVGtUjWqp+Ddp4lFAL/PPuPNLMLJiRiz5oy8WuSIZorRkbgoUS85YXhPD6Ip  ijIUGmqTBpbsDWMICsUQ  =+Hb+  -----END PGP SIGNATURE-----
    Add documentation and test coverage to createChainedEventKeyCallback
    
    UIP-294
    + Add doc comments describing createChainedEventKeyCallback
    + Improve style of createChainedEventKeyCallback - rename unclear
    variable and add block to if statement
    + Add test coverage for the order of the function execution, and around
    return values.

--- lib/src/ui_core/util/handler_chain_util.dart
+++ lib/src/ui_core/util/handler_chain_util.dart
@@ -1,11 +1,16 @@
 part of w_ui_platform.ui_core;
 
+/// Creates a EventKeyCallback that calls through to the two provided callbacks in order.
+/// Useful for executing multiple callbacks where only a single callback is accepted.
+///
+/// Returns `false` if one or more of the provided callback returns `false`.
 EventKeyCallback createChainedEventKeyCallback(EventKeyCallback a, EventKeyCallback b) {
   return (react.SyntheticEvent event, dynamic eventKey) {
-    var aDidPreventDefault = a != null ? a(event, eventKey) == false : false;
-    var bDidPreventDefault = b != null ? b(event, eventKey) == false : false;
+    var aDidReturnFalse = a != null ? a(event, eventKey) == false : false;
+    var bDidReturnFalse = b != null ? b(event, eventKey) == false : false;
 
-    if (aDidPreventDefault || bDidPreventDefault)
+    if (aDidReturnFalse || bDidReturnFalse) {
       return false;
+    }
   };
 }
