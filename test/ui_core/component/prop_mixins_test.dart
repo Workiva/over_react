fbe086d87235fd0fad87a4c8dd785ed6f38447a2
Greg Littlefield <greg.littlefield@webfilings.com>
2015-08-25 12:28:00 -0700

    Rename w_ui_platform package to web_skin_dart, and rename references
    UIP-529

--- test/ui_core/component/prop_mixins_test.dart
+++ test/ui_core/component/prop_mixins_test.dart
@@ -1,7 +1,7 @@
 library prop_mixins_test;
 
 import 'package:test/test.dart';
-import 'package:w_ui_platform/ui_core.dart';
+import 'package:web_skin_dart/ui_core.dart';
 
 import 'prop_utils.dart';
 
