e2ab8eeb48e195722a76762cc98132dc312682b4
Greg Littlefield <greg.littlefield@webfilings.com>
2015-09-14 15:56:11 -0700

    Update typedef for mouseenter/mouseleave events
    UIP-286

--- lib/src/ui_core/component/callback_typedefs.dart
+++ lib/src/ui_core/component/callback_typedefs.dart
@@ -7,6 +7,12 @@ typedef KeyboardEventCallback(react.SyntheticKeyboardEvent event);
 typedef FocusEventCallback(react.SyntheticFocusEvent event);
 typedef FormEventCallback(react.SyntheticFormEvent event);
 typedef MouseEventCallback(react.SyntheticMouseEvent event);
+/// A callback for React 'mouseenter' and 'mouseleave' events.
+///
+/// React JS either calls this with:
+/// - just a [react.SyntheticMouseEvent] event
+/// - a [JsObject] and String id
+typedef MouseEnterLeaveEventCallback(/* String|JsObject */ eventOrInstance, [String reactId]);
 typedef TouchEventCallback(react.SyntheticTouchEvent event);
 typedef UIEventCallback(react.SyntheticUIEvent event);
 typedef WheelEventCallback(react.SyntheticWheelEvent event);
