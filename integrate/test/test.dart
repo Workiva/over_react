7478813625f35727a4e3c658e27ba27362296537
Claire Sarsam <claire.sarsam@workiva.com>
2015-09-30 16:39:49 -0700

    Add test to integration example
    UIP-579
    + This enables us to validate that importing web_skin_dart doesn't cause
    runtime errors.

---
+++ integrate/test/test.dart
@@ -0,0 +1,19 @@
+@TestOn('dartium')
+library test;
+
+import 'package:react/react.dart' as react;
+import 'package:react/react_client.dart' show setClientConfiguration;
+import 'package:test/test.dart';
+import 'package:react/react_test_utils.dart' as react_test_utils;
+import 'package:web_skin_dart/ui_components.dart';
+import 'package:web_skin_dart/ui_core.dart';
+
+/// Entry point into tests
+main() {
+  setClientConfiguration();
+
+  test('renders without breaking', () {
+    var well = react_test_utils.renderIntoDocument(Well()('Hello World!'));
+    expect(well, isNotNull);
+  });
+}
