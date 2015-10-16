6e53c816e894c9b11bdc92d80c9e3fc59a6ff47d
Jace Hensley <jace.hensley@workiva.com>
2015-10-16 11:10:49 -0700

    iQIcBAABCAAGBQJYXA4JAAoJEJ5mu3zHWF1P2uEQAJxvzMIcShhCsHwDa7pC5OJJ  HWl+KAAlI8P3e8c+6H6TZXdwWbnw5Iv+2rAm1Hhhg2q7Ols1jiDbYwCy7VKPFPoI  yYSOKejOyKoXujTkwwrmzSnkES81pJhdhmCfkHI4PUM6ptZ8PMoi2m0OHNhQ/E7v  sKrT4NP1gddqNyCEh5XXh3/Ps+5ytbqiU8INNRMI1ttInrsP/OGPqIMULyOdq2uI  qI5Lyb2Owk/IEHQmI45RDRHm3BgjhkxCgrEUH9BUpfWQNafyQjBUJDHvn9nMCoTY  DIfnFiJf4wPfto3olUT8vaJIz/zaWsgOtdDHaU6Zl65OXoP1cZ8wVg6C+a3q+NAR  oay3n4XmtGhVw8kpEw5shKCvE/4kroA4tZYrTf4GEOkAx2ZsyvapFQ2Mc4QbGHHR  jumBk5GJIBqlB3vErIQgCd6RHHzdHJgDYulMAwPTQYHJ0cdiZD80Ef4tTke64Klb  0Wq/K0ZomTl0SN/5yZclJuNb6SdpaRkoOM6NpvPx6TKWLoXPLVPw8Bafzfsnv2rX  H9aCZ8zqOVHk1NjLBS6J0I+9CtCj85MVGdWBcqxK0vRG362Wn5Lejj5zzP7n2K76  EnDQLs6s30gf79vFusKWnUd4rGcTgHYIE7UW2OP3kOY2ZFJroU0qeMwYi7jeKhIE  zXx0hZ3g2I3HwyGOzHdr  =uhuf  -----END PGP SIGNATURE-----
    Add tests for children passed in to components Dom and Custom
    
    UIP-452

---
+++ test/ui_core/component/base_component_test.dart
@@ -0,0 +1,135 @@
+library base_component_test;
+
+import 'dart:js';
+
+import 'package:react/react.dart' as react;
+import 'package:react/react_client.dart';
+import 'package:test/test.dart';
+import 'package:web_skin_dart/ui_components.dart';
+import 'package:web_skin_dart/ui_core.dart';
+
+import '../../test_util/react_util.dart';
+import '../../test_util/custom_matchers.dart';
+
+/// Main entry point for BaseComponent testing
+main() {
+  group('ComponentDefinition', () {
+    group('renders a DomComponentDefinition with the correct chilren', () {
+      test('chilren is null', () {
+        var renderedInstance = render(Dom.div()(null));
+
+        expect(getJsProps(renderedInstance)['children'], isNull);
+      });
+
+      test('a single child is passed in', () {
+        var child = 'Only child';
+        var renderedInstance = render(Dom.div()(child));
+
+        expect(getJsProps(renderedInstance)['children'], equals(child));
+      });
+
+      test('children are set via a list', () {
+        var children = ['First Child', 'Second Child'];
+        var renderedInstance = render(Dom.div()(children));
+
+        expect(getJsProps(renderedInstance)['children'], new isInstanceOf<List>(), reason: 'Should be a list because lists will be JSified');
+        expect(getJsProps(renderedInstance)['children'], equals(children));
+      });
+
+      test('children are set via an iterable', () {
+        var children = (() sync*{
+          yield 'First Child';
+          yield 'Second Child';
+        })();
+        var renderedInstance = render(Dom.div()(children));
+
+        expect(getJsProps(renderedInstance)['children'], new isInstanceOf<JsArray>(), reason: 'Should not be a Dart Object');
+        expect(getJsProps(renderedInstance)['children'], orderedEquals(children));
+      }, skip: 'TODO: Remove skip once react-dart is updated to accept iterables: https://github.com/cleandart/react-dart/pull/71');
+
+      test('children are set variadically via noSuchMethod', () {
+        var firstChild = 'First Child';
+        var secondChild = 'Second Child';
+        var renderedInstance = render(Dom.div()(firstChild, secondChild));
+
+        expect(getJsProps(renderedInstance)['children'], new isInstanceOf<JsArray>(), reason: 'Should not be a Dart Object');
+        expect(getJsProps(renderedInstance)['children'], equals([firstChild, secondChild]));
+      });
+    });
+
+    group('renders a BaseComponentDefinition with the correct children when', () {
+      test('chilren is null', () {
+        var renderedInstance = render(TestComponent()(null));
+
+        expect(getProps(renderedInstance)['children'], new isInstanceOf<List>(), reason: 'Should still be the Dart Object');
+        expect(getProps(renderedInstance)['children'], isEmpty);
+
+        expect(getJsProps(renderedInstance)['children'], isNull);
+      });
+
+      test('a single child is passed in', () {
+        var child = 'Only child';
+        var renderedInstance = render(TestComponent()(child));
+
+        expect(getProps(renderedInstance)['children'], new isInstanceOf<List>(), reason: 'Should still be the Dart Object');
+        expect(getProps(renderedInstance)['children'], equals([child]));
+
+        expect(getJsProps(renderedInstance)['children'], equals(child));
+      });
+
+      test('children are set via a list', () {
+        var children = ['First Child', 'Second Child'];
+        var renderedInstance = render(TestComponent()(children));
+
+        expect(getProps(renderedInstance)['children'], new isInstanceOf<List>(), reason: 'Should still be the Dart Object');
+        expect(getProps(renderedInstance)['children'], equals(children));
+
+        expect(getJsProps(renderedInstance)['children'], new isInstanceOf<List>(), reason: 'Should be a list because lists will be JSified');
+        expect(getJsProps(renderedInstance)['children'], equals(children));
+      });
+
+      test('children are set via an iterable', () {
+        var children = (() sync*{
+          yield 'First Child';
+          yield 'Second Child';
+        })();
+        var renderedInstance = render(TestComponent()(children));
+
+        expect(getProps(renderedInstance)['children'], new isInstanceOf<List>(), reason: 'Should still be the Dart Object');
+        expect(getProps(renderedInstance)['children'], orderedEquals(children));
+
+        expect(getJsProps(renderedInstance)['children'], new isInstanceOf<JsArray>(), reason: 'Should not be a Dart Object');
+        expect(getJsProps(renderedInstance)['children'], orderedEquals(children));
+      }, skip: 'TODO: Remove skip once react-dart is updated to accept iterables: https://github.com/cleandart/react-dart/pull/71');
+
+      test('children are set variadically via noSuchMethod', () {
+        var firstChild = 'First Child';
+        var secondChild = 'Second Child';
+        var renderedInstance = render(TestComponent()(firstChild, secondChild));
+
+        expect(getProps(renderedInstance)['children'], new isInstanceOf<List>(), reason: 'Should still be the Dart Object');
+        expect(getProps(renderedInstance)['children'], equals([firstChild, secondChild]));
+
+        expect(getJsProps(renderedInstance)['children'], new isInstanceOf<JsArray>(), reason: 'Should not be a Dart Object');
+        expect(getJsProps(renderedInstance)['children'], equals([firstChild, secondChild]));
+      });
+    });
+  });
+}
+
+TestComponentDefinition TestComponent() => new TestComponentDefinition({});
+
+class TestComponentDefinition extends BaseComponentDefinition {
+  TestComponentDefinition(Map props) : super(_TestComponentComponentFactory, props);
+}
+
+ReactComponentFactory _TestComponentComponentFactory = react.registerComponent(() => new _TestComponent());
+class _TestComponent extends BaseComponent<TestComponentDefinition> {
+  @override
+  render() {
+  }
+
+  @override
+  TestComponentDefinition typedPropsFactory(Map propsMap) => new TestComponentDefinition(propsMap);
+}
+
