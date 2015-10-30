70b276487d490731400ab1bd66d407ec8bce8dbe
Tim McCall <tim.mccall@workiva.com>
2015-10-30 10:50:36 -0600

    iQIcBAABCAAGBQJYXA4PAAoJEJ5mu3zHWF1Pb0MP+wbApbRs6SRnNz1m5Vl3m8cS  VR3XxNjAXAVcSQm6Ie35U1XeGQEWQ0yzBMHMMiZT61Jf/yKrUYfG+Ca4psinaX6v  s10JgNdVBce+nPCgVV7Z0LXY5J2R8M04zOqBJh2JrQrBBeYeV5P4YKRr7xhiDwml  z31p1MwPuExszVeMOKLRh0rI98EoAXCcT8oeMErNwYGZ6cXjssqy6ykori42v+yJ  jjkkZVsQ+jzeYznpX76Z/Nc8fCUeL+NxwpOfjClA69/m7duF0c4WzesAANolHIAm  PKbW+Kw130yrqDrtvdI+6nFJh8iCL23u5wJR3BXSkEfbjD7qgRjQ5hasNwqyhXRV  hxi50XlW65WyVKh+CrkV/xSX/oZ+rNq5idg6jYXxtf87hmsxVX1wrUJLUU3Qsr6p  6UQYAe8pDxW54ezqdnIXsGFRfIe5Q6fdqQft42kkKGH50mSFQzvP3anQ8WRZLEki  M4zvXM6NjsSc6hx1ziDxp/K+ANYq8JCGtMZHF2U4YNAktKBLilWWZEEfLGhdlrLk  YO4vfElWw+GElrtmDyE4aNnTsNHx1bg5Fcg4S0Ej7HmVQSp1YfDA0U61pPgx834N  DApGDp8b8X4Yfvo3r1JdJZShIfT5HJmLeN2X0PbtT4Gqcbw6W4yYM8Ui0+0U6MXS  sLK67uSa37jslvm0N7bK  =8AyG  -----END PGP SIGNATURE-----
    REF-212: Add ResizeSensor
    
    The ResizeSensor detects when its host element is resized and invokes
    a callback provided via `onResize`. This component is needed because
    the DOM API does not provide a resize event on elements.

---
+++ test/ui_core/component/resize_sensor_test.dart
@@ -0,0 +1,114 @@
+@Timeout(const Duration(seconds: 2))
+library resize_sensor_test;
+
+import 'dart:async';
+import 'dart:html';
+
+import 'package:react/react.dart' as react;
+import 'package:react/react_client.dart' as reactClient;
+import 'package:react/react_test_utils.dart' as reactTestUtils;
+import 'package:test/test.dart';
+
+import 'package:web_skin_dart/ui_core.dart';
+
+void main() {
+  reactClient.setClientConfiguration();
+
+  group('ResizeSensor', () {
+    const int containerWidth = 100;
+    const int containerHeight = 100;
+
+    Element domTarget;
+
+    setUp(() {
+      domTarget = document.createElement('div');
+      document.body.append(domTarget);
+    });
+
+    tearDown(() {
+      domTarget.remove();
+    });
+
+    Element renderSensorIntoContainer(ResizeHandler handler) {
+      // Create component hierarchy.
+      var sensor = (ResizeSensor()..onResize = handler)();
+      var container = react.div({
+        'className': 'container',
+        'style': {
+          'position': 'absolute',
+          'width': containerWidth,
+          'height': containerHeight
+        }
+      }, sensor);
+
+      // Render into DOM and validate.
+      var jsContainer = react.render(container, domTarget);
+      var jsTree = reactTestUtils.findRenderedDOMComponentWithClass(jsContainer, 'container');
+      expect(jsTree, isNotNull);
+
+      // Return the container element for testing.
+      return reactTestUtils.getDomNode(jsTree);
+    }
+
+    /// Expect resize sensor invokes registered `onResize` callback.
+    ///
+    /// Note: Test cases must await calls to this function. A boolean value is
+    /// tracked to confirm callback invocation, instead of `expectAsync`, due to
+    /// oddities in detecting callback invocations.
+    Future expectResizeAfter(void action(Element container),
+        {void onResize(ResizeSensorEvent)}) async {
+      var wasResizeDetected = false;
+
+      Element containerEl;
+      containerEl = renderSensorIntoContainer((event) {
+        if (onResize != null) {
+          onResize(event);
+        }
+        wasResizeDetected = true;
+      });
+
+      action(containerEl);
+
+      // Note: there is a delay here because Smithy has trouble running these
+      // tests successfully without it. :(
+      await new Future.delayed(const Duration(milliseconds: 200),
+          () => expect(wasResizeDetected, isTrue));
+    }
+
+    test('should detect when bounding rect grows horizontally', () async {
+      await expectResizeAfter((containerEl) {
+        containerEl.style.width = '${containerWidth * 2}px';
+      });
+    });
+
+    test('should detect when bounding rect grows vertically', () async {
+      await expectResizeAfter((containerEl) {
+        containerEl.style.height = '${containerHeight * 2}px';
+      });
+    });
+
+    test('should detect when bounding rect shrinks horizontally', () async {
+      await expectResizeAfter((containerEl) {
+        containerEl.style.width = '${containerWidth / 2}px';
+      });
+    });
+
+    test('should detect when bounding rect shrinks vertically', () async {
+      await expectResizeAfter((containerEl) {
+        containerEl.style.height = '${containerHeight / 2}px';
+      });
+    });
+
+    test('should pass the correct event args on resize', () async {
+      await expectResizeAfter((containerEl) {
+        containerEl.style.width = '${containerWidth * 2}px';
+        containerEl.style.height = '${containerHeight * 2}px';
+      }, onResize: (ResizeSensorEvent event) {
+        expect(event.newWidth, equals(containerWidth * 2));
+        expect(event.newHeight, equals(containerHeight * 2));
+        expect(event.prevWidth, equals(containerWidth));
+        expect(event.prevHeight, equals(containerHeight));
+      });
+    });
+  });
+}
