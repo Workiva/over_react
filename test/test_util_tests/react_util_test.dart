c3f8558226d76285015eb54f0e2c773270a02ca7
Claire Sarsam <claire.sarsam@workiva.com>
2015-11-02 15:09:56 -0700

    Add CustomMatchers test suite and doc comments
    UIP-638

---
+++ test/test_util_tests/react_util_test.dart
@@ -0,0 +1,13 @@
+library react_util_test;
+
+import 'package:test/test.dart';
+import 'package:web_skin_dart/ui_components.dart';
+import 'package:web_skin_dart/ui_core.dart';
+
+/// Main entry point for ReactUtil testing
+main() {
+  group('ReactUtil', () {
+
+
+  });
+}
