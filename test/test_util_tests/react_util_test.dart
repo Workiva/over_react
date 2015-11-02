6434e659d2f3068e286b91a472595b9eb4a5b8af
Claire Sarsam <claire.sarsam@workiva.com>
2015-11-02 16:20:11 -0700

    iQIcBAABCAAGBQJYXA4RAAoJEJ5mu3zHWF1PQT8QALzl6kBgij/htWKGXqZOiZZt  uMduBanDU4X7ng+Wbgc4vDKEpl9w6UcM9EV6EAuQeiMSDZrQ/qMHJ4sdmDE0WTFT  VXcWqGIvac75PifzRXet4c0FYXnI1YjFxB8XA36b/4IBHlq3nIcl+uG4TEoeXYVx  L3MfAdCRXVt6yQFU42maUtO5VhCV6HKijgyEbL7jbsFn6CMCvjYqTbFyr8A/z+jE  YRLMJzo7bZZtGF4jefrk8vttPvzT9fm0Ex1zpTnx1PQg7akx9tgktkhL7BbqrQLs  DhcIauE8E95koAvoA/wASbpo4+VvuAu/MN6eRI3Pa6C7ZLaPGgCf+0bFMynZsZh0  UJWYpQlT8gM+2YIw2SdtWnRmWZf4Okd9aDeBa6AFtMPchxeGqcZHYjS7iBgYOQkD  unVaGwfNCSyLyOrloaF0WJMmphOJA3KI8OD6bG8quQD9BsyILrftWP4JL1sVw2LE  8Z8n4VsS8DfwuEx82qBv0MdoIrYsGzl3OR1ELtE2HQrjwx16VZu/WF59iKhXj2O+  FRZjf1vVwI6khSm+llFtrz8qda69asaOXQTrqYCVVHrPfXZ7fFS2LjL4ycBZcBJ+  du5fgm+9Ro03Pqd3u36fSgbuHJuz2vGQRjIOCsZQp/gkrGahxUe8U3bcGoSQAadV  z7xkBDLeJWgYYUUlv5q9  =7eAC  -----END PGP SIGNATURE-----
    Add test coverage to react_util
    
    UIP-638

--- test/test_util_tests/react_util_test.dart
+++ test/test_util_tests/react_util_test.dart
@@ -1,13 +1,85 @@
 library react_util_test;
 
+import 'dart:html';
+
 import 'package:test/test.dart';
-import 'package:web_skin_dart/ui_components.dart';
+import 'package:web_skin_dart/test_util.dart';
 import 'package:web_skin_dart/ui_core.dart';
 
 /// Main entry point for ReactUtil testing
 main() {
   group('ReactUtil', () {
+    test('renderAttachedToDocument renders the component into the document', () {
+      expect(document.body.children, isEmpty);
 
+      var renderedInstance = renderAttachedToDocument(Dom.div());
 
+      expect(document.body.children[0].children.contains(findDomNode(renderedInstance)), isTrue,
+          reason: 'The component should have been rendered into the container div.');
+    });
+
+    group('getRef', () {
+      test('returns the JsObject if the ref exists', () {
+        var renderedInstance = render(RenderingContainerComponentFactory({'renderer': Dom.div()..ref = 'childDiv'}));
+
+        var ref = getRef(renderedInstance, 'childDiv');
+        expect(ref, isNotNull);
+      });
+
+      test('returns null if the ref doesn\'t exist', () {
+        var renderedInstance = render(RenderingContainerComponentFactory({'renderer': Dom.div()}));
+
+        var ref = getRef(renderedInstance, 'childDiv');
+        expect(ref, isNull);
+      });
+    });
+
+    test('click simulates a click on a component', () {
+      var flag = false;
+      var renderedInstance = render((Dom.div()..onClick = (evt) => flag = true)());
+
+      click(renderedInstance);
+
+      expect(flag, isTrue);
+    });
+
+    test('simulateMouseEnter simulates a MouseEnter on a component', () {
+      var flag = false;
+      var renderedInstance = render((Dom.div()..onMouseEnter = (evt) => flag = true));
+
+      simulateMouseEnter(findDomNode(renderedInstance));
+
+      expect(flag, isTrue);
+    });
+
+    test('simulateMouseLeave simulates a MouseLeave on a component', () {
+      var flag = false;
+      var renderedInstance = render((Dom.div()..onMouseLeave = (evt) => flag = true));
+
+      simulateMouseLeave(findDomNode(renderedInstance));
+
+      expect(flag, isTrue);
+    });
+
+    test('findDescendantsWithProp returns the descendants with the propKey', () {
+      var renderedInstance = render(Dom.div()([
+        (Dom.div()..disabled = true)(),
+        Dom.div()([
+          (Dom.div()..disabled = true)()
+        ])
+      ]));
+
+      var descendants = findDescendantsWithProp(renderedInstance, 'disabled');
+      expect(descendants.length, equals(2));
+    });
+
+    test('setProps sets a subset of a component\'s props', () {
+      var renderedInstance = render(Dom.div()..tabIndex = -1);
+
+      setProps(renderedInstance, {'className': 'class1'});
+
+      expect(getProps(renderedInstance)['className'], equals('class1'));
+      expect(getProps(renderedInstance)['tabIndex'], equals(-1));
+    });
   });
 }
