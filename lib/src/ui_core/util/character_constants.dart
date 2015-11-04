5461d8cd794522b7370d2bbc98b57d5cde9573d5
Claire Sarsam <claire.sarsam@workiva.com>
2015-11-04 16:53:37 -0700

    Add doc comments to character constants
    UIP-639

--- lib/src/ui_core/util/character_constants.dart
+++ lib/src/ui_core/util/character_constants.dart
@@ -1,6 +1,13 @@
 library ui_core.character_constants;
 
+/// ' ' - The non-breaking space character.
 const String nonBreakingSpace = '\u00a0';
+
+/// '×' - The multiplication sign character.
 const String multiplicationSign = '\u00d7';
+
+/// '·' - The middle dot character.
 const String middleDot = '\u00b7';
+
+/// '…' - The horizontal ellipsis character.
 const String horizontalEllipsis = '\u2026';
