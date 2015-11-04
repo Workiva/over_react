87c8e7a207e6facd2d4c45d1d0e1106ede7f9cc4
Claire Sarsam <claire.sarsam@workiva.com>
2015-11-04 16:36:24 -0700

    Add horizontalEllipsis character constant
    UIP-639
    + Also fixed spelling of character_constants filename

--- lib/src/ui_core/util/character_constants.dart
+++ lib/src/ui_core/util/character_constants.dart
@@ -3,3 +3,4 @@ library ui_core.character_constants;
 const String nonBreakingSpace = '\u00a0';
 const String multiplicationSign = '\u00d7';
 const String middleDot = '\u00b7';
+const String horizontalEllipsis = '\u2026';
