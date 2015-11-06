63143030c5fbdec9c5f02990435b45c52b8d0612
Greg Littlefield <greg.littlefield@webfilings.com>
2015-11-06 16:57:45 -0700

    iQIcBAABCAAGBQJYXA4mAAoJEJ5mu3zHWF1P4nMQAMM3DVYzSbi392Xf6e9y5gAV  1Ik/bAmr91qjZSf3quC/22rPqvcgUdyWhN+q10c5QXpIgQJM8DIRkSiSFPHyNKiJ  HBTQWwU0E84iN34/jFndggS6VDttNw2YeAQgKz4NyMGU80I+DHdVgBgV0UhE9VK9  KIbxjN88+Dqb7RwvmxFDg9t0mzcun/0kicUPif9X/68C1I9tipVtlMwyI8iN3ncx  pIyeNZI9NdciICElmcm3RpSaJ+ntp7vlYlKrZoQ4WItFioOh72b4LSKOm1sjnei8  NggFLlvmf2i2PXb3HxbYdfpYn889usKnVLQlTzUrNjZaCcJ/ozTeQmqw0vLea3tr  bS0iuc6D9dk7Wpc2iUus77qit3sD1iJbYV3xiaS98fc3KbP4hGWr1sgcZY8s61X4  w6HYuSONRb5gXfm00xpYiJrg6nRzGJF+OVQcsAXOUnNJ5aH+Y/ITG/AzCUVp5LY6  SipuyVxe7BAAGr6D5JbTuUvl02calKGpIupAALUtAM4iS7rNZ/7gtQ0j/1na3Avc  8ozGFwShSkv6XgcVc4MpKBpfbFTMH89QLNgO+zT1XGmTjHAPqmtwCiYDVpQ6A46G  V841fu/IroCDemi0ErBE99M4fqPMAGukPMQGuJhH2vQ4Pyk/w78gWQddMsSU4KLg  AkltumA5kvIAgT9U9iXZ  =yO5f  -----END PGP SIGNATURE-----
    Dependency-inject mock timers for OverlayTrigger tests
    
    UIP-286

--- test/wsd_test_util/mock_classes.dart
+++ test/wsd_test_util/mock_classes.dart
@@ -1,5 +1,6 @@
 library mock_classes;
 
+import 'dart:async';
 import 'dart:html';
 
 import 'package:mockito/mockito.dart';
@@ -10,4 +11,40 @@ class MockKeyEvent extends Mock implements KeyEvent {
 
 class MockDocument extends Mock implements Document {
   noSuchMethod(i) => super.noSuchMethod(i);
+}
+
+typedef void _TimerCallback();
+
+class MockTimer extends Mock implements Timer {
+  noSuchMethod(i) => super.noSuchMethod(i);
+
+  bool _isInstantiated = false;
+  _TimerCallback _callback;
+  Duration _duration;
+
+  /// Whether the timer has been initialized via [getTimerFactory].
+  bool get isInstantiated => _isInstantiated;
+
+  /// The callback the timer was initialized with.
+  _TimerCallback get callback => _callback;
+
+  /// The duration the timer was initialized with.
+  Duration get duration => _duration;
+
+  /// Returns a function that initializes [mock] with the given duration and callback.
+  ///
+  /// This function will raise an [AssertionError] if called more than once.
+  static Function getTimerFactory(MockTimer mock) {
+    Timer timerFactory(Duration duration, _TimerCallback callback) {
+      assert(!mock._isInstantiated);
+
+      mock._isInstantiated = true;
+      mock._callback = callback;
+      mock._duration = duration;
+
+      return mock;
+    };
+
+    return timerFactory;
+  }
 }
