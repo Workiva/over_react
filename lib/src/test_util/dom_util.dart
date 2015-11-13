fe22912ce06026623ae863eea536b769281c6cc8
Jace Hensley <jace.hensley@workiva.com>
2015-11-13 16:50:48 -0700

    iQIcBAABCAAGBQJYXA4/AAoJEJ5mu3zHWF1PBHYP/RFDSwdFYpP0YHtC9Loxx/ef  UJIyCoq9+OOBBgNQi+xgzUOSc1xWwXv0zQOLHd8DKEP/YKMIOtnzI8aESQYF0Uf2  bpiCj1gebE6/cv0Z45irE45HhNKVrFTkQXsHF3/6dsn9gOLDwtYDSTPzJFU+X3bD  C07u2EDe44K4gL5laqBLZWtzJ1Q9qKLTj2rWxluo7hpoj/EJrYz5s+DAFaN/oRCI  z2D43IHIYaAeQ5f1uTwLpKso6SWYGXc9l8yRYFWDQfkD/Cl3rMJDui17FherOXrK  /Wz/HjKG6e6rS6JYF2zUtoKxNIxEgmj76vNF+0sSU+Zrncp6Vxtrl04hVPEl31nY  flr7DLIiVvJ9JTQ8DjtyNW5Hwys8m+xNdpoBO5rxht1Id6pvLJj2PpFyONBhNCv3  h8jnQq/bk54fzbqHrNrxwYl7ladWAglHTHZ0/KvCD3TFCNjRa9WhrrC+uqNFmS0C  NJIfCRFbs33Wga+76mzxWiSYlpuk85G0EI5Jg1ILkDgLFVPlfXPUofoh9QLcLban  5QXQxbdi1iaxMK+VJCWVvJSAiRUuZj35O/KczxTXelNHhQXtyfnB8J8kVCZVEQOw  Nv0eqbHcapuqv1vCl5c/mmZYHwecPzE6qTrk6cHsU1yUfxn57WyQfJI5+hK3PyRx  RcPKStdn3kFcg14DgqZ1  =o1ko  -----END PGP SIGNATURE-----
    Add tests for dom_util.dart
    
    UIP-288

--- lib/src/test_util/dom_util.dart
+++ lib/src/test_util/dom_util.dart
@@ -3,15 +3,20 @@ library test_util.dom_util;
 import 'dart:async';
 import 'dart:html';
 
+/// Dispatches a transitionEnd event.
+///
+/// Used for testing components that rely on a transitionEnd event.
 Future triggerTransitionEnd(Element element) {
   Future eventFiredFuture = element.onTransitionEnd.first;
   element.dispatchEvent(new CustomEvent('transitionend'));
   return eventFiredFuture;
 }
 
+/// Dispatches a click event to the specified Element.
+///
+/// Verifies that the element is not a detached node.
 void triggerDocumentClick(Element target) {
-  // Ensure that the target is attached to the document.
-  assert(document.documentElement.contains(target));
+  assert(document.documentElement.contains(target) && 'Target should be attached to the document.' is String);
 
   target.dispatchEvent(new MouseEvent('click'));
 }
