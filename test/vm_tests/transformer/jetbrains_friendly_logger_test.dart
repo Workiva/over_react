a04cb3e3aafb9a76f377e15213795cd01b2501d0
Greg Littlefield <greg.littlefield@webfilings.com>
2015-11-23 17:49:31 -0700

    iQIcBAABCAAGBQJYXA5SAAoJEJ5mu3zHWF1P410P/24sRvD/48DP1Cy8JlQ++SJ4  helILtCR3/7hWWgVn4uJBLOyvLkfbJMPJNS6Z8Rq7oB77bAb5fzTbjNb1FJ71Mf2  Xn+di4K+NTKjAnwDmJCd6nq36Udvf9kLdkFVljAJq9S8Lra/kiLL4R3BluOlit8A  9vBXKcQq5NdeRTZeBlSpIsTNCR3MLuaxFllYyibBk6H6J7Q6Gyp+X8bnfONMIe7P  WaFmVgXevSDGKG9FsiBeHS6EfouBUd5a48F6WJSnNwc4bqDCYNxuW+vdKTDmwvDN  jQTjTRO0AZFHX3Noz9uGxZmQiEydRRBRBusEM5E/H3S1HaMR88vZjYv64SL1tU6F  sn6Hx7J/3Mc+XXRX2VnAhBfP5a8hmMcGdEeNS6akmFgd5teBzA8UYOPR4G79fvHb  30LToikrFtQbjI5zj6Lh68Y1xvgtIm6wqWfUION5pfAzukqFJ7WxZ9tSlesnJpjV  zq+6Df0/kypyD4hElrT/EUHQkX595Z56sKSaQmw00hvuIzuevn9XDYmE1D1bmITQ  UlllMtHzGIB2jntIYvLHCcc9L2yTrCXoBdxeHu18Fr0cI+KYZT3NSeAU5lpNYgrf  1momTNJsl164lnwpIg2lHHkN66hCFbogK1vfFh5Rm0pJByK8g9fKIYNcmTFA0pVx  DTwh5eKQsDorPbtfUBOn  =VWo7  -----END PGP SIGNATURE-----
    Add tests for JetBrainsFriendlyLogger
    
    UIP-599

---
+++ test/vm_tests/transformer/jetbrains_friendly_logger_test.dart
@@ -0,0 +1,96 @@
+library jetbrains_friendly_logger_test;
+
+import 'package:barback/barback.dart';
+import 'package:mockito/mockito.dart';
+import 'package:source_span/source_span.dart';
+import 'package:test/test.dart';
+import 'package:web_skin_dart/src/transformer/jetbrains_friendly_logger.dart';
+
+main() {
+  group('JetBrainsFriendlyLogger', () {
+    MockTransformLogger mockWrappedLogger;
+    JetBrainsFriendlyLogger logger;
+
+    setUp(() {
+      mockWrappedLogger = new MockTransformLogger();
+      logger = new JetBrainsFriendlyLogger(mockWrappedLogger);
+    });
+
+    group('clickableReference()', () {
+      test('returns a properly-formatted reference to a source span', () {
+        SourceFile sourceFile = new SourceFile('line 1\nline 2 TARGET\n line 3', url: 'source_url');
+        SourceSpan span = sourceFile.span(sourceFile.getText(0).indexOf('TARGET'));
+        String formatted = JetBrainsFriendlyLogger.clickableReference(span);
+        expect(formatted, '[source_url 2:8]: ');
+      });
+
+      test('returns an empty string when the specified span is null', () {
+        expect(JetBrainsFriendlyLogger.clickableReference(null), '');
+      });
+    });
+
+    group('proxies logs methods,', () {
+      SourceSpan span;
+      String formattedSpan;
+      AssetId asset;
+      String message;
+
+      setUp(() {
+        SourceFile sourceFile = new SourceFile('source file');
+
+        span = sourceFile.span(0);
+        formattedSpan = JetBrainsFriendlyLogger.clickableReference(span);
+        asset = new AssetId('package', 'path');
+        message = 'message';
+      });
+
+      group('prepending the message with the formatted source span when a span is specified:', () {
+        test('info()', () {
+          logger.info(message, asset: asset, span: span);
+          verify(mockWrappedLogger.info(formattedSpan + message, asset: asset));
+        });
+
+        test('fine()', () {
+          logger.fine(message, asset: asset, span: span);
+          verify(mockWrappedLogger.fine(formattedSpan + message, asset: asset));
+        });
+
+        test('warning()', () {
+          logger.warning(message, asset: asset, span: span);
+          verify(mockWrappedLogger.warning(formattedSpan + message, asset: asset));
+        });
+
+        test('error()', () {
+          logger.error(message, asset: asset, span: span);
+          verify(mockWrappedLogger.error(formattedSpan + message, asset: asset));
+        });
+      });
+
+      group('leaving the message as-is when a span is not specified:', () {
+        test('info()', () {
+          logger.info(message, asset: asset);
+          verify(mockWrappedLogger.info(message, asset: asset));
+        });
+
+        test('fine()', () {
+          logger.fine(message, asset: asset);
+          verify(mockWrappedLogger.fine(message, asset: asset));
+        });
+
+        test('warning()', () {
+          logger.warning(message, asset: asset);
+          verify(mockWrappedLogger.warning(message, asset: asset));
+        });
+
+        test('error()', () {
+          logger.error(message, asset: asset);
+          verify(mockWrappedLogger.error(message, asset: asset));
+        });
+      });
+    });
+  });
+}
+
+class MockTransformLogger extends Mock implements TransformLogger {
+  noSuchMethod(i) => super.noSuchMethod(i);
+}
