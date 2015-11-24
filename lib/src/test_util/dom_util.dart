c625e47e6012772ec35a75749b8302b1f0bd80bb
Jace Hensley <jace.hensley@workiva.com>
2015-11-24 11:34:10 -0700

    Update doc comments
    UIP-288

--- lib/src/test_util/dom_util.dart
+++ lib/src/test_util/dom_util.dart
@@ -3,18 +3,19 @@ library test_util.dom_util;
 import 'dart:async';
 import 'dart:html';
 
-/// Dispatches a transitionEnd event. And returns a Future that completes after the event has been dispatched.
+/// Dispatches a `transitionend` event when the CSS transition of the [element]
+/// is complete. Returns a [Future] that completes after the event has been dispatched.
 ///
-/// Used for testing components that rely on a transitionEnd event.
+/// Used for testing components that rely on a `transitionend` event.
 Future triggerTransitionEnd(Element element) {
   Future eventFiredFuture = element.onTransitionEnd.first;
   element.dispatchEvent(new CustomEvent('transitionend'));
   return eventFiredFuture;
 }
 
-/// Dispatches a click event to the specified Element.
+/// Dispatches a `click` event to the specified [target].
 ///
-/// Verifies that the element is not a detached node.
+/// Verifies that the [target] element is not a detached node.
 void triggerDocumentClick(Element target) {
   assert(document.documentElement.contains(target) && 'Target should be attached to the document.' is String);
 
