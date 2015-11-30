9efc5e25b4dc8dccab5087fcaa4c1c66326bff50
Greg Littlefield <greg.littlefield@webfilings.com>
2015-11-30 16:51:32 -0700

    iQIcBAABCAAGBQJYXA5YAAoJEJ5mu3zHWF1PxCUQAMmhI1aM7mMWEYC9q9D6+TMK  vzvJiaJUbDzEJNhehzCshwROeAz6p1a6RMhkLWZoPl14GSyzZW5BlXLaFu5LHtSa  ZtgAg08BldYAs0Az8HqXf3U4HaQLhaMV70jIpEevYxMZAuBC7BWElZSFjYOHnBvp  BOh4Cs1z6lXUfylGG5xfPA9KKzYOqjQJdTUjIA9kJhSETBFT2nZK9E+67fulEgna  5QQMUEWsaX8pvheAwM9FoJszKGnHnCdBaDbOqMRihjPbP6yyDi6YJs2e1NC3950g  SWNaaALz6OaB2cN9L/KrCMRqaWh20h59vkNs1/fFIwgNc0FEuBaZ4TZbnWyaP+NX  V7l4CVfj/s88p77NWNeO6J3irKBlTQgYmWOAOt4VsLl7bE50l0qMUBF4jHPDIpBO  C127Ngpd++IL8Vv70xp68Jc34MqwduUYva3tyjeO5AYEe5LxNmC/aGzyuYVLlyua  fyoy3GfRcpY+mzR2BUVOe8JEClWuDUiuiQShX3xUb6U/h57ikkKVQKnsin+dBTj+  pX1PyMCJcYPQac4f9gNFSqj+FQc5ak7I+S2RHBqtEXoHae8vbiCM8jcHBsdkXhhY  1EEWo0ENzrWIdvo/n+eBtIsDpmhB6t4d07oMYL2zgqzhS8lSfBKY3jSol7qhhNEC  OQ8OCO/NAVkJJvX19TOy  =jUFS  -----END PGP SIGNATURE-----
    Add some tests for new core component declaration helpers
    
    UIP-599

---
+++ test/ui_core/transformer_generation/helpers_test.dart
@@ -0,0 +1,147 @@
+library ui_core.transformer_generation.helpers_test;
+
+import 'package:test/test.dart';
+import 'package:web_skin_dart/ui_core.dart';
+
+const Matcher throwsAnUngeneratedError = const Throws(const isInstanceOf<UngeneratedError>());
+const Matcher throwsAnIllegalInstantiationError = const Throws(const isInstanceOf<IllegalInstantiationError>());
+
+main() {
+  group('transformation generation helpers:', () {
+    void testStubbedMapMembers(Map mapFactory()) {
+      group('Map members:', () {
+        Map map;
+
+        setUp(() {
+          map = mapFactory();
+        });
+
+        test('operator[]',    () {expect(() => map[null],                    throwsAnUngeneratedError);});
+        test('operator[]=',   () {expect(() => map[null] = null,             throwsAnUngeneratedError);});
+        test('addAll',        () {expect(() => map.addAll({}),               throwsAnUngeneratedError);});
+        test('clear',         () {expect(() => map.clear(),                  throwsAnUngeneratedError);});
+        test('containsKey',   () {expect(() => map.containsKey(null),        throwsAnUngeneratedError);});
+        test('containsValue', () {expect(() => map.containsValue(null),      throwsAnUngeneratedError);});
+        test('forEach',       () {expect(() => map.forEach((key, value) {}), throwsAnUngeneratedError);});
+
+        test('isEmpty',       () {expect(() => map.isEmpty,                  throwsAnUngeneratedError);});
+        test('isNotEmpty',    () {expect(() => map.isNotEmpty,               throwsAnUngeneratedError);});
+        test('keys',          () {expect(() => map.keys,                     throwsAnUngeneratedError);});
+        test('length',        () {expect(() => map.length,                   throwsAnUngeneratedError);});
+
+        test('putIfAbsent',   () {expect(() => map.putIfAbsent(null, () {}), throwsAnUngeneratedError);});
+        test('remove',        () {expect(() => map.remove(null),             throwsAnUngeneratedError);});
+        test('values',        () {expect(() => map.values,                   throwsAnUngeneratedError);});
+      });
+    }
+
+    group('UiProps', () {
+      test('cannot be instantiated directly if not generated', () {
+        expect(() => new UngeneratedUiProps(), throwsAnIllegalInstantiationError);
+      });
+
+      group('throws errors when stubbed members are called that have not been generated:', () {
+        UiProps unimplemented;
+
+        setUp(() {
+          unimplemented = new UnimplementedUiProps();
+        });
+
+        test('props',            () {expect(() => unimplemented.props,            throwsAnUngeneratedError);});
+        test('componentFactory', () {expect(() => unimplemented.componentFactory, throwsAnUngeneratedError);});
+        test('propKeyNamespace', () {expect(() => unimplemented.propKeyNamespace, throwsAnUngeneratedError);});
+
+        testStubbedMapMembers(() => new UnimplementedUiProps());
+      });
+    });
+
+    group('UiComponent', () {
+      test('cannot be instantiated directly if not generated', () {
+        expect(() => new UngeneratedUiComponent(), throwsAnIllegalInstantiationError);
+      });
+
+      group('throws errors when stubbed members are called that have not been generated:', () {
+        UiComponent unimplemented;
+
+        setUp(() {
+          unimplemented = new UnimplementedUiComponent();
+        });
+
+        test('typedPropsFactory', () {expect(() => unimplemented.typedPropsFactory({}), throwsAnUngeneratedError);});
+      });
+    });
+
+    group('UiState', () {
+      test('cannot be instantiated directly if not generated', () {
+        expect(() => new UngeneratedUiState(), throwsAnIllegalInstantiationError);
+      });
+
+      group('throws errors when stubbed members are called that have not been generated:', () {
+        UiState unimplemented;
+
+        setUp(() {
+          unimplemented = new UnimplementedUiState();
+        });
+
+        test('typedPropsFactory', () {expect(() => unimplemented.state, throwsAnUngeneratedError);});
+
+        testStubbedMapMembers(() => new UnimplementedUiState());
+      });
+    });
+
+    group('UiStatefulComponent', () {
+      test('cannot be instantiated directly if not generated', () {
+        expect(() => new UngeneratedUiStatefulComponent(), throwsAnIllegalInstantiationError);
+      });
+
+      group('throws errors when stubbed members are called that have not been generated:', () {
+        UiStatefulComponent unimplemented;
+
+        setUp(() {
+          unimplemented = new UnimplementedUiStatefulComponent();
+        });
+
+        test('typedPropsFactory', () {expect(() => unimplemented.typedPropsFactory({}), throwsAnUngeneratedError);});
+        test('typedStateFactory', () {expect(() => unimplemented.typedStateFactory({}), throwsAnUngeneratedError);});
+      });
+    });
+  });
+}
+
+
+class UngeneratedUiProps extends UiProps {}
+
+class UngeneratedUiComponent extends UiComponent {
+  render() {}
+}
+
+class UngeneratedUiState extends UiState {}
+
+class UngeneratedUiStatefulComponent extends UiStatefulComponent {
+  render() {}
+}
+
+
+class UnimplementedUiProps extends UiProps {
+  @override
+  bool get $generated => true;
+}
+
+class UnimplementedUiComponent extends UiComponent {
+  @override
+  bool get $generated => true;
+
+  render() {}
+}
+
+class UnimplementedUiState extends UiState {
+  @override
+  bool get $generated => true;
+}
+
+class UnimplementedUiStatefulComponent extends UiStatefulComponent {
+  @override
+  bool get $generated => true;
+
+  render() {}
+}
