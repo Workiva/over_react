17b760a3cda67e7a20ad8b4dfab3a600b2ff9a61
Greg Littlefield <greg.littlefield@webfilings.com>
2015-11-30 15:53:50 -0700

    iQIcBAABCAAGBQJYXA5YAAoJEJ5mu3zHWF1PLLoQALk5aymIYAs1rGDKejYuEh3i  IPDD0ds6wWWkMtHFpVz1JRBDfusDBD+oTHvWgQdVsJN1o64hD15NRn9yEXCsaCrG  VJ0LvDUb/CSMFeeAHg1mXz83afIOkB5Risrfdh1+pAq+C0hnc6x12W0jBhte/C5a  mTzuCepDbpLDN3TNisB+66SNXiVr+fWnfsRBcesz08bLVkKGhDWuX02psUiNVFH4  E5ouhIMSbDPMEshB3bS7BHnFnxRvLJXB0yhUo9mVmJMP5xEcanRh0fnIR4PEQ8cg  wIujY+Ovq7L2ii7NoRW51pkgdW38xk50DTQ+5BXL25EVt2OkNyglMJ8pdzv5KbI/  K8OIvmGeF1NKd5371LOTIDKawYxTZ4dtvliHeFVl/sBuilGuZ9HJqMNyHtsyu4Ca  HoqSiBYhvG1HIF525zQ6FSPJJIvohur4BUUb4/e+uWy30a70G4RDtnMSbtHz2ROR  DYMfjoGKW3scVUX9wSJobV6f3dr6RhrsvQ8M2bbVuKBWcvF2SkCBa8ewKeaJWLPn  lIFpVii+Tmf/IQgs+bzHQxs3U2WeusTFMuy4PLCgmTPnYud1taf7DXn3uYBKonVH  QawyOjY3WTFNKHaX8mVLs2gJwgbE2m28S75ePNtF1wzrq+Z+AKXDEczHvp6UedCF  1UrDLoIdSs5o7XOzqqkf  =N0oQ  -----END PGP SIGNATURE-----
    Add HTML diff view to aid in debugging web_skin_dart transformer
    
    UIP-599

--- lib/src/transformer/source_file_helpers.dart
+++ lib/src/transformer/source_file_helpers.dart
@@ -1,5 +1,7 @@
 library web_skin_dart.transformer.source_file_helpers;
 
+import 'dart:convert';
+
 import 'package:analyzer/analyzer.dart';
 import 'package:source_span/source_span.dart';
 
@@ -47,27 +49,90 @@ class TransformedSourceFile {
     );
   }
 
-  String getTransformedText() {
+  void processReplacements({onUnmodified(String string), onRemoval(String string), onAddition(String string)}) {
     _replacements.sort((r1, r2) => r1.span.compareTo(r2.span));
-
-    StringBuffer transformedSource = new StringBuffer();
-
+    
     var lastEdge = 0;
     for (Replacement replacement in _replacements) {
       if (replacement.span.start.offset < lastEdge) {
         throw 'Overlapping replacement $replacement in replacements $_replacements.';
       }
 
-      transformedSource
-        ..write(sourceFile.getText(lastEdge, replacement.span.start.offset))
-        ..write(replacement.newText);
+      if (onUnmodified != null) {
+        onUnmodified(sourceFile.getText(lastEdge, replacement.span.start.offset));
+      }
+      if (onRemoval != null) {
+        onRemoval(replacement.span.text);
+      }
+      if (onAddition != null) {
+        onAddition(replacement.newText);
+      }
 
       lastEdge = replacement.span.end.offset;
     }
 
-    transformedSource.write(sourceFile.getText(lastEdge));
+    if (onUnmodified != null) {
+      onUnmodified(sourceFile.getText(lastEdge));
+    }
+  }
+
+  String getTransformedText() {
+    StringBuffer transformedSource = new StringBuffer();
+
+    processReplacements(
+        onUnmodified: (source) => transformedSource.write(source),
+        onAddition: (source) => transformedSource.write(source)
+    );
 
     return transformedSource.toString();
+  }
+
+  String getHtmlDiff() {
+    StringBuffer diff = new StringBuffer();
+
+    void writeDiff(String source, String className) {
+      if (source.isEmpty) {
+        return;
+      }
+
+      diff.write('<span class="$className">');
+      diff.write(const HtmlEscape().convert(source));
+      diff.write('</span>');
+    }
+    
+    processReplacements(
+        onUnmodified: (source) => writeDiff(source, 'diff-unmodified'),
+        onRemoval: (source) => writeDiff(source, 'diff-removal'),
+        onAddition: (source) => writeDiff(source, 'diff-addition')
+    );
+
+    return '''
+        <!DOCTYPE html>
+        <html>
+        <head>
+          <title>web_skin_dart Transformer Diff - ${const HtmlEscape().convert(sourceFile.url.path)}</title>
+          <style>
+            .diff-unmodified {
+                color: #444;
+            }
+            .diff-removal {
+                color: #a00;
+                background-color: #fee;
+                text-decoration: line-through;
+            }
+            .diff-addition {
+                color: #0a0;
+                background-color: #efe;
+            }
+          </style>
+        </head>
+        <body>
+          <code>
+            <pre>$diff</pre>
+          </code>
+        </body>
+        </html>
+    ''';
   }
 }
 
