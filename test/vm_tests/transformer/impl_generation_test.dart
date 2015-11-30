524dabdafaa7e09568f75cb6f8821cb23d2b9162
Greg Littlefield <greg.littlefield@webfilings.com>
2015-11-30 09:35:06 -0700

    iQIcBAABCAAGBQJYXA5XAAoJEJ5mu3zHWF1PxV8QAM7iKelals2SulS1F8PIeJh6  f6PxryX4/D3Rv42MLM0FawjZiWGLI4nCUOqwDCLkh1waqUsQDVd9dsIKRaMjkAcg  VzZ5bzRzay8tF9zvoiSyj2seGKQyINjZFPdrepI4UIgXbFvxQQOtdbIAeceaPf8D  hZsz9S/NutBlJAm0ZjSGoq/rmN3vNkABm2tbJsn2fir+fYCdT7yOQPTJcsvv6Wz1  yJnaEJ/Sy/xVzbkPorlpIoieQXSNM9Suc/1312z632mr3yculasmu68QlnABU3lU  5xr1+az5pZETSo0zQuJnJKvyR1CEeycC563bycaSJbJ0A2I/OvYxszvBVN7F6cXx  2f7zKipnR1G+EOUSwWUJRBsn8Og+G9OfeY+nbAEEZLbnZUfNvjrhTT41YqFh58jQ  oZOI6jHXryxHUlfy6vz+cIcrX7hohnOlSJqp/oHZqeethzcRLGolP0EnlZ87/XDq  inDA2iCNDuQa121dn9fW2Px8kvtgsvwoeEoT4koUSRBU3ehXtwFkSame0VGzhB4K  RgF3VQfcHeIqS8u3Jo00kZEK16MBHF9LXwQzKu65FZo21DSjX4OQH2GAjIK7n6kf  w7JXE9CpApdWdXkRT1YJrMMOz1FoNdYYRryVCPzCywPsT4ZctrHT3G645T/qsQEf  bUDDv0uw3AzdVonvb2/R  =zt3F  -----END PGP SIGNATURE-----
    Add error log tests, validate transformed source, fix accessor stripping
    
    UIP-599

--- test/vm_tests/transformer/impl_generation_test.dart
+++ test/vm_tests/transformer/impl_generation_test.dart
@@ -12,59 +12,76 @@ import 'package:web_skin_dart/src/transformer/source_file_helpers.dart';
 
 main() {
   group('ComponentDeclarations', () {
-    group('preserves line numbers for', () {
-      ImplGenerator implGenerator;
+    ImplGenerator implGenerator;
 
-      MockTransformLogger logger;
-      SourceFile sourceFile;
-      TransformedSourceFile transformedFile;
-      CompilationUnit unit;
-      ComponentDeclarations declarations;
+    MockTransformLogger logger;
+    SourceFile sourceFile;
+    TransformedSourceFile transformedFile;
+    CompilationUnit unit;
+    ComponentDeclarations declarations;
+  
+    void setUpAndParse(String source) {
+      logger = new MockTransformLogger();
 
-      void setUpAndParse(String source) {
-        logger = new MockTransformLogger();
+      sourceFile = new SourceFile(source);
+      transformedFile = new TransformedSourceFile(sourceFile);
 
-        sourceFile = new SourceFile(source);
-        transformedFile = new TransformedSourceFile(sourceFile);
+      unit = parseCompilationUnit(source);
+      declarations = new ComponentDeclarations(unit, sourceFile, logger);
+      implGenerator = new ImplGenerator(logger, transformedFile);
+    }
 
-        unit = parseCompilationUnit(source);
-        declarations = new ComponentDeclarations(unit, sourceFile, logger);
-        implGenerator = new ImplGenerator(logger, transformedFile);
-      }
+    void setUpAndGenerate(String source) {
+      setUpAndParse(source);
 
-      void verifyNoErrors() {
-        // Check all permutations of optional parameters being specified
-        // since they look like different calls to Mockito.
-        verifyNever(logger.warning(any));
-        verifyNever(logger.warning(any, span: any));
-        verifyNever(logger.warning(any, asset: any));
-        verifyNever(logger.warning(any, span: any, asset: any));
-        verifyNever(logger.error(any));
-        verifyNever(logger.error(any, span: any));
-        verifyNever(logger.error(any, asset: any));
-        verifyNever(logger.error(any, span: any, asset: any));
+      implGenerator = new ImplGenerator(logger, transformedFile);
+      implGenerator.generateComponent(declarations);
+    }
 
-        expect(declarations.hasErrors, isFalse);
-      }
+    void verifyNoErrors() {
+      // Check all permutations of optional parameters being specified
+      // since they look like different calls to Mockito.
+      verifyNever(logger.warning(any));
+      verifyNever(logger.warning(any, span: any));
+      verifyNever(logger.warning(any, asset: any));
+      verifyNever(logger.warning(any, span: any, asset: any));
+      verifyNever(logger.error(any));
+      verifyNever(logger.error(any, span: any));
+      verifyNever(logger.error(any, asset: any));
+      verifyNever(logger.error(any, span: any, asset: any));
 
+      expect(declarations.hasErrors, isFalse);
+    }
+
+    void verifyTransformedSourceIsValid() {
+      expect(() {
+        parseCompilationUnit(transformedFile.getTransformedText());
+      }, isNot(throws), reason: 'transformed source should parse without errors');
+    }
+      
+    group('generates an implementation that parses correctly, preserving line numbers', () {
       void preservedLineNumbersTest(String source) {
         var lines = source.split('\n');
         for (int i = 0; i < lines.length; i++) {
-          lines[i] = '/* line $i start */${lines[i]}/* line $i end */';
+          lines[i] = '/* line $i start */${lines[i]}';
         }
         String numberedSource = lines.join('\n');
 
         setUpAndParse(numberedSource);
+
         implGenerator.generateComponent(declarations);
 
         String transformedSource = transformedFile.getTransformedText();
-
         var transformedLines = transformedSource.split('\n');
         for (int i = 0; i < lines.length; i++) {
           expect(transformedLines[i], startsWith('/* line $i start */'));
-          expect(transformedLines[i], endsWith('/* line $i end */'));
         }
       }
+
+      tearDown(() {
+        verifyNoErrors();
+        verifyTransformedSourceIsValid();
+      });
 
       test('stateful components', () {
         preservedLineNumbersTest('''
@@ -90,7 +107,6 @@ main() {
             }
           }
         ''');
-        verifyNoErrors();
       });
 
       test('props mixins', () {
@@ -102,7 +118,6 @@ main() {
             var baz;
           }
         ''');
-        verifyNoErrors();
       });
 
       test('state mixins', () {
@@ -114,7 +129,6 @@ main() {
             var baz;
           }
         ''');
-        verifyNoErrors();
       });
 
       test('abstract props classes', () {
@@ -124,7 +138,6 @@ main() {
             var baz;
           }
         ''');
-        verifyNoErrors();
       });
 
       test('abstract state classes', () {
@@ -134,20 +147,234 @@ main() {
             var baz;
           }
         ''');
-        verifyNoErrors();
       });
 
-      test('props mixins with accessors with funky formatting', () {
-        preservedLineNumbersTest('''
-          @PropsMixin()
-          class FooPropsMixin
-          { Map get props;
+      group('accessors', () {
+        test('with doc comments and annotations', () {
+          preservedLineNumbersTest('''
+            @AbstractProps()
+            class FooProps {
+              /// Doc comment
+              @Annotation()
+              var bar;
+            }
+          ''');
+        });
 
-            var bar, // line comment
-                baz, /* inline comment*/ qux;
+        group('defined using comma-separated variables', () {
+          test('on the same line', () {
+            preservedLineNumbersTest('''
+              @AbstractProps()
+              class FooProps {
+                var bar, baz, qux;
+              }
+            ''');
+          });
+
+          test('on separate lines', () {
+            String numberedSource = '''
+              /* line 0 start */@AbstractProps()
+              /* line 1 start */class FooProps {
+              /* line 2 start */  var bar,
+              /* line 3 start */      baz,
+              /* line 4 start */      qux;
+              /* line 5 start */}
+            ''';
+
+            setUpAndParse(numberedSource);
+
+            implGenerator.generateComponent(declarations);
+
+            String transformedSource = transformedFile.getTransformedText();
+            var transformedLines = transformedSource.split('\n');
+
+            expect(transformedLines[0].trimLeft(), startsWith('/* line 0 start */'));
+            expect(transformedLines[1].trimLeft(), startsWith('/* line 1 start */'));
+            expect(transformedLines[2].trimLeft(), startsWith('/* line 2 start */'));
+            // The leading comments on lines 3 and 4 get stripped out since comments
+            // are not currently preserved for comma-separated accessors.
+            // Uncomment these tests after comment preservation is added.
+//            expect(transformedLines[3].trimLeft(), startsWith('/* line 3 start */'));
+//            expect(transformedLines[4].trimLeft(), startsWith('/* line 4 start */'));
+            expect(transformedLines[5].trimLeft(), startsWith('/* line 5 start */'));
+          });
+        });
+      });
+    });
+    
+    group('logs an error when', () {
+      group('a factory is', () {
+        const String restOfComponent = '''
+          @Props()
+          class FooProps {}
+
+          @Component()
+          class FooComponent {}
+        ''';
+
+        test('declared using multiple variables', () {
+          setUpAndGenerate('''
+            @Factory()
+            UiFactory<FooProps> Foo, Bar;
+
+            $restOfComponent
+          ''');
+
+          verify(logger.error('Factory declarations must a single variable.', span: any));
+        });
+
+        test('declared with an initializer', () {
+          setUpAndGenerate('''
+            @Factory()
+            UiFactory<FooProps> Foo = null;
+
+            $restOfComponent
+          ''');
+
+          verify(logger.error('Factory variables are stubs for the generated factories, and should not have initializers.', span: any));
+        });
+      });
+
+      group('a props mixin is', () {
+        const String expectedPropsGetterError =
+            'Props mixin classes must declare an abstract props getter `Map get props;` '
+            'so that they can be statically analyzed properly.';
+
+        test('declared without an abstract `props` getter', () {
+          setUpAndGenerate('''
+            @PropsMixin()
+            class FooPropsMixin {
+              var bar;
+            }
+          ''');
+          verify(logger.error(expectedPropsGetterError, span: any));
+        });
+
+        group('declared with a malformed `props` getter:', () {
+          test('a field', () {
+            setUpAndGenerate('''
+              @PropsMixin()
+              class FooPropsMixin {
+                Map props;
+
+                var bar;
+              }
+            ''');
+            verify(logger.error(expectedPropsGetterError, span: any));
+          });
+
+          test('a getter of the wrong type', () {
+            setUpAndGenerate('''
+              @PropsMixin()
+              class FooPropsMixin {
+                NotAMap get props;
+
+                var bar;
+              }
+            ''');
+            verify(logger.error(expectedPropsGetterError, span: any));
+          });
+
+          test('an untyped getter', () {
+            setUpAndGenerate('''
+              @PropsMixin()
+              class FooPropsMixin {
+                get props;
+
+                var bar;
+              }
+            ''');
+            verify(logger.error(expectedPropsGetterError, span: any));
+          });
+
+          test('a concrete getter', () {
+            setUpAndGenerate('''
+              @PropsMixin()
+              class FooPropsMixin {
+                Map get props => null;
+
+                var bar;
+              }
+            ''');
+            verify(logger.error(expectedPropsGetterError, span: any));
+          });
+        });
+      });
+
+      group('a state mixin is', () {
+        const String expectedStateGetterError =
+            'State mixin classes must declare an abstract state getter `Map get state;` '
+            'so that they can be statically analyzed properly.';
+
+        test('declared without an abstract `state` getter', () {
+          setUpAndGenerate('''
+            @StateMixin()
+            class FooStateMixin {
+              var bar;
+            }
+          ''');
+          verify(logger.error(expectedStateGetterError, span: any));
+        });
+
+        group('declared with a malformed `state` getter:', () {
+          test('a field', () {
+            setUpAndGenerate('''
+              @StateMixin()
+              class FooStateMixin {
+                Map state;
+
+                var bar;
+              }
+            ''');
+            verify(logger.error(expectedStateGetterError, span: any));
+          });
+
+          test('a getter of the wrong type', () {
+            setUpAndGenerate('''
+              @StateMixin()
+              class FooStateMixin {
+                NotAMap get state;
+
+                var bar;
+              }
+            ''');
+            verify(logger.error(expectedStateGetterError, span: any));
+          });
+
+          test('an untyped getter', () {
+            setUpAndGenerate('''
+              @StateMixin()
+              class FooStateMixin {
+                get state;
+
+                var bar;
+              }
+            ''');
+            verify(logger.error(expectedStateGetterError, span: any));
+          });
+
+          test('a concrete getter', () {
+            setUpAndGenerate('''
+              @StateMixin()
+              class FooStateMixin {
+                Map get state => null;
+
+                var bar;
+              }
+            ''');
+            verify(logger.error(expectedStateGetterError, span: any));
+          });
+        });
+      });
+
+      test('accessors are declared as fields with initializers', () {
+        setUpAndGenerate('''
+          @PropsMixin()
+          class FooPropsMixin {
+            var bar = null;
           }
         ''');
-        verifyNoErrors();
+        verify(logger.error('Fields are stubs for generated setters/getters and should not have initializers.', span: any));
       });
     });
   });
