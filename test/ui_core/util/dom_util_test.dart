0630ff8a83d264b4730c7717a6b02829eb93135f
Jace Hensley <jace.hensley@workiva.com>
2015-12-01 12:54:36 -0700

    iQIcBAABCAAGBQJYXA5DAAoJEJ5mu3zHWF1P2+MQAN3a188+uvebslOTLBz8ySbS  pNBjNQa6pvATNU36x4mFmuvZxraI6GuLARsvWfl2swCJ03IcsNtM+UGx9vr/T6Co  evUYgjed75ZtOAyqoh41SFSENM8twpFAfzqG28VTpTMI1bqyqZf2e38FPs1a7Nnu  VRzuT/1u5sAdseQmzFErqau/QuqUEEAzxBPL4TnSDogq1vdGTMXaOISRFYUeyoaD  94fv9d2YGMW33l9d67DmVCR+7HEre2ONFrGi1yn8aU9n38+DO+vYPGx5B06bc4T/  oaNcKXqLOgnDJJBs5asqIhiOjz8KGc8DriOocN1OmJgnlvryYtCPCO7QsJH8EuVX  KmyzPdvG6bI6cx/r34iy0rVcxTzCs7qnQFV3EoGJ0ObjVqEups8DQ4ZnOxCgkrMa  B8OIzAqc8JwcVsEw9pTxaNPw7om+dJPrO6lcU9fSHs4eRY2ZzondfAuIcHeaLHSb  r6wgPTrI1dycu5kIWimu+LPlQgRJrGy4dinJJKWNR8IS0GywYW8VhHyY0Hog67/J  uwZdcm3dUEW1WSekNns4Tt9GNhx+zarUIdNpCU6qd8sGCaqum7VWgsDsRTSwVATU  adT8IoxiPiVLn7g/BrTU/M1AKPr6FPJkOdNsKkHDG1xveQYSHwuwH7jNolEvztzN  vDCqh5IUytRsXGPi9QkH  =WdKf  -----END PGP SIGNATURE-----
    Split out isOrContains to public method
    
    UIP-288

---
+++ test/ui_core/util/dom_util_test.dart
@@ -0,0 +1,81 @@
+library dom_util_test;
+
+import 'package:react/react.dart' as react;
+import 'package:react/react_client.dart' show ReactComponentFactory;
+import 'package:test/test.dart';
+import 'package:web_skin_dart/test_util.dart';
+import 'package:web_skin_dart/ui_components.dart';
+import 'package:web_skin_dart/ui_core.dart';
+
+import '../../wsd_test_util/common_component_tests.dart';
+import '../../wsd_test_util/key_down_util.dart';
+
+/// Main entry point for DomUtil testing
+main() {
+  group('isOrContains returns', () {
+    group('true when', () {
+      test('root is the other element', () {
+        var rootNode = renderAndGetDom(Dom.div());
+
+        expect(isOrContains(rootNode, rootNode), isTrue);
+      });
+
+      test('root contains the other element', () {
+        var rootInstance = render(DomTest());
+        var rootNode = findDomNode(rootInstance);
+        var otherNode = findDomNode(getRef(rootInstance, 'innerComponent'));
+
+        expect(isOrContains(rootNode, otherNode), isTrue);
+      });
+    });
+
+    group('false when', () {
+      test('root is null', () {
+        var otherNode = renderAndGetDom(Dom.div()());
+
+        expect(isOrContains(null, otherNode), isFalse);
+      });
+
+      test('other is null', () {
+        var rootNode = renderAndGetDom(Dom.div()());
+
+        expect(isOrContains(rootNode, null), isFalse);
+      });
+
+      test('root and other is null', () {
+        expect(isOrContains(null, null), isFalse);
+      });
+
+      test('root is not, or does not contain, the other element', () {
+        var rootNode = renderAndGetDom(Dom.div()());
+        var otherNode = renderAndGetDom(Dom.div()());
+
+        expect(isOrContains(rootNode, otherNode), isFalse);
+      });
+    });
+  });
+}
+
+DomTestDefinition DomTest() => new DomTestDefinition({});
+
+class DomTestDefinition extends BaseComponentDefinition with HitAreaProps {
+  DomTestDefinition(Map props) : super(_DomTestComponentFactory, props);
+}
+
+ReactComponentFactory _DomTestComponentFactory = react.registerComponent(() => new _DomTest());
+class _DomTest extends BaseComponent<DomTestDefinition> with HitAreaMixin<DomTestDefinition> {
+  @override
+  Map getDefaultProps() => (newProps()
+    ..addProps(HitAreaMixin.defaultProps)
+  );
+
+  @override
+  render() {
+    return Dom.div()(
+      (Dom.div()..ref = 'innerComponent')()
+    );
+  }
+
+  @override
+  DomTestDefinition typedPropsFactory(Map propsMap) => new DomTestDefinition(propsMap);
+}
