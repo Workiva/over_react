65d0e2e24acd4f42348288d06f159e84ef8e8ba3
Greg Littlefield <greg.littlefield@webfilings.com>
2015-12-17 17:16:55 -0700

    Remove outer libraries that were being used to hide .g.dart files
    UIP-726

--- lib/ui_core.dart
+++
@@ -1,5 +0,0 @@
-/// Base classes for UI components and related utilities.
-library web_skin_dart.public.ui_core;
-
-// Importing and exporting this library so we can hide the generated files
-export  'src/ui_core.dart';
