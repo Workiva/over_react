0e37c5640a15e78e9e4f7f3896f79468f3d23d03
Greg Littlefield <greg.littlefield@webfilings.com>
2015-12-17 17:22:25 -0700

    iQIcBAABCAAGBQJYXA5zAAoJEJ5mu3zHWF1PTfgP/jPNn+JGOoKVOs+RpWDQ9jNb  96VZgEjojlyjvUtq0YY4B7fYoX63sZaQxQj5QOkCpa7a2bpaT/5hvu+DauHTTXgf  oLv4oRyhyln36HCRFlueBpDNciGmNW0s77dZbdPM4xcNosdHe2rMM4wENFz0uNZI  jGHn28cc1KKKEoNr83SZjLXszV+IwM8AQo0V6PsVcsdBaiHRtekUlYWsaCSkKVkV  8kfTrfWUNFLk3oWe5STxEBpXI7z9qIyNyp2vw64pgdbp19jCpQtR9tHXhJesuhes  JklWzuCn2hXWC+UpcY3QbU3xK2zFtIJhZPd1mxisVdXCWvWW+PH22QvGP4F/tpTY  1hSUb2dB+zV6J0h8hHZW0zR5wKaMLOUXMeYzUWCDzWinWesmRJ9LJCKYJoLGh8db  s4/lr5NKstRqWk8/OAtheJz7Lewky/2vONd2jphKlbRk2MMdrlBs7gyls4qPFD2l  dd5yiK0I8Ggxd7NQvrjX359KPSM8kY6Ed1gYQLitBBrkVd0FIxBcTlU1mKeSEBhD  G5uWjDHNZnt3Ro6/kjpSJNcIEPhTqb3+kld2EHF5OTOjDxgIyHL08yZ+Ngb2MA7G  sQq53tu13dAn+wqL0z+eNeACCLkkO2w4iQobLDHOCXFWiKXIXD4D+CP3/iQAQL+S  NdkKZgg4AwEXh0xEppEK  =38iC  -----END PGP SIGNATURE-----
    Fix path references, update imports that pointed to inner libraries
    
    UIP-726

--- lib/ui_core.dart
+++ lib/ui_core.dart
@@ -9,31 +9,31 @@ import 'dart:js';
 import 'package:react/react.dart' as react;
 import 'package:react/react_client.dart';
 
-import 'ui_core/component/callback_typedefs.dart';
-import 'ui_core/component/typed_mixins.dart';
-import 'ui_core/component_declaration/component_base.dart' as component_base;
-import 'ui_core/component_declaration/transformer_helpers.dart';
+import 'src/ui_core/component/callback_typedefs.dart';
+import 'src/ui_core/component/typed_mixins.dart';
+import 'src/ui_core/component_declaration/component_base.dart' as component_base;
+import 'src/ui_core/component_declaration/transformer_helpers.dart';
 
 // Exports
-export 'ui_core/component/callback_typedefs.dart';
-export 'ui_core/component/resize_sensor.dart';
-export 'ui_core/component/typed_mixins.dart';
-export 'ui_core/util/character_constants.dart';
-export 'ui_core/util/css_value_util.dart';
-export 'ui_core/util/document_event_helper_util.dart';
-export 'ui_core/util/dom_util.dart';
-export 'ui_core/util/guid_util.dart';
-export 'ui_core/util/handler_chain_util.dart';
-export 'ui_core/util/react_wrappers.dart';
-export 'ui_core/util/validation_util.dart';
-export 'ui_core/component_declaration/flux_component.dart';
-export 'ui_core/component_declaration/transformer_helpers.dart';
+export 'src/ui_core/component/callback_typedefs.dart';
+export 'src/ui_core/component/resize_sensor.dart';
+export 'src/ui_core/component/typed_mixins.dart';
+export 'src/ui_core/util/character_constants.dart';
+export 'src/ui_core/util/css_value_util.dart';
+export 'src/ui_core/util/document_event_helper_util.dart';
+export 'src/ui_core/util/dom_util.dart';
+export 'src/ui_core/util/guid_util.dart';
+export 'src/ui_core/util/handler_chain_util.dart';
+export 'src/ui_core/util/react_wrappers.dart';
+export 'src/ui_core/util/validation_util.dart';
+export 'src/ui_core/component_declaration/flux_component.dart';
+export 'src/ui_core/component_declaration/transformer_helpers.dart';
 
 // Parts
-part 'ui_core/component/aria_mixin.dart';
-part 'ui_core/component/base_component.dart';
-part 'ui_core/component/dom_components.dart';
-part 'ui_core/component/prop_mixins.dart';
-part 'ui_core/util/class_names.dart';
-part 'ui_core/util/event_helpers.dart';
-part 'ui_core/util/map_util.dart';
+part 'src/ui_core/component/aria_mixin.dart';
+part 'src/ui_core/component/base_component.dart';
+part 'src/ui_core/component/dom_components.dart';
+part 'src/ui_core/component/prop_mixins.dart';
+part 'src/ui_core/util/class_names.dart';
+part 'src/ui_core/util/event_helpers.dart';
+part 'src/ui_core/util/map_util.dart';
