09e5237fc3505ff8cdb9925dc60784845ba1c1f9
Greg Littlefield <greg.littlefield@webfilings.com>
2016-01-04 09:56:24 -0700

    Add tests for component type checking utilities
    UIP-680

--- test/ui_core/component_declaration/component_type_checking_test.dart
+++ test/ui_core/component_declaration/component_type_checking_test.dart
@@ -1,5 +1,7 @@
 library ui_core.component_declaration.component_type_checking_test;
 
+import 'dart:js';
+
 import 'package:test/test.dart';
 import 'package:web_skin_dart/src/ui_core/component_declaration/component_type_checking.dart';
 import 'package:web_skin_dart/ui_core.dart';
@@ -11,13 +13,129 @@ import 'component_type_checking_test/two_level_wrapper.dart';
 import 'component_type_checking_test/type_inheritance/parent.dart';
 import 'component_type_checking_test/type_inheritance/subsubtype.dart';
 import 'component_type_checking_test/type_inheritance/subtype.dart';
+import 'package:react/react_client.dart';
 
 main() {
   group('component type checking:', () {
+    group('getComponentTypeFromAlias', () {
+      group('passes through valid component types:', () {
+        test('String', () {
+          expect(getComponentTypeFromAlias('div'), equals('div'));
+        });
+
+        test('JsFunction', () {
+          var jsFunction = new JsFunction.withThis((_) {});
+          expect(getComponentTypeFromAlias(jsFunction), same(jsFunction));
+        });
+      });
+
+      test('returns the JsFunction type for a ReactDartComponentFactoryProxy', () {
+        var jsFunction = new JsFunction.withThis((_) {});
+        var factory = new ReactDartComponentFactoryProxy(jsFunction);
+        expect(getComponentTypeFromAlias(factory), same(jsFunction));
+      });
+
+      test('returns the String type for a ReactDomComponentFactoryProxy', () {
+        var factory = new ReactDomComponentFactoryProxy('div');
+        expect(getComponentTypeFromAlias(factory), equals('div'));
+      });
+
+      test('returns the JsFunction type for an aliased ReactDartComponentFactoryProxy', () {
+        var jsFunction = new JsFunction.withThis((_) {});
+        var factory = new ReactDartComponentFactoryProxy(jsFunction);
+
+        var typeAlias = new Object();
+        registerComponentTypeAlias(factory, typeAlias);
+
+        expect(getComponentTypeFromAlias(typeAlias), same(jsFunction));
+      });
+
+      test('returns null for an unregistered/invalid type alias', () {
+        var unregisteredTypeAlias = new Object();
+        expect(getComponentTypeFromAlias(unregisteredTypeAlias), isNull);
+      });
+
+      group('returns null and does not throw when passed', () {
+        var type;
+
+        test('null', () {
+          expect(() {
+            type = getComponentTypeFromAlias(null);
+          }, isNot(throws));
+
+          expect(type, isNull);
+        });
+
+        test('a number', () {
+          expect(() {
+            type = getComponentTypeFromAlias(2);
+          }, isNot(throws));
+
+          expect(type, isNull);
+        });
+
+        test('a bool', () {
+          expect(() {
+            type = getComponentTypeFromAlias(true);
+          }, isNot(throws));
+
+          expect(type, isNull);
+        });
+      });
+    });
+
+    group('getParentTypes', () {
+      group('getParentTypes', () {
+        group('returns an iterable of the parent types', () {
+          setUpAll(() {
+            // Force lazy-instantiation of factory variables so that component class type aliases
+            // are registered when we read them in the `getComponentTypeFromAlias` calls.
+            TestParent()();
+            TestSubtype()();
+            TestSubsubtype()();
+          });
+
+          test('that is empty for a component without parent types', () {
+            expect(getParentTypes(getComponentTypeFromAlias(TestParent)), isEmpty);
+          });
+
+          test('that contains a component\'s parent type', () {
+            expect(
+                getParentTypes(getComponentTypeFromAlias(TestSubtype)),
+                orderedEquals([
+                  getComponentTypeFromAlias(TestParent),
+                ])
+            );
+          });
+
+          test('that contains all of a component\'s parent types', () {
+            expect(
+                getParentTypes(getComponentTypeFromAlias(TestSubsubtype)),
+                orderedEquals([
+                  getComponentTypeFromAlias(TestSubtype),
+                  getComponentTypeFromAlias(TestParent),
+                ])
+            );
+          });
+        });
+
+        test('asserts that the input is a valid component type', () {
+          expect(() {
+            // Fully iterate the result by calling toList().
+            getParentTypes(new Object()).toList();
+          }, throwsA(const isInstanceOf<AssertionError>()));
+        }, testOn: 'dart-vm');
+      });
+    });
+
     group('isComponentOfType()', () {
       group('returns expected result when given', (){
         test('null', () {
           expect(isComponentOfType(null, TestA), isFalse);
+        });
+
+        test('a component and an invalid/unregistered type alias', () {
+          expect(isComponentOfType(TestA()(), new Object()), isFalse);
         });
 
         test('a component and its factory', () {
@@ -32,7 +150,7 @@ main() {
           expect(isComponentOfType(TestA()(), TestAComponent), isTrue);
         });
 
-        test('a component and its canonical type', () {
+        test('a component and its component type', () {
           expect(isComponentOfType(TestA()(), TestA()()['type']), isTrue);
         });
 
@@ -48,7 +166,7 @@ main() {
           expect(isComponentOfType(TestA()(), TestBComponent), isFalse);
         });
 
-        test('a component and a canonical type for a different component', () {
+        test('a component and a component type for a different component', () {
           expect(isComponentOfType(TestA()(), TestB()()['type']), isFalse);
         });
 
