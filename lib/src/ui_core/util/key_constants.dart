c36b14f5481b3759454c6510ae8bee541b5966fa
Jace Hensley <jace.hensley@workiva.com>
2016-01-06 09:57:45 -0600

    Add key value constants to ui_core for keyboard event handling
    UIP-694

---
+++ lib/src/ui_core/util/key_constants.dart
@@ -0,0 +1,48 @@
+library key_constants;
+
+/// Key values that are returned from [KeyboardEvent.key].
+///
+///See: <https://w3c.github.io/uievents/#h-fixed-virtual-key-codes>.
+abstract class KeyValue {
+  static const String BACKSPACE = 'Backspace';
+  static const String TAB = 'Tab';
+  static const String ENTER = 'Enter';
+  static const String SHIFT = 'Shift';
+  static const String CONTROL = 'Control';
+  static const String ALT = 'Alt';
+  static const String CAPS_LOCK = 'CapsLock';
+  static const String ESC = 'Escape';
+  static const String SPACE  = ' ';
+  static const String PAGE_UP = 'PageUp';
+  static const String PAGE_DOWN = 'PageDown';
+  static const String END = 'End';
+  static const String HOME = 'Home';
+  static const String ARROW_LEFT = 'ArrowLeft';
+  static const String ARROW_UP = 'ArrowUp';
+  static const String ARROW_RIGHT = 'ArrowRight';
+  static const String ARROW_DOWN = 'ArrowDown';
+  static const String DELETE = 'Delete';
+
+
+  /// All of the [KeyValue] values.
+  static const List<String> values = const <String>[
+    BACKSPACE,
+    TAB,
+    ENTER,
+    SHIFT,
+    CONTROL,
+    ALT,
+    CAPS_LOCK,
+    ESC,
+    SPACE,
+    PAGE_UP,
+    PAGE_DOWN,
+    END,
+    HOME,
+    ARROW_LEFT,
+    ARROW_UP,
+    ARROW_RIGHT,
+    ARROW_DOWN,
+    DELETE
+  ];
+}
