28b33451f562c4e208e15f97b7641c1cadd12b67
Aaron Lademann <aaron.lademann@workiva.com>
2016-01-15 08:21:13 -0700

    Add getFormElements top-level method
    + Intended for use within the `onSubmit` callback of a `Form()` component.

--- lib/src/ui_core/util/dom_util.dart
+++ lib/src/ui_core/util/dom_util.dart
@@ -1,6 +1,9 @@
 library dom_util;
 
 import 'dart:html';
+import 'dart:js';
+
+import 'js_util.dart';
 
 /// Returns whether [root] is the same as or contains the [other] node.
 ///
@@ -9,3 +12,13 @@ bool isOrContains(Element root, Element other) => (
   (root != null && other != null) &&
   (root == other || root.contains(other))
 );
+
+/// Returns the JS [HTMLFormElement](https://developer.mozilla.org/en-US/docs/Web/API/HTMLFormElement)
+JsObject getJsForm(FormElement form) => new JsObject.fromBrowserObject(form);
+
+/// Returns a [List] of all of the form control [Element]s contained in a given [form].
+///
+/// Does not include `<input type="image">` controls.
+///
+/// Wraps the [HTMLFormElement.elements](https://developer.mozilla.org/en-US/docs/Web/API/HTMLFormElement/elements) property.
+List<Element> getFormElements(FormElement form) => convertToArray(getJsForm(form)['elements']);
