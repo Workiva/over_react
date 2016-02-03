1bdbb15efa50d06bd30561266623ec900fde78f5
Aaron Lademann <aaron.lademann@workiva.com>
2016-02-03 11:17:27 -0700

    Move unindent out of doc utils
    + Since it has valid applications in places other than just the docs.

--- lib/ui_core.dart
+++ lib/ui_core.dart
@@ -27,6 +27,7 @@ export 'src/ui_core/util/handler_chain_util.dart';
 export 'src/ui_core/util/js_util.dart';
 export 'src/ui_core/util/key_constants.dart';
 export 'src/ui_core/util/react_wrappers.dart';
+export 'src/ui_core/util/string_util.dart';
 export 'src/ui_core/util/validation_util.dart';
 export 'src/ui_core/component_declaration/flux_component.dart';
 export 'src/ui_core/component_declaration/transformer_helpers.dart';
