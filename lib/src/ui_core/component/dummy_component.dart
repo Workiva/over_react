ade6545fcc10af84a8bc80264d65f5ce66995969
Jace Hensley <jace.hensley@workiva.com>
2016-02-05 16:41:26 -0700

    Remove unnecessary file
    UIP-661

--- lib/src/ui_core/component/dummy_component.dart
+++
@@ -1,9 +0,0 @@
-library dummy_component;
-
-import 'package:react/react.dart' as react;
-
-/// Dummy component to be used when needing to type check a sub component of an abstract super component.
-class DummyComponent extends react.Component {
-  @override
-  render() {}
-}
