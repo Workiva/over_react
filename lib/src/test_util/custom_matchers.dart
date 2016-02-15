c9c2c2fb9e49055f6b00a2a06cee95ad554d0b34
Greg Littlefield <greg.littlefield@webfilings.com>
2016-02-15 15:23:33 -0700

    Fix low-hanging dart2js test issues
    UIP-755

--- lib/src/test_util/custom_matchers.dart
+++ lib/src/test_util/custom_matchers.dart
@@ -160,7 +160,7 @@ class _IsFocused extends Matcher {
         ..add('is not a valid Element.');
     }
 
-    if (!document.contains(item as Element)) {
+    if (!document.documentElement.contains(item)) {
       return mismatchDescription
         ..add('is not attached to the document, and thus cannot be focused.')
         ..add(' If testing with React, you can use `renderAttachedToDocument`.');
@@ -168,12 +168,13 @@ class _IsFocused extends Matcher {
 
     mismatchDescription.add('is not focused; ');
 
-    if (matchState['activeElement'] == document.body) {
+    final activeElement = matchState['activeElement'];
+    if (activeElement is! Element || activeElement == document.body) {
       mismatchDescription.add('there is no element currently focused');
     } else {
       mismatchDescription
         ..add('the currently focused element is ')
-        ..addDescriptionOf(matchState['activeElement']);
+        ..addDescriptionOf(activeElement);
     }
 
     return mismatchDescription;
