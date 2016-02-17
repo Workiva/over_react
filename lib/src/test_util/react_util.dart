d69f29da6f597dbaf060a2fbd357cbb8ec196eb3
Jace Hensley <jace.hensley@workiva.com>
2016-02-17 14:14:08 -0700

    Add missing doc comment
    UIP-844

--- lib/src/test_util/react_util.dart
+++ lib/src/test_util/react_util.dart
@@ -102,6 +102,7 @@ JsObject getRef(JsObject instance, dynamic ref) {
 /// Helper function to simulate clicks
 void click(dynamic node) => context['React']['addons']['TestUtils']['Simulate'].callMethod('click', [node]);
 
+/// Helper function to simulate focus events.
 void focus(dynamic instanceOrNode) => react_test_utils.Simulate.focus(instanceOrNode);
 
 /// Helper function to simulate mouseMove events.
