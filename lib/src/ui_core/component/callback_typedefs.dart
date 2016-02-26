1d240e2137be2d021cb62d9265d5a0cd59ed0d86
Claire Sarsam <claire.sarsam@workiva.com>
2016-02-26 14:30:12 -0700

    iQIcBAABCAAGBQJYXA9uAAoJEJ5mu3zHWF1PGaIP/jjzNkNQ4pFJwvw5qWNASkvn  0XCJ6A41U8o4qD5Bafjo6HE/IR5EbKjv9I5lwbNjo/6bHqVeqkoDpOxwjucxYyft  QTCmT4NxYjDY4Wlhsbu+4OKcYWsUOzRMpbRyqOR+WNudUlTcJHxFWXoU4usOsFcN  6Vm1qK62XnvGg2iZbLT/DVqJm0gHEFjd/Ul0G6iQCg6BAMTm10qUlShs7cl+n3Vy  2GLNaRFB+oA/z6b1gkeyvVDkwti/U9Mw7NRWOhcMAHSIumrXalhIdaKMiUJhk6oZ  YEctdf1UWwXrtbh2fcNuqZ8AgEENrbUe7RVUbTHFzn1Vsro0Ukp3PLBla0OBdFPk  20YJ18S0t9PyzemLZBCCMogLGysvJQ7qfnAy1R+QikFDsAst5FYgebJ/v9IwmWeZ  OL8acPZUH3H1rJYKGhdOMI6BPb7q2q/gr0rtezJ5Cu5+76BFGlYoTJOZ1xRnbaaP  MNd+9kfv+LwnKRRnwBZTEYB/tG9oZHD7m04SnyvUu1KM68Y5MbFR4N0B8jeWqeu0  v9ZBHOLL8Ta+n8q7HJ1VWiZhhCd8SFtI4Y+HAVEe9BiO1I4FgjpdddTz11MMYSjF  0QE2ujOYehY67CmNBGpnOciwHya9v2EhcQI6pMNPp99h6WkGO+nTzBneYR+9gjYs  mRSJqqX/d8ToLQYdoJYs  =YW55  -----END PGP SIGNATURE-----
    Add EventKeyIndexCallback typedef
    
    UIP-848

--- lib/src/ui_core/component/callback_typedefs.dart
+++ lib/src/ui_core/component/callback_typedefs.dart
@@ -18,6 +18,7 @@ typedef WheelEventCallback(react.SyntheticWheelEvent event);
 // Callback for item selection events
 typedef EventKeyCallback(react.SyntheticEvent event, Object eventKey);
 typedef IndexCallback(react.SyntheticEvent event, int index);
+typedef EventKeyIndexCallback(react.SyntheticEvent event, Object eventKey, int index);
 
 // Callback for state changes
 typedef StateDidChangeCallback(react.SyntheticEvent event, Map oldState);
