c9fe36e952b79b3fa2578251caade23ea5b508c6
Claire Sarsam <claire.sarsam@workiva.com>
2016-02-29 14:31:56 -0700

    Add InvalidPropValueError and rename required_props_error.dart
    UIP-848

--- lib/ui_core.dart
+++ lib/ui_core.dart
@@ -33,7 +33,7 @@ export 'src/ui_core/util/string_util.dart';
 export 'src/ui_core/util/validation_util.dart';
 export 'src/ui_core/component_declaration/flux_component.dart';
 export 'src/ui_core/component_declaration/transformer_helpers.dart';
-export 'src/ui_core/util/required_props_error.dart';
+export 'src/ui_core/util/prop_errors.dart';
 
 // Parts
 part 'src/ui_core/component/aria_mixin.dart';
