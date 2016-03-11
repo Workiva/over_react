53554564f12221a7c0a15b1908e89338245bb646
dianachen-wf <diana.chen@workiva.com>
2016-03-11 11:23:54 -0700

    Revert "Merge remote-tracking branch 'Workiva/0.2.3-wip' into UIP-920_navbar-no-brand"
    This reverts commit c25d868cacc2bde392680d446cba3998db2c5f34, reversing
    changes made to 2f7105a9e3a6af9a11f864b85290a96865f4c51d.

---
+++ lib/src/ui_core/util/required_props_error.dart
@@ -0,0 +1,9 @@
+library ui_core.required_props_error;
+
+class RequiredPropsError extends Error {
+  final String message;
+
+  RequiredPropsError(this.message);
+
+  String toString() => 'RequiredPropsError: $message';
+}
