4bb84c4c835ac17272efae4481b0d07fe76fcd34
Jace Hensley <jace.hensley@workiva.com>
2016-03-14 11:17:54 -0700

    Add a way to type check components that extend abstract components
    UIP-852

---
+++ lib/src/ui_core/component/dummy_component.dart
@@ -0,0 +1,9 @@
+library dummy_component;
+
+import 'package:react/react.dart' as react;
+
+/// Dummy component useful for:
+/// - Allowing sub-typing of components.
+class DummyComponent extends react.Component{
+  render() => false;
+}
