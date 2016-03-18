fe9467a4ad313b7199e41ccc68fc4a1ad2e7c84b
Greg Littlefield <greg.littlefield@webfilings.com>
2016-03-18 14:24:39 -0700

    iQIcBAABCAAGBQJYXA8sAAoJEJ5mu3zHWF1P6kMP/3wWC0dKN4hzgxjx28KrkQxE  pjLYXmOVkmogVnjml5LuL5piVJzm/0IWVLEXU65n6p7Av+c5TOvHm4jdxdvXgFFv  /jDHYYHtrMeal53OvM1f7CewEXeL8rTAC8yrmAnI/3Bww821ACg90AeQvSI7CEmO  Bwp3QPBZfaySmw2MyCVfSkkGzWD56nAOMn2Gfq5k/AQFr0/muRsx3f4YdUp1mmMO  pWShdpOi94hf2gpBwsxSzxnPIl/xAOrGqbFdgMCKKKoCT37gnWOMDWx9T1olqjJX  AKYOXWizB+kDBAPm0tESW/dScJ9EPlCICMCGklSx79I3i6GcaUmE94hhu9nmyx+a  qkK1vp3NxqV8j87+OrbMsvVWg5yRxsbGvPow7sXJxTaS2qWyHhqfoSvTwzGvPebd  s4XHinIi93JVqdvukBJvA0rgWCS710d897GuwYwjI8jBg5lvoabFzdB+lnBvUl25  1cuxVO1iLc0L/vls2ra0U6MChAnXEJA4WOfVTl51Y2tpCcwjxLVVBPueJV45dtGI  oymIdjDidWVYOmZTxBFwsq5Bc4VTbn/bmgeAW3x6vaLnEnbAganxvyUSD7SsjJtE  53rtZiKdSvHcSipn7iwrBtiaEOKNKLlvDB0CZpNYjSgeRtbEZnduLDA+C75NTUvG  MTv7MOJ0tiwgvwUo7ktg  =HLML  -----END PGP SIGNATURE-----
    Reduce # of args in UiProps.call until dart2js issue is fixed
    
    See: https://github.com/dart-lang/sdk/pull/26032
    
    UIP-774

--- lib/src/ui_core/component_declaration/component_base.dart
+++ lib/src/ui_core/component_declaration/component_base.dart
@@ -281,7 +281,8 @@ abstract class UiProps
   /// (alias for [build] with support for variadic children)
   ///
   /// This method actually takes any number of children as arguments ([c2], [c3], ...) via [noSuchMethod].
-  ReactElement call([children, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13, c14, c15, c16, c17, c18, c19, c20, c21, c22, c23, c24, c25, c26, c27, c28, c29, c30, c31, c32, c33, c34, c35, c36, c37, c38, c39, c40, c41, c42, c43, c44, c45, c46, c47, c48, c49, c50]);
+  // Restrict to 40 arguments until the dart2js fix in https://github.com/dart-lang/sdk/pull/26032 is released.
+  ReactElement call([children, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13, c14, c15, c16, c17, c18, c19, c20, c21, c22, c23, c24, c25, c26, c27, c28, c29, c30, c31, c32, c33, c34, c35, c36, c37, c38, c39, c40]);
 
   /// Supports variadic children of the form `call([child1, child2, child3...])`.
   dynamic noSuchMethod(Invocation invocation) {
