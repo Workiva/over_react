46000872e2a229d2d421ab47c3b38afdae91aaee
Claire Sarsam <claire.sarsam@workiva.com>
2016-03-28 14:18:58 -0700

    iQIcBAABCAAGBQJYXA9NAAoJEJ5mu3zHWF1PfFEP/RyxGqO1PR2+ss2fV9NVDQlj  h7w+OEhUv9Cfxb7blHgzFlkutRgkL8NHcyYyxpylKzQoq4Myb8ZZbTOJgbS/s88i  Mu9xw6ZQjbxv2niQ+nB8mh/Sur4YaKAxBm+6jfkb7DhY9dlYRzn0VXQtufEgTNUz  9HmRZgAzaCp9j3r1M9q/Mlv/bSxZ5H9kv80MBPh644N4YDRfOR4okH3QOX9k3KM3  H5e3zO/DgOA1ojHIMIRJbpLBBtxnO06lfPLoB8rvQrH/JO0Ly1njuUg7UXm1CbMv  FP3V3m9TJQfxNcm0K/WLnGrjVO5fBHJfGq/XQHMrxnrhuzQ0P7nbfkfSQaVWO18B  EqKOhZSJzarkMe2//JdTdf2u8zPbfDom98jG4MRL8p3DTbDqS19XBMumbsIeDV8H  dW+FGy0O/hSC/s2BW2+g4z3zyR3qbDBGIXBgAc+CfF45+Ojp7Xf/On5GVjKWpHTV  lBLDqlG4AaWLNPD58GRiLAd2NFnEpWsJcBMPfIqlQJkpAnZLT/gs6QutwhnCYFbh  e0FOId+gREaLiWSU882dniSE9SH/yqDWL1meo+21zFqLBzXx/BVUGdOrCqbkPsEJ  oq9TC4CUdZoAxj2E9vW2dUFbX+NlsaNOMkVLkH4E65uRIZxMDVsCIO4r08PjbP+w  8rnm/5Mr3hGXFZ6BA8IM  =Lpla  -----END PGP SIGNATURE-----
    Add analyze config to integration dart.dev
    
    UIP-882

--- integrate/tool/dev.dart
+++ integrate/tool/dev.dart
@@ -3,6 +3,11 @@ library tool.dev;
 import 'package:dart_dev/dart_dev.dart' show dev, config;
 
 main(List<String> args) async {
+  config.analyze
+    ..strong = true
+    ..fatalWarnings = true
+    ..entryPoints = ['web/'];
+
   config.test
     ..pubServe = true
     ..platforms = [
