17a9a2b1ea04e891a4cab77c082d0d0bcfeba447
Claire Sarsam <claire.sarsam@workiva.com>
2016-03-29 10:50:54 -0700

    Remove needed imports of typedefs that are now redeclared
    UIP-882

--- lib/src/ui_core/component_declaration/component_base.dart
+++ lib/src/ui_core/component_declaration/component_base.dart
@@ -16,9 +16,7 @@ import 'package:web_skin_dart/ui_core.dart' show
     getProps,
     getPropsToForward,
     isDartComponent,
-    isValidElement,
-    getDomNodeTypeef,
-    refTypedef;
+    isValidElement;
 
 export 'package:web_skin_dart/src/ui_core/component_declaration/component_type_checking.dart' show isComponentOfType, isValidElementOfType;
 
