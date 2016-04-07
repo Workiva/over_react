7ba3ad0490e1abf3d9881a2b227f9ffb859e95d5
Joel Leibow <joel.leibow@workiva.com>
2016-04-07 12:09:43 -0700

    iQIcBAABCAAGBQJYXA9VAAoJEJ5mu3zHWF1PHdsQAMnzc4f9O+m74OIUmLNgiDpL  TmuJof5XuA0afxd7TDnNR5oF65d893mly2/oE5zynjEpSz//2dTYA+VKgcMSqLuO  PoQOrBO/ePaJp0jxiDlkCpG6fYLhwGtiNLLYYxnfb/9pn6aBRtdaMCJ27zZnJIBD  AgT08MDzr41CoEPNw2M1UwqsoHDg2U4dNyTRniqeZOI2sgfh/Wz2LsClzRpxUqpp  fcvQfUILm/U/r0y/Qb4V9q329BIgvoYWKTOOG4U1yfxJo1HNa/ZMwYn+0TFaCdYA  v3FU+xUR0mkqpuFQFXXTZBL107+R5QEXlP81C74mKOKEpw7N1b03BXyO4MyGXP/X  awGQry04seeeyyCqjk6u9Y8KnKuaf/CjP+IFai4MbsVXOjcxINTmXJ34VGNbPQ+c  k4DrjME2vZBvlltjae0GGay+dVa+AptAnJxBwxgIiDHoOvfJtAiSkcWsY0cAVN6L  1bNikV9j6/DEtk6hHGrB9a75BgczOqK+5KBllkQV3tsAK6j1GIaGx0J5oolQ8kfp  IeNdvjEW/6WYtb08Yi5EgIGaiRn967F4zCmVOGknN+XvdeCTo3Mg/O0fpdDnxoCr  p56sn2JMnHVdtlOdoXph4FuG79tdShDSrkX67E8WIsCr5bfoSDv321YwZYRMM6sN  Bx1doVgYFf6h7eXAKHwr  =cffu  -----END PGP SIGNATURE-----
    All modifying methods overrode and stubbing of tests
    
    UIP-915

--- test/ui_core/util/warn_on_modify_props_test.dart
+++ test/ui_core/util/warn_on_modify_props_test.dart
@@ -3,7 +3,55 @@ library warn_on_modify_props_test;
 import 'package:test/test.dart';
 
 main() {
-  group('WarnOnModifyMap', () {
+  group('WarnOnModifyProps: ', () {
+    group('[]= operator override', () {
+      test('issues a warning on usage', () {
 
+      });
+
+      test('adds the new key to the props', () {
+
+      });
+    });
+
+    group('addAll', () {
+      test('issues a warning on usage', () {
+
+      });
+
+      test('adds new keys to the props', () {
+
+      });
+    });
+
+    group('clear', () {
+      test('issues a warning on usage', () {
+
+      });
+
+      test('removes all keys from the props', () {
+
+      });
+    });
+
+    group('putIfAbsent', () {
+      test('issues a warning on usage', () {
+
+      });
+
+      test('adds the new key to the props', () {
+
+      });
+    });
+
+    group('remove', () {
+      test('issues a warning on usage', () {
+
+      });
+
+      test('removes the provided key from the props', () {
+
+      });
+    });
   });
 }
