02f1df59dfcb5ad3d687a0e249714f1754078906
Greg Littlefield <greg.littlefield@webfilings.com>
2016-04-15 15:00:47 -0700

    Fix typo in DocumentEventHelper.document's type
    UIP-1017

--- lib/src/ui_core/util/document_event_helper_util.dart
+++ lib/src/ui_core/util/document_event_helper_util.dart
@@ -4,11 +4,11 @@ import 'dart:html' as html;
 
 /// Utility class that helps with dependency injection when mocking document events.
 abstract class DocumentEventHelper {
-  static html.Document _document = html.document;
+  static html.HtmlDocument _document = html.document;
   /// Singleton that can be used to listen to events like you would with `document`.
   ///
   /// Can be mutated when testing
-  static html.Document get document => _document;
+  static html.HtmlDocument get document => _document;
   static set document(html.Document value) {
     // TODO add `.fromEnvironment` check for warning when not testing.
     _document = value;
