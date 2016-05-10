c58ea98da30339fadf71205cd954ade5e94a3268
Greg Littlefield <greg.littlefield@webfilings.com>
2016-05-10 16:30:46 -0700

    Add parameter to ReactElement return value for better strong mode support

--- lib/src/ui_core/component/base_component.dart
+++ lib/src/ui_core/component/base_component.dart
@@ -96,7 +96,7 @@ abstract class ComponentDefinition extends MapView implements Function {
   }
 
   /// Create a new component with this builder's props and the specified children.
-  ReactElement build([dynamic children]) {
+  ReactElement<react.Component> build([dynamic children]) {
     assert(validate());
     return componentFactory(props, children);
   }
@@ -106,7 +106,7 @@ abstract class ComponentDefinition extends MapView implements Function {
   /// (alias for [build] with support for variadic children)
   ///
   /// This method actually takes any number of children as arguments ([c2], [c3], ...) via [noSuchMethod].
-  ReactElement call([children, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13, c14, c15, c16, c17, c18, c19, c20, c21, c22, c23, c24, c25, c26, c27, c28, c29, c30, c31, c32, c33, c34, c35, c36, c37, c38, c39, c40]);
+  ReactElement<react.Component> call([children, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13, c14, c15, c16, c17, c18, c19, c20, c21, c22, c23, c24, c25, c26, c27, c28, c29, c30, c31, c32, c33, c34, c35, c36, c37, c38, c39, c40]);
 
   @override
   dynamic noSuchMethod(Invocation invocation) {
