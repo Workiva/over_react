5ba5037e83120afd301381c35a9b04624624d940
Jace Hensley <jace.hensley@workiva.com>
2016-07-05 12:07:59 -0700

    iQIcBAABCAAGBQJYXBAfAAoJEJ5mu3zHWF1Pm24P/Ri16Ay0U95G7hA+LbjZk4E/  VdZcJn9gol7xyeR8U/ykM5rhooNkm6XrP+fz6VKhqq1Ph1+HkmA55iQ/CLefMmrW  h7Rl6LwY2+2Lqn4PTVkj0Nl2+YNKxdyCkwD2DOhq71TrgqfDZXJGvYJKCZoASJ4N  X3kPootM6nnnqKYynuCJ56NWa126JBcTOUXurfNpsTyeWEWHRBEPu5eERH+cqStT  ofO6tr3p25EQM9GL8OKX4ozcSW8HqwO1LSvUpDNSGSVKg24U4eoi9f8UQzfpl+MI  WEHGy8joqAQRbinuA+SGCGunMwudO57p+u9QrHe8oYLy6IpbtmAuSweSniHxB8g5  x4eJIelK32hR/YllurTSilmD4XPsAAr470iZyIZjxO5Komvy/27U1vPl9ql0EwWM  UY0em/ZYGeO0K/TFDuM6oQSuKQg57cjuu5y+hwvaXU6J/vx3SCYqki/SpBddyB9+  /ychkGwHgoNvoyUVBdZr4691abNCHcRdj/dWsLRIYz9wWZGPTObLlC89wo2A4f7E  fZoTACEjf2HNp1Lx99hjYWlcjgnYTVmFpG4XFHG7Q5Pox3GkB9uc3ZoZeNAfHkxC  tE0mqfBFuXAuQeQ72Bmpvu2D/bQiZdJir/Y1AybjCPWM8czhC6nwTizDdmTRmDcY  mvTFCknMQRH/VnFuhuMR  =XRqk  -----END PGP SIGNATURE-----
    Add easy way to forward only DOM props
    
    UIP-1296

--- lib/src/ui_core/util/map_util.dart
+++ lib/src/ui_core/util/map_util.dart
@@ -4,7 +4,7 @@ part of web_skin_dart.ui_core;
 /// in addition to any specified keys.
 ///
 /// Useful for prop forwarding.
-Map getPropsToForward(Map props, {bool omitReactProps: true, Iterable keysToOmit, Iterable<Iterable> keySetsToOmit}) {
+Map getPropsToForward(Map props, {bool omitReactProps: true, bool onlyCopyDomProps: false, Iterable keysToOmit, Iterable<Iterable> keySetsToOmit}) {
   Map propsToForward = new Map.from(props);
 
   if (omitReactProps) {
@@ -25,6 +25,16 @@ Map getPropsToForward(Map props, {bool omitReactProps: true, Iterable keysToOmit
       keySet.forEach((key) {
         propsToForward.remove(key);
       });
+    });
+  }
+
+  if (onlyCopyDomProps) {
+    new List.from(propsToForward.keys).forEach((String key) {
+      if (const $PropKeys(DomPropsMixin).contains(key)) return;
+      if (const $PropKeys(ReactPropsMixin).contains(key)) return;
+      if (key.startsWith(new RegExp('data-|aria-'))) return;
+
+      propsToForward.remove(key);
     });
   }
 
