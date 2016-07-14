f6905f1cba397729a8c92baced214118eeb7bf42
Aaron Lademann <aaron.lademann@workiva.com>
2016-07-13 17:30:21 -0700

    Address dart analyzer messages

--- test/wsd_test_util/mock_classes.dart
+++ test/wsd_test_util/mock_classes.dart
@@ -32,9 +32,6 @@ import 'package:react/react.dart' as react;
 typedef void _TimerCallback();
 
 class MockTimer extends Mock implements Timer {
-  @override
-  noSuchMethod(i) => super.noSuchMethod(i);
-
   bool _isInstantiated = false;
   _TimerCallback _callback;
   Duration _duration;
@@ -66,20 +63,11 @@ class MockTimer extends Mock implements Timer {
   }
 }
 
-class MockFileList extends Mock implements FileList {
-  @override
-  noSuchMethod(i) => super.noSuchMethod(i);
-}
+class MockFileList extends Mock implements FileList {}
 
-class MockFile extends Mock implements File {
-  @override
-  noSuchMethod(i) => super.noSuchMethod(i);
-}
+class MockFile extends Mock implements File {}
 
-class MockFileUploadInputElement extends Mock implements FileUploadInputElement {
-  @override
-  noSuchMethod(i) => super.noSuchMethod(i);
-}
+class MockFileUploadInputElement extends Mock implements FileUploadInputElement {}
 
 class MockSyntheticEvent extends Mock implements react.SyntheticEvent {}
 
