9ae91491b5c5a827cbc63292eaa12bfc2fdf58e3
Greg Littlefield <greg.littlefield@webfilings.com>
2016-08-15 18:08:16 -0700

    iQIcBAABCAAGBQJYXBA8AAoJEJ5mu3zHWF1PGEAQALkMZjI9ITs5q4hqLiNWh6vY  AvscWk6vD9vuNl13HVN9MeKJCrtraQr/iVlXYandLAuCnO1dtDU+6yi/Q4aHTOuC  EOIagfmoWrt+F4P7hhx7rl4pPryJiuyJajG9/R2inD/q+vgSzOINe92THal6LQWY  q2buN0j43hr+Y2/HD5q0srzIOjJq5bWDoIa0ttMT6v23qWWrPV+SWTPAmSTAY7ug  EM0uCFcAa97PVzli0KxJXgksKUutV0QbcNRUz3rIDRjJwfEvdDVXD05fGDEUcNi5  69CABWims9XP6Nvt47ix+RK1NGpB4k6MlfOcuvIydaAGGsq4EBwypUC98OjbYChp  u5SCG9M8wQwQ5iuW+BNxDlEZYUphQIyqIQwo/5EshnAERsskERhwJpL6CkVJbFYk  qRutR1Q6tG8xsj6R2yuOV+qMTROIi9JeqWcJ2UhtucB+SFnSpl7hED45EMi3LcvL  gVGowo4CgggpI0gdZ7eOLolvH1ynkJZuL+d+1DDk8pNrn24mm4AEF9MYqXjfB8Lj  2pM9AmAv+Qqir5dAA5M5wN64jkJNobtxPzJxsgrfWqUaa6H2nNa2J5j7L6ajTnRU  tf8LaHi/Yf0eJu+OgYoApUEdvAA2eeA4C5551OCtEl1NZXABiMmt0KCEGTpBzUy6  wMypzFUTUfKEZ72lwhY5  =Ah2c  -----END PGP SIGNATURE-----
    Add pretty printing for props/state maps
    
    UIP-842

---
+++ lib/src/ui_core/util/pretty_print.dart
@@ -0,0 +1,107 @@
+library web_skin_dart.ui_core.pretty_print;
+
+/// Returns a pretty-printed version of [map], with namespaced keys grouped together.
+///
+/// Useful for debugging props/state maps (and build in to [UiProps.toString]/[UiState.toString]).
+///
+/// __Only for use when debugging.__
+///
+/// Example:
+///
+///     var map = {'foo': 'bar'};
+///     Foo(map)
+///       ..bar = 1
+///       ..baz = 2;
+///     Bar(map)
+///       ..foo = 1
+///       ..baz = 2;
+///
+///     assert(map.toString == '{foo: bar, Foo.bar: true, Foo.baz: false, Bar.foo: 1, Bar.baz: 2}');
+///     assert(prettyPrintMap(map) == '''
+///     {
+///       Foo…
+///         .bar: true,
+///         .baz: false,
+///
+///       Bar…
+///         .foo: 1,
+///         .baz: 2,
+///
+///       foo: bar
+///     }
+///     '''.trim());
+///
+String prettyPrintMap(Map map) {
+  return _prettyObj(map);
+}
+
+const String _indent = '  ';
+const int _maxKeyValuePairsPerLine = 1;
+const int _maxListItemsPerLine = 4;
+
+/// Indents [str] by [_indent], trimming any trailing whitespace.
+String _indentString(String str) {
+  return str.split('\n').map((line) => (_indent + line).trimRight()).join('\n');
+}
+
+String _prettyObj(Object obj) {
+  if (obj is List) {
+    var items = obj.map(_prettyObj).toList();
+
+    if (items.length > _maxListItemsPerLine || items.any((items) => items.contains('\n'))) {
+      var inner = _indentString(items.join(',\n'));
+      return '[\n$inner\n]';
+    } else {
+      var inner = items.join(', ');
+      return '[$inner]';
+    }
+  } else if (obj is Map) {
+    final namespacedKeys = <String, List<String>>{};
+    final otherKeys = [];
+
+    obj.keys.forEach((dynamic key) {
+      const namespaceSeparator = '.';
+      if (key is String && key.contains(namespaceSeparator)) {
+        var index = key.indexOf(namespaceSeparator);
+        var namespace = key.substring(0, index);
+        var subkey = key.substring(index);
+
+        namespacedKeys[namespace] ??= <String>[];
+        namespacedKeys[namespace].add(subkey);
+      } else {
+        otherKeys.add(key);
+      }
+    });
+
+    final pairs = <String>[];
+
+    pairs.addAll(namespacedKeys.keys.map((String namespace) {
+      String renderSubKey(String subkey) {
+        var key = '$namespace$subkey';
+        var value = obj[key];
+
+        return '$subkey: ' + _prettyObj(value);
+      }
+
+      Iterable<String> subkeys = namespacedKeys[namespace];
+
+      return '$namespace…\n' + _indentString(subkeys.map(renderSubKey).map((pair) => pair + ',\n').join());
+    }));
+
+    pairs.addAll(otherKeys.map((dynamic key) {
+       return '$key: ' + _prettyObj(obj[key]) + ',';
+    }));
+
+    final RegExp trailingComma = new RegExp(r'\s*,\s*$');
+
+    if (pairs.length > _maxKeyValuePairsPerLine || pairs.any((pair) => pair.contains('\n'))) {
+      var inner = _indentString(pairs.join('\n')).replaceFirst(trailingComma, '');
+      return '{\n$inner\n}';
+    } else {
+      var inner = pairs.join(' ').replaceFirst(trailingComma, '');
+      return '{$inner}';
+    }
+  } else {
+    return obj.toString();
+  }
+}
