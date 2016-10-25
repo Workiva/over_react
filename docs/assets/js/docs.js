a55b6306134789f750c5d5d8c1a8d593aef3bc94
Aaron Lademann <aaron.lademann@workiva.com>
2016-10-25 08:37:38 -0700

    Add analytics handlers for external links

--- docs/assets/js/docs.js
+++ docs/assets/js/docs.js
@@ -1,3 +1,57 @@
+var gaSourceCodeViewPath = '/over_react/demos/source/';
+
+function gaLinkHandler(action, label, isAffiliated, page) {
+    // For one reason or another, analytics was not loaded onto the page
+    if (!ga) return;
+
+    isAffiliated = isAffiliated || false;
+
+    var evCategory = 'External Link';
+
+    if (isAffiliated) {
+        evCategory += ': Affiliated';
+    } else {
+        evCategory += ': 3rd Party';
+    }
+
+    // Track as an event
+    ga('send', 'event', evCategory, action, label);
+
+    // Track as pageview if the "external" location is directly
+    // related to our library (e.g. click to go to gh repo, click to go to dartdocs, etc.)
+    if (isAffiliated) {
+        page = page || label;
+        ga('send', 'pageview', page);
+    }
+}
+
+// These link clicks take the user to a 3rd party location, unaffiliated with over_react.
+function handleExternalClick(action, label) {
+    gaLinkHandler(action, label, false);
+}
+
+// These link clicks take the user to a different domain, but one that
+// we wish we could track with ga as part of the github.io site...
+function handleAffiliatedClick(action, label, page) {
+    gaLinkHandler(action, label, true, page);
+}
+
+function handleSourceCodeView(componentName) {
+    handleAffiliatedClick('View Source Code', componentName, gaSourceCodeViewPath + componentName);
+}
+
+function handleBsExternalClick() {
+    try {
+        var linkText = arguments.callee.caller.arguments[0].target.innerText;
+
+        handleExternalClick("Click: Hero", linkText);
+    } catch (err) {
+        handleExternalClick("Click: Hero", "Bootstrap component");
+    }
+}
+
+///////////////////////////////////////////
+
 var contentPlaceholders = $('.content-placeholder__bg');
 var waitFn;
 
