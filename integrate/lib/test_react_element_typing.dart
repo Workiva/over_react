3205b263067188c9e7a8950e23da89a753555dea
Aaron Lademann <aaron.lademann@workiva.com>
2016-10-28 09:22:56 -0700

    Turn on strong mode for integrate directory

--- integrate/lib/test_react_element_typing.dart
+++ integrate/lib/test_react_element_typing.dart
@@ -20,7 +20,14 @@ import './test_component_declaration.dart';
 
 /// Verify that treating the invocation of the builder as an unparameterized [ReactElement]
 /// does not result in the following analyzer warning:
-/// > Unsound implicit cast from ReactElement<dynamic> to ReactElement<Component>
-void _reactElementTypingTest() {
+///
+///     Unsound implicit cast from ReactElement<dynamic> to ReactElement<Component>
+ReactElement _reactElementTypingTest() {
   ReactElement element = Foo()();
+
+  return element;
+}
+
+main() {
+  _reactElementTypingTest();
 }
