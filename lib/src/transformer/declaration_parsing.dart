8ccb8541125be208630ed687293f6a99fa9c9ce9
Aaron Lademann <aaron.lademann@workiva.com>
2016-11-01 11:00:18 -0700

    Fix the vm test errors a better way

--- lib/src/transformer/declaration_parsing.dart
+++ lib/src/transformer/declaration_parsing.dart
@@ -64,8 +64,8 @@ class ParsedDeclarations {
 
     // Validate the types of the annotated declarations.
 
-    List topLevelVarsOnly(String annotationName, Iterable<CompilationUnitMember> declarations) {
-      var topLevelVarDeclarations = <CompilationUnitMember>[];
+    List<TopLevelVariableDeclaration> topLevelVarsOnly(String annotationName, Iterable<CompilationUnitMember> declarations) {
+      var topLevelVarDeclarations = <TopLevelVariableDeclaration>[];
 
       declarations.forEach((declaration) {
         if (declaration is TopLevelVariableDeclaration) {
@@ -81,8 +81,8 @@ class ParsedDeclarations {
       return topLevelVarDeclarations;
     };
 
-    List classesOnly(String annotationName, Iterable<CompilationUnitMember> declarations) {
-      var classDeclarations = [];
+    List<ClassDeclaration> classesOnly(String annotationName, Iterable<CompilationUnitMember> declarations) {
+      var classDeclarations = <ClassDeclaration>[];
 
       declarations.forEach((declaration) {
         if (declaration is ClassDeclaration) {
@@ -98,8 +98,7 @@ class ParsedDeclarations {
       return classDeclarations;
     };
 
-    declarationMap[key_factory] =
-        topLevelVarsOnly(key_factory, declarationMap[key_factory]) as Iterable<CompilationUnitMember>; // ignore: avoid_as
+    declarationMap[key_factory] = topLevelVarsOnly(key_factory, declarationMap[key_factory]);
 
     [
       key_component,
@@ -111,8 +110,7 @@ class ParsedDeclarations {
       key_propsMixin,
       key_stateMixin,
     ].forEach((annotationName) {
-      declarationMap[annotationName] =
-          classesOnly(annotationName, declarationMap[annotationName]) as Iterable<CompilationUnitMember>; // ignore: avoid_as
+      declarationMap[annotationName] = classesOnly(annotationName, declarationMap[annotationName]);
     });
 
 
