ed107568a5d2f450cfb395124578cb325f87ba06
Max Peterson <maxwell.peterson@webfilings.com>
2018-01-10 15:14:10 -0700

    over_react should follow new logger name standard

--- lib/src/component_declaration/flux_component.dart
+++ lib/src/component_declaration/flux_component.dart
@@ -139,7 +139,7 @@ abstract class FluxUiStatefulComponent<TProps extends FluxUiProps, TState extend
 ///
 /// Private so it will only get used in this file, since having lifecycle methods in a mixin is risky.
 abstract class _FluxComponentMixin<TProps extends FluxUiProps> implements BatchedRedraws {
-  static final Logger _logger = new Logger('_FluxComponentMixin');
+  static final Logger _logger = new Logger('over_react._FluxComponentMixin');
   TProps get props;
 
   /// List of store subscriptions created when the component mounts.
