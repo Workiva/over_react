e98efccd67ef3776e8b8a89a49ca3fda2c0b562f
Aaron Lademann <aaron.lademann@workiva.com>
2018-02-26 16:41:19 -0700

    Do not execute prop validation logic in dart2js code
    + UIP-3062

--- lib/src/component_declaration/component_base.dart
+++ lib/src/component_declaration/component_base.dart
@@ -195,16 +195,19 @@ abstract class UiComponent<TProps extends UiProps> extends react.Component imple
     validateRequiredProps(appliedProps);
   }
 
+  /// Validates that props with the `@requiredProp` annotation are present.
   void validateRequiredProps(Map appliedProps) {
-    consumedProps?.forEach((ConsumedProps consumedProps) {
-      consumedProps.props.forEach((PropDescriptor prop) {
-        if (!prop.isRequired) return;
-        if (prop.isNullable && appliedProps.containsKey(prop.key)) return;
-        if (!prop.isNullable && appliedProps[prop.key] != null) return;
+    if (inReactDevMode) {
+      consumedProps?.forEach((ConsumedProps consumedProps) {
+        consumedProps.props.forEach((PropDescriptor prop) {
+          if (!prop.isRequired) return;
+          if (prop.isNullable && appliedProps.containsKey(prop.key)) return;
+          if (!prop.isNullable && appliedProps[prop.key] != null) return;
 
-        throw new PropError.required(prop.key, prop.errorMessage);
+          throw new PropError.required(prop.key, prop.errorMessage);
+        });
       });
-    });
+    }
   }
 
   /// Returns a new ClassNameBuilder with className and blacklist values added from [CssClassPropsMixin.className] and
@@ -220,13 +223,17 @@ abstract class UiComponent<TProps extends UiProps> extends react.Component imple
   @override
   @mustCallSuper
   void componentWillReceiveProps(Map nextProps) {
-    validateProps(nextProps);
+    if (inReactDevMode) {
+      validateProps(nextProps);
+    }
   }
 
   @override
   @mustCallSuper
   void componentWillMount() {
-    validateProps(props);
+    if (inReactDevMode) {
+      validateProps(props);
+    }
   }
 
   @override
