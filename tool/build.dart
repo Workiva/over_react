c1700c08c32879b2568511dc4bf09edab9e53fb8
Aaron Lademann <aaron.lademann@workiva.com>
2018-05-09 20:01:01 -0700

    Update dart version for travis

--- tool/build.dart
+++ tool/build.dart
@@ -6,13 +6,14 @@ import 'package:source_gen/source_gen.dart';
 import 'package:built_redux/generator.dart';
 
 Future main(List<String> args) async {
-  await build([
-    new BuildAction(
-        new PartBuilder([
-          new BuiltValueGenerator(),
-          new BuiltReduxGenerator(),
-        ]),
-        'over_react',
-        inputs: const ['test/over_react/component_declaration/redux_component_test/**'])
-  ], deleteFilesByDefault: true);
+  // TODO: This is not compatible with the latest version(s) of the build / build_runner packages.
+//  await build([
+//    new BuildAction(
+//        new PartBuilder([
+//          new BuiltValueGenerator(),
+//          new BuiltReduxGenerator(),
+//        ]),
+//        'over_react',
+//        inputs: const ['test/over_react/component_declaration/redux_component_test/**'])
+//  ], deleteFilesByDefault: true);
 }
