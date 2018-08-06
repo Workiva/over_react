eb2bfcf61abaff2701d2b67a7388ba1d7fbcb2d6
Corwin Sheahan <corwin.sheahan@workiva.com>
2018-08-06 10:31:04 -0600

    Include super class in sub's imports

---
+++ example/builder/generic_inheritance_sub.dart
@@ -0,0 +1,31 @@
+import 'package:over_react/over_react.dart';
+import 'generic_inheritance_super.dart';
+
+//part 'generic_inheritance_sub.g.dart';
+part 'generic_inheritance_sub.overReactBuilder.g.dart';
+
+@Factory()
+UiFactory<GenericSubProps> GenericSub = $GenericSub;
+
+@Props()
+// Heads up: props class inheritance doesn't work properly currently
+class GenericSubProps extends GenericSuperProps {
+  static const PropsMeta meta = $metaForGenericSubProps;
+
+  String subProp;
+}
+
+@Component()
+class GenericSubComponent extends GenericSuperComponent<GenericSubProps, ArbitraryClass> {
+  getDefaultProps() => newProps()..id = 'generic_sub';
+
+  @override
+  render() {
+    return Dom.div()('GenericSub', {
+      'props.subProp': props.subProp,
+      'props.superProp': props.superProp,
+//      'props': props.toString(),
+    }.toString());
+  }
+}
+
--- example/builder/generic_inheritance_super.dart
+++ example/builder/generic_inheritance_super.dart
@@ -1,7 +1,8 @@
 import 'package:over_react/over_react.dart';
 import 'package:react/react_client.dart';
 
-part 'generic_inheritance.g.dart';
+//part 'generic_inheritance.g.dart';
+part 'generic_inheritance_sub.overReactBuilder.g.dart';
 
 @Factory()
 UiFactory<GenericSuperProps> GenericSuper = $GenericSuper;
