5a323616974ef82bbc32d10c6002a2512dc88d51
Sebastian Malysa <sebastian.malysa@workiva.com>
2018-12-06 14:36:10 -0500

    AF-3084: add more tests

--- test/vm_tests/transformer/declaration_parsing_test.dart
+++ test/vm_tests/transformer/declaration_parsing_test.dart
@@ -463,7 +463,7 @@ main() {
       });
 
       group('and throws an error when', () {
-        test('a public props class when an private \$ prefixed props class is declared', () {
+        test('a public props class is not found when an private \$ prefixed props class is declared', () {
           setUpAndParse('''
               @Factory()    
               UiFactory<FooProps> Foo;
@@ -475,6 +475,57 @@ main() {
               class FooComponent {}
             ''');
           verify(logger.error('_\$FooProps must have an accompanying public class within the same file for Dart 2 builder compatibility, but one was not found.', span: any));
+        });
+
+        test('a public state class is not found when an private \$ prefixed state class is declared', () {
+          setUpAndParse('''
+              @Factory()    
+              UiFactory<FooProps> Foo;
+              
+              @Props()      
+              class FooProps {}
+              
+              @State()
+              class _\$FooState {}
+              
+              @Component()  
+              class FooComponent {}
+            ''');
+          verify(logger.error('_\$FooState must have an accompanying public class within the same file for Dart 2 builder compatibility, but one was not found.', span: any));
+        });
+
+        test('a public abstract props class is not found  when an private \$ prefixed abstract props class is declared', () {
+          setUpAndParse('''
+              @Factory()    
+              UiFactory<FooProps> Foo;
+              
+              @Props()      
+              class FooProps {}
+              
+              @Component()  
+              class FooComponent {}
+              
+              @AbstractProps() 
+              class _\$AbstractFooProps {}
+            ''');
+          verify(logger.error('_\$AbstractFooProps must have an accompanying public class within the same file for Dart 2 builder compatibility, but one was not found.', span: any));
+        });
+
+        test('a public abstract state class is not found when an private \$ prefixed abstract state class is declared', () {
+          setUpAndParse('''
+              @Factory()    
+              UiFactory<FooProps> Foo;
+              
+              @Props()      
+              class FooProps {}
+              
+              @Component()  
+              class FooComponent {}
+              
+              @AbstractState() 
+              class _\$AbstractStateProps {}
+             ''');
+          verify(logger.error('_\$AbstractStateProps must have an accompanying public class within the same file for Dart 2 builder compatibility, but one was not found.', span: any));
         });
 
         test('`subtypeOf` is an unsupported expression that is not an identifier', () {
