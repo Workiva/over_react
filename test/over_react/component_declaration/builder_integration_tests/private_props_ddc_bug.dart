2e6e3413924636cd961f2faf702ed696ea897de0
Corwin Sheahan <corwin.sheahan@workiva.com>
2019-03-03 10:15:59 -0700

    Add integration test

---
+++ test/over_react/component_declaration/builder_integration_tests/private_props_ddc_bug.dart
@@ -0,0 +1,39 @@
+import 'package:over_react/over_react.dart';
+import 'package:test/test.dart';
+
+import '../../../test_util/test_util.dart';
+
+part 'private_props_ddc_bug.over_react.g.dart';
+
+main() {
+  test('sets private props correctly in `getDefaultProps`', () {
+    var instance = render((Foo()
+      ..stringProp = 'some string value'
+    )());
+
+    expect(instance, isNotNull); // sanity check
+
+    var node = findDomNode(instance);
+
+    expect(node.text, contains('some private value'));
+  });
+}
+
+
+@Factory()
+UiFactory<FooProps> Foo = _$Foo;
+
+@Props()
+class _$FooProps extends UiProps {
+  String stringProp;
+  String _privateProp;
+}
+
+@Component()
+class FooComponent extends UiComponent<FooProps> {
+  @override
+  Map getDefaultProps() => newProps().._privateProp = 'some private value';
+
+  @override
+  render() => (Dom.div())(props._privateProp);
+}
