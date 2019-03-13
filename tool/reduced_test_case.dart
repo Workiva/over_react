fc210c48fda3fc3987ff3ff6c3b577fd16d4b88a
Greg Littlefield <greg.littlefield@workiva.com>
2019-03-13 16:03:27 -0700

    Remove leftover file

--- tool/reduced_test_case.dart
+++
@@ -1,8 +0,0 @@
-//class Component {
-//  Map get props {}
-//}
-//
-//class UiComponent<TProps extends Map> extends Component {
-//  @override
-//  TProps get props; // only happens when abstract
-//}
