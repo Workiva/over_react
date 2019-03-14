f6371fd4e6991c516be0e4fbc1bb7da39f82653c
Greg Littlefield <greg.littlefield@workiva.com>
2019-03-14 06:27:37 -0700

    Update boilerplate and fix builder setup

--- playground/web/component_render.dart
+++ playground/web/component_render.dart
@@ -5,7 +5,7 @@ part 'component_render.over_react.g.dart';
 
 @Factory()
 // ignore: undefined_identifier
-UiFactory<BadRenderProps> BadRender = $BadRender;
+UiFactory<BadRenderProps> BadRender = _$BadRender;
 
 @Props()
 class _$BadRenderProps extends UiProps {}
@@ -13,7 +13,7 @@ class _$BadRenderProps extends UiProps {}
 // ignore: mixin_of_non_class, undefined_class
 class BadRenderProps extends _$BadRenderProps with _$BadRenderPropsAccessorsMixin {
   // ignore: undefined_identifier, undefined_class, const_initialized_with_non_constant_value
-  static const PropsMeta meta = $metaForBadRenderProps;
+  static const PropsMeta meta = _$metaForBadRenderProps;
 }
 
 @Component()
--- playground/web/foo.dart
+++ playground/web/foo.dart
@@ -1,11 +1,10 @@
 import 'package:over_react/over_react.dart';
 
-// ignore: uri_has_not_been_generated
 part 'foo.over_react.g.dart';
 
 @Factory()
 // ignore: undefined_identifier
-UiFactory<FooProps> Foo = $Foo;
+UiFactory<FooProps> Foo = _$Foo;
 
 @Props()
 class _$FooProps extends UiProps {}
@@ -13,7 +12,7 @@ class _$FooProps extends UiProps {}
 // ignore: mixin_of_non_class, undefined_class
 class FooProps extends _$FooProps with _$FooPropsAccessorsMixin {
   // ignore: undefined_identifier, undefined_class, const_initialized_with_non_constant_value
-  static const PropsMeta meta = $metaForFooProps;
+  static const PropsMeta meta = _$metaForFooProps;
 }
 
 @Component()
