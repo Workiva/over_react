44f45abbe745eab92bb160b2b429af4c235c98dc
Greg Littlefield <greg.littlefield@workiva.com>
2019-03-23 16:48:57 -0700

    Required prop cleanup

--- over_react_analyzer_plugin/lib/src/diagnostic/over_react/missing_required_prop.dart
+++ over_react_analyzer_plugin/lib/src/diagnostic/over_react/missing_required_prop.dart
@@ -28,37 +28,47 @@ class MissingRequiredPropChecker extends ComponentUsageChecker {
 
     // FIXME this almost definitely needs optimization/caching
 
-    final builderType = usage.builder.staticType;
+    var builderType = usage.builder.staticType;
+    // Handle generic factories (todo might not be needed)
+    while (builderType is TypeParameterType) {
+      builderType = (builderType as TypeParameterType).bound;
+    }
+
     // todo check if factory invocation
-    if (builderType != null && builderType.name != 'UiProps') {
-      if (builderType is InterfaceType) {
-        final classAndSuperclasses = [builderType.element]
-          ..addAll(builderType.element.allSupertypes.map((t) => t.element));
-        final allFields = classAndSuperclasses.expand((c) => c.fields);
-        requiredFields.addAll(allFields.where((field) {
-          return field.metadata.any((annotation) {
-            // Common case, might be good to short circuit here for perf
-            if (annotation.isOverride) return false;
+    if (builderType != null && builderType.name != 'UiProps' && builderType is InterfaceType) {
+      final classAndSuperclasses = [builderType.element]
+        ..addAll(builderType.element.allSupertypes.map((t) => t.element));
+      final allFields = classAndSuperclasses.expand((c) => c.fields);
+      for (var field in allFields) {
+        if (requiredFields.any((requiredField) => requiredField.name == field.name)) {
+          // Short-circuit if we've already identified this field as required.
+          // There might be duplicates if props are overridden, and there will
+          // definitely be duplicates in the builder-generated code.
+          continue;
+        }
 
-            final value = annotation.computeConstantValue();
-            final type = value?.type;
-            final typeLibrary = type?.element?.library;
-            if (typeLibrary?.name != 'over_react.component_declaration.annotations') {
-              return false;
-            }
+        if (field.metadata.any((annotation) {
+          // Common case, might be good to short circuit here for perf
+          if (annotation.isOverride) return false;
 
-            _cachedAccessorClass ??= typeLibrary.getType('Accessor');
-            if (!type.isAssignableTo(_cachedAccessorClass.type)) {
-              return false;
-            }
+          final value = annotation.computeConstantValue();
+          final type = value?.type;
+          final typeLibrary = type?.element?.library;
+          if (typeLibrary?.name != 'over_react.component_declaration.annotations') {
+            return false;
+          }
 
-            // This is null when isRequired does not have a valid value
-            // (e.g., as the user is typing it in)
-            return value.getField('isRequired').toBoolValue() ?? false;
-          });
-        }));
-      } else {
-        // todo TypeParameterType
+          _cachedAccessorClass ??= typeLibrary.getType('Accessor');
+          if (!type.isAssignableTo(_cachedAccessorClass.type)) {
+            return false;
+          }
+
+          // This is null when isRequired does not have a valid value
+          // (e.g., as the user is typing it in)
+          return value.getField('isRequired').toBoolValue() ?? false;
+        })) {
+          requiredFields.add(field);
+        }
       }
     }
 
