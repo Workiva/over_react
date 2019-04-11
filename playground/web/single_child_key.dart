5e6bc1d76aa69c03faad0a757f56209a3fa98816
Joseph Bingham <joe.bingham@workiva.com>
2019-04-11 12:02:27 -0500

    Add extended example

--- playground/web/single_child_key.dart
+++ playground/web/single_child_key.dart
@@ -51,3 +51,53 @@ singleChildKey() {
     ),
   );
 }
+
+@Props()
+class TestComponentProps extends UiProps {
+
+}
+
+@Component()
+class TestComponent extends UiComponent<TestComponentProps> {
+
+  @override
+  render(){
+    final children = [ _renderDivs()];
+    return Dom.div()(children);
+  }
+
+  _renderDivs() => (Dom.div()..key = 'button')(
+    (Dom.div()
+      ..key = 'n-sibbling-1'
+      ..id = '2'
+      ..onClick = (_) {
+        final bar = 'baz';
+        return false;
+      }
+    )(
+      [
+        (Dom.div()
+          ..key = 'n2-sibbling-1'
+          ..id = '2'
+          ..onClick = (_) {
+            final bar = 'baz';
+            return false;
+          }
+        )(
+          "Hello",
+        )
+      ]
+    ),
+    (Dom.div()
+      ..key = 'sibbling-2'
+      ..id = '2'
+      ..onClick = (_) {
+        final bar = 'baz';
+        return false;
+      }
+    )(
+      "Child 2"
+    ),
+  );
+}
+
