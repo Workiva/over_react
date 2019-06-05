a8711a6d201cb6952bb88988037a441d56c765d8
Greg Littlefield <greg.littlefield@workiva.com>
2019-06-05 11:59:51 -0700

    Remove duplicated doc comments, which get inherited

--- lib/src/util/handler_chain_util.dart
+++ lib/src/util/handler_chain_util.dart
@@ -73,16 +73,9 @@ class CallbackUtil0Arg extends CallbackUtil {
 
   _noop() {}
 
-  /// A function of type [Callback0Arg] that does nothing.
   @override
   Callback0Arg get noop => _noop;
 
-  /// Returns a strongly-typed chained callback that calls through to the two provided callbacks, [a] and [b], in order.
-  /// Useful for executing multiple callbacks where only a single callback is accepted.
-  ///
-  /// Returns `false` if one or more of the provided callbacks returns `false`.
-  ///
-  /// Gracefully handles when [a] and/or [b] are null, always returning a callable function.
   @override
   Callback0Arg chain(Callback0Arg a, Callback0Arg b) {
     if (a == null && b == null) return noop;
@@ -108,16 +101,9 @@ class CallbackUtil1Arg<T> extends CallbackUtil {
 
   _noop(T arg1) {}
 
-  /// A function of type [Callback1Arg<T>] that does nothing.
   @override
   Callback1Arg<T> get noop => _noop;
 
-  /// Returns a strongly-typed chained callback that calls through to the two provided callbacks, [a] and [b], in order.
-  /// Useful for executing multiple callbacks where only a single callback is accepted.
-  ///
-  /// Returns `false` if one or more of the provided callbacks returns `false`.
-  ///
-  /// Gracefully handles when [a] and/or [b] are null, always returning a callable function.
   @override
   Callback1Arg<T> chain(Callback1Arg<T> a, Callback1Arg<T> b) {
     if (a == null && b == null) return noop;
@@ -143,16 +129,9 @@ class CallbackUtil2Arg<T1, T2> extends CallbackUtil {
 
   _noop(T1 arg1, T2 arg2) {}
 
-  /// A function of type [Callback2Arg<T1, T2>] that does nothing.
   @override
   Callback2Arg<T1, T2> get noop => _noop;
 
-  /// Returns a strongly-typed chained callback that calls through to the two provided callbacks, [a] and [b], in order.
-  /// Useful for executing multiple callbacks where only a single callback is accepted.
-  ///
-  /// Returns `false` if one or more of the provided callbacks returns `false`.
-  ///
-  /// Gracefully handles when [a] and/or [b] are null, always returning a callable function.
   @override
   Callback2Arg<T1, T2> chain(Callback2Arg<T1, T2> a, Callback2Arg<T1, T2> b) {
     if (a == null && b == null) return noop;
@@ -178,16 +157,9 @@ class CallbackUtil3Arg<T1, T2, T3> extends CallbackUtil {
 
   _noop(T1 arg1, T2 arg2, T3 arg3) {}
 
-  /// A function of type [Callback3Arg<T1, T2, T3>] that does nothing.
   @override
   Callback3Arg<T1, T2, T3> get noop => _noop;
 
-  /// Returns a strongly-typed chained callback that calls through to the two provided callbacks, [a] and [b], in order.
-  /// Useful for executing multiple callbacks where only a single callback is accepted.
-  ///
-  /// Returns `false` if one or more of the provided callbacks returns `false`.
-  ///
-  /// Gracefully handles when [a] and/or [b] are null, always returning a callable function.
   @override
   Callback3Arg<T1, T2, T3> chain(Callback3Arg<T1, T2, T3> a, Callback3Arg<T1, T2, T3> b) {
     if (a == null && b == null) return noop;
@@ -213,16 +185,9 @@ class CallbackUtil4Arg<T1, T2, T3, T4> extends CallbackUtil {
 
   _noop(T1 arg1, T2 arg2, T3 arg3, T4 arg4) {}
 
-  /// A function of type [Callback3Arg<T1, T2, T3, T4>] that does nothing.
   @override
   Callback4Arg<T1, T2, T3, T4> get noop => _noop;
 
-  /// Returns a strongly-typed chained callback that calls through to the two provided callbacks, [a] and [b], in order.
-  /// Useful for executing multiple callbacks where only a single callback is accepted.
-  ///
-  /// Returns `false` if one or more of the provided callbacks returns `false`.
-  ///
-  /// Gracefully handles when [a] and/or [b] are null, always returning a callable function.
   @override
   Callback4Arg<T1, T2, T3, T4> chain(Callback4Arg<T1, T2, T3, T4> a, Callback4Arg<T1, T2, T3, T4> b) {
     if (a == null && b == null) return noop;
@@ -263,6 +228,6 @@ abstract class CallbackUtil<_> {
   Function chainFromList(covariant List<Function> callbacks) =>
       callbacks.fold(null, chain) ?? noop;
 
-  /// A function of type [Function] that does nothing.
+  /// A callback that does nothing.
   Function get noop;
 }
