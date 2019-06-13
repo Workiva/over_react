6467eb40dbd71667f681618cbb1f0e659505b114
Joseph Bingham <joe.bingham@workiva.com>
2019-06-13 13:13:20 -0700

    Pull fragment from react.dart

--- lib/src/component/fragment_component.dart
+++ lib/src/component/fragment_component.dart
@@ -1,7 +1,8 @@
 import 'package:over_react/src/component_declaration/component_base.dart' as component_base;
 import 'package:over_react/src/component_declaration/builder_helpers.dart' as builder_helpers;
-import 'package:react/react_client.dart' as rc;
+import 'package:react/react_client.dart';
 import 'package:react/src/react_client/js_backed_map.dart';
+import 'package:react/react.dart' as react;
 
 class FragmentProps extends component_base.UiProps
     with
@@ -14,7 +15,7 @@ class FragmentProps extends component_base.UiProps
   FragmentProps(this.componentFactory, [Map props]) : this.props = props ?? new JsBackedMap();
 
   @override
-  final rc.ReactJsComponentFactoryProxy componentFactory;
+  final ReactJsComponentFactoryProxy componentFactory;
 
   @override
   final Map props;
@@ -23,4 +24,4 @@ class FragmentProps extends component_base.UiProps
   String get propKeyNamespace => '';
 }
 
-Fragment() => new FragmentProps(rc.Fragment);
+Fragment() => new FragmentProps(react.Fragment);
