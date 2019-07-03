180dc957b5ada26f78d62db2d27750b64a9b91ae
Keal Jones <keal.jones@workiva.com>
2019-07-03 13:25:51 -0700

    remove covariant

--- lib/src/component_declaration/component_base.dart
+++ lib/src/component_declaration/component_base.dart
@@ -619,7 +619,7 @@ abstract class UiProps extends MapBase
     return true;
   }
 
-  covariant ReactComponentFactoryProxy componentFactory;
+  ReactComponentFactoryProxy componentFactory;
 
   /// An unmodifiable map view of the default props for this component brought
   /// in from the [componentFactory].
