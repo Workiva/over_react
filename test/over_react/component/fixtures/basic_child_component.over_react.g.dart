797d10b46af8cdd0a6aad5f246d218bd3d8f4904
Sydney Jodon <sydney.jodon@workiva.com>
2019-07-15 16:07:59 -0700

    Update generated files from merging in 3.1.0-wip

--- test/over_react/component/fixtures/basic_child_component.over_react.g.dart
+++ test/over_react/component/fixtures/basic_child_component.over_react.g.dart
@@ -62,14 +62,10 @@ abstract class _$$BasicChildProps extends _$BasicChildProps
   @override
   bool get $isClassGenerated => true;
 
-  ReactComponentFactoryProxy _factoryOverride;
-
   /// The [ReactComponentFactory] associated with the component built by this class.
   @override
   ReactComponentFactoryProxy get componentFactory =>
-      _factoryOverride ?? $BasicChildComponentFactory;
-  @override
-  set componentFactory(ReactComponentFactoryProxy v) => _factoryOverride = v;
+      super.componentFactory ?? $BasicChildComponentFactory;
 
   /// The default namespace for the prop getters/setters generated for this class.
   @override
