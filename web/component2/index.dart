33dc42aa5690ac0357196816f9aeeba777500de2
Keal Jones <keal.jones@workiva.com>
2019-07-15 13:26:50 -0700

    remove some stuff that was accidently left in

--- web/component2/index.dart
+++ web/component2/index.dart
@@ -3,12 +3,47 @@ import 'dart:html';
 import 'package:over_react/over_react.dart';
 import 'package:over_react/react_dom.dart' as react_dom;
 
-//import 'src/demos.dart';
-import 'src/demo_components/simple.dart';
-import 'src/demo_components/button.dart';
+import 'src/demos.dart';
+
 void main() {
   setClientConfiguration();
 
   react_dom.render(
-    (Simple()..componentFactory = Button().componentFactory)(), querySelector('#test'));
+    buttonExamplesDemo(), querySelector('$demoMountNodeSelectorPrefix--button'));
+
+  react_dom.render(
+    listGroupBasicDemo(), querySelector('$demoMountNodeSelectorPrefix--list-group'));
+
+  react_dom.render(
+    progressBasicDemo(), querySelector('$demoMountNodeSelectorPrefix--progress'));
+
+  react_dom.render(
+    tagBasicDemo(), querySelector('$demoMountNodeSelectorPrefix--tag'));
+
+  react_dom.render(
+    checkboxToggleButtonDemo(), querySelector('$demoMountNodeSelectorPrefix--checkbox-toggle'));
+
+  react_dom.render(
+    radioToggleButtonDemo(), querySelector('$demoMountNodeSelectorPrefix--radio-toggle'));
+
+  react_dom.render(
+    (ErrorBoundary()
+      ..onComponentDidCatch = (error, info) {
+        print('Consumer props.onComponentDidCatch($error, $info)');
+      }
+    )(Faulty()()),
+    querySelector('$demoMountNodeSelectorPrefix--faulty-component'),
+  );
+
+  react_dom.render(
+    (CustomErrorBoundary()
+      ..onComponentDidCatch = (error, info) {
+        print('Consumer props.onComponentDidCatch($error, $info)');
+      }
+    )(Faulty()()),
+    querySelector('$demoMountNodeSelectorPrefix--faulty-component-with-custom-error-boundary'),
+  );
+
+  react_dom.render(
+    Faulty()(), querySelector('$demoMountNodeSelectorPrefix--faulty-component-without-error-boundary'));
 }
