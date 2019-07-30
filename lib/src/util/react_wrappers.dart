86d335fc16dca55ceaff0397192fe12be8bd3559
Greg Littlefield <greg.littlefield@workiva.com>
2019-07-29 18:30:44 -0700

    Export alias to createPortal

--- lib/src/util/react_wrappers.dart
+++ lib/src/util/react_wrappers.dart
@@ -162,6 +162,15 @@ Map getProps(/* ReactElement|ReactComponent */ instance, {bool traverseWrappers:
 /// (and for the function itself, which is declared using `var` in react-dart).
 Element findDomNode(dynamic instance) => react_dom.findDOMNode(instance);
 
+/// Returns a a portal that renders [children] into a [container].
+///
+/// Portals provide a first-class way to render children into a DOM node that exists outside the DOM hierarchy of the parent component.
+///
+/// [children] can be any renderable React child, such as a [ReactElement], [String], or fragment.
+///
+/// See: <https://reactjs.org/docs/portals.html>
+ReactPortal createPortal(dynamic children, Element container) => ReactDom.createPortal(children, container);
+
 /// Dart wrapper for React.isValidElement.
 ///
 /// _From the JS docs:_
