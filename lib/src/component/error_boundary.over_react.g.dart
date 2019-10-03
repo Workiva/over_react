de99fd155ff25a5320e143c72ec28a1aed410892
Aaron Lademann <aaron.lademann@workiva.com>
2019-10-03 12:15:27 -0700

    Improve handling of “repeat” errors that can lock up the main thread

--- lib/src/component/error_boundary.over_react.g.dart
+++ lib/src/component/error_boundary.over_react.g.dart
@@ -22,38 +22,40 @@ abstract class _$ErrorBoundaryPropsAccessorsMixin
   @override
   Map get props;
 
-  /// An optional callback that will be called with an [Error] and a `ComponentStack`
-  /// containing information about which component in the tree threw the error when
-  /// the `componentDidCatch` lifecycle method is called.
+  /// An optional callback that will be called with an [Error] _(or [Exception])_
+  /// and `errorInfo` containing information about which component in the tree
+  /// threw when the `componentDidCatch` lifecycle method is called.
   ///
   /// This callback can be used to log component errors like so:
   ///
   ///     (ErrorBoundary()
-  ///       ..onComponentDidCatch = (error, componentStack) {
+  ///       ..onComponentDidCatch = (error, errorInfo) {
   ///         // It is up to you to implement the service / thing that calls the service.
-  ///         logComponentStackToAService(error, componentStack);
+  ///         logComponentStackToAService(error, errorInfo);
   ///       }
   ///     )(
   ///       // The rest of your component tree
   ///     )
   ///
   /// > See: <https://reactjs.org/docs/react-component.html#componentdidcatch>
+  ///
+  /// > Related: [onComponentIsUnrecoverable]
   ///
   /// <!-- Generated from [_$ErrorBoundaryProps.onComponentDidCatch] -->
   @override
   _ComponentDidCatchCallback get onComponentDidCatch =>
       props[_$key__onComponentDidCatch___$ErrorBoundaryProps] ??
       null; // Add ` ?? null` to workaround DDC bug: <https://github.com/dart-lang/sdk/issues/36052>;
-  /// An optional callback that will be called with an [Error] and a `ComponentStack`
-  /// containing information about which component in the tree threw the error when
-  /// the `componentDidCatch` lifecycle method is called.
+  /// An optional callback that will be called with an [Error] _(or [Exception])_
+  /// and `errorInfo` containing information about which component in the tree
+  /// threw when the `componentDidCatch` lifecycle method is called.
   ///
   /// This callback can be used to log component errors like so:
   ///
   ///     (ErrorBoundary()
-  ///       ..onComponentDidCatch = (error, componentStack) {
+  ///       ..onComponentDidCatch = (error, errorInfo) {
   ///         // It is up to you to implement the service / thing that calls the service.
-  ///         logComponentStackToAService(error, componentStack);
+  ///         logComponentStackToAService(error, errorInfo);
   ///       }
   ///     )(
   ///       // The rest of your component tree
@@ -61,15 +63,75 @@ abstract class _$ErrorBoundaryPropsAccessorsMixin
   ///
   /// > See: <https://reactjs.org/docs/react-component.html#componentdidcatch>
   ///
+  /// > Related: [onComponentIsUnrecoverable]
+  ///
   /// <!-- Generated from [_$ErrorBoundaryProps.onComponentDidCatch] -->
   @override
   set onComponentDidCatch(_ComponentDidCatchCallback value) =>
       props[_$key__onComponentDidCatch___$ErrorBoundaryProps] = value;
 
+  /// An optional callback that will be called _(when [ErrorBoundaryProps.fallbackUIRenderer] is not set)_
+  /// with an [Error] _(or [Exception])_ and `errorInfo` containing information about which component in
+  /// the tree threw multiple consecutive errors/exceptions frequently enough that it has the potential
+  /// to lock the main thread.
+  ///
+  /// The locking of the main thread is possible in this scenario because when [ErrorBoundaryProps.fallbackUIRenderer]
+  /// is not set, the [ErrorBoundary] simply re-mounts the child when an error occurs to try to "recover" automatically.
+  /// However, if multiple identical errors are thrown from the exact same component in the tree - every time
+  /// the [ErrorBoundary] re-mounts the tree, a sort of "infinite loop" will occur.
+  ///
+  /// When this callback is called, the tree wrapped by this [ErrorBoundary] has "crashed" - and is completely
+  /// non-functional. Instead of re-mounting the component tree, the [ErrorBoundary] will simply render a
+  /// non-interactive `String` representation of the DOM that was captured at the time of the error.
+  ///
+  /// Once this happens, regaining interactivity within the tree wrapped by this [ErrorBoundary] is possible by:
+  ///
+  /// 1. Passing in a new child
+  ///   _(preferably one that will not repeatedly throw errors when the [ErrorBoundary] mounts it)_.
+  /// 2. Calling the [ErrorBoundaryComponent.reset] instance method using a `ref`.
+  ///
+  /// > Will never be called when [ErrorBoundaryProps.fallbackUIRenderer] is set.
+  ///
+  /// > Related: [identicalErrorFrequencyTolerance]
+  ///
+  /// <!-- Generated from [_$ErrorBoundaryProps.onComponentIsUnrecoverable] -->
+  @override
+  _ComponentDidCatchCallback get onComponentIsUnrecoverable =>
+      props[_$key__onComponentIsUnrecoverable___$ErrorBoundaryProps] ??
+      null; // Add ` ?? null` to workaround DDC bug: <https://github.com/dart-lang/sdk/issues/36052>;
+  /// An optional callback that will be called _(when [ErrorBoundaryProps.fallbackUIRenderer] is not set)_
+  /// with an [Error] _(or [Exception])_ and `errorInfo` containing information about which component in
+  /// the tree threw multiple consecutive errors/exceptions frequently enough that it has the potential
+  /// to lock the main thread.
+  ///
+  /// The locking of the main thread is possible in this scenario because when [ErrorBoundaryProps.fallbackUIRenderer]
+  /// is not set, the [ErrorBoundary] simply re-mounts the child when an error occurs to try to "recover" automatically.
+  /// However, if multiple identical errors are thrown from the exact same component in the tree - every time
+  /// the [ErrorBoundary] re-mounts the tree, a sort of "infinite loop" will occur.
+  ///
+  /// When this callback is called, the tree wrapped by this [ErrorBoundary] has "crashed" - and is completely
+  /// non-functional. Instead of re-mounting the component tree, the [ErrorBoundary] will simply render a
+  /// non-interactive `String` representation of the DOM that was captured at the time of the error.
+  ///
+  /// Once this happens, regaining interactivity within the tree wrapped by this [ErrorBoundary] is possible by:
+  ///
+  /// 1. Passing in a new child
+  ///   _(preferably one that will not repeatedly throw errors when the [ErrorBoundary] mounts it)_.
+  /// 2. Calling the [ErrorBoundaryComponent.reset] instance method using a `ref`.
+  ///
+  /// > Will never be called when [ErrorBoundaryProps.fallbackUIRenderer] is set.
+  ///
+  /// > Related: [identicalErrorFrequencyTolerance]
+  ///
+  /// <!-- Generated from [_$ErrorBoundaryProps.onComponentIsUnrecoverable] -->
+  @override
+  set onComponentIsUnrecoverable(_ComponentDidCatchCallback value) =>
+      props[_$key__onComponentIsUnrecoverable___$ErrorBoundaryProps] = value;
+
   /// A renderer that will be used to render "fallback" UI instead of the child
   /// component tree that crashed.
   ///
-  /// > Default: [ErrorBoundaryComponent._renderDefaultFallbackUI]
+  /// > Related: [onComponentIsUnrecoverable], [onComponentDidCatch]
   ///
   /// <!-- Generated from [_$ErrorBoundaryProps.fallbackUIRenderer] -->
   @override
@@ -79,30 +141,94 @@ abstract class _$ErrorBoundaryPropsAccessorsMixin
   /// A renderer that will be used to render "fallback" UI instead of the child
   /// component tree that crashed.
   ///
-  /// > Default: [ErrorBoundaryComponent._renderDefaultFallbackUI]
+  /// > Related: [onComponentIsUnrecoverable], [onComponentDidCatch]
   ///
   /// <!-- Generated from [_$ErrorBoundaryProps.fallbackUIRenderer] -->
   @override
   set fallbackUIRenderer(_FallbackUiRenderer value) =>
       props[_$key__fallbackUIRenderer___$ErrorBoundaryProps] = value;
+
+  /// The amount of time that is "acceptable" between consecutive identical errors thrown from a component
+  /// within the tree wrapped by this [ErrorBoundary].
+  ///
+  /// If [fallbackUIRenderer] is not set, and more than one identical error is thrown consecutively by
+  /// the exact same component anywhere within the tree wrapped by this [ErrorBoundary] - more often than
+  /// the specified duration, the [ErrorBoundary] will:
+  ///
+  ///   1. Call [onComponentIsUnrecoverable].
+  ///   2. Stop attempting to re-mount the tree (to protect the main thread from being locked up).
+  ///   3. Render a non-interactive `String` representation of the DOM at the time of the error.
+  ///
+  /// When this happens, recovery can only occur by passing in a new child to
+  /// the [ErrorBoundary], or by calling [ErrorBoundaryComponent.reset].
+  ///
+  /// __DO NOT MODIFY THIS VALUE UNLESS YOU KNOW WHAT YOU ARE DOING.__
+  ///
+  /// > Default: `const Duration(seconds: 5)`
+  ///
+  /// <!-- Generated from [_$ErrorBoundaryProps.identicalErrorFrequencyTolerance] -->
+  @override
+  Duration get identicalErrorFrequencyTolerance =>
+      props[_$key__identicalErrorFrequencyTolerance___$ErrorBoundaryProps] ??
+      null; // Add ` ?? null` to workaround DDC bug: <https://github.com/dart-lang/sdk/issues/36052>;
+  /// The amount of time that is "acceptable" between consecutive identical errors thrown from a component
+  /// within the tree wrapped by this [ErrorBoundary].
+  ///
+  /// If [fallbackUIRenderer] is not set, and more than one identical error is thrown consecutively by
+  /// the exact same component anywhere within the tree wrapped by this [ErrorBoundary] - more often than
+  /// the specified duration, the [ErrorBoundary] will:
+  ///
+  ///   1. Call [onComponentIsUnrecoverable].
+  ///   2. Stop attempting to re-mount the tree (to protect the main thread from being locked up).
+  ///   3. Render a non-interactive `String` representation of the DOM at the time of the error.
+  ///
+  /// When this happens, recovery can only occur by passing in a new child to
+  /// the [ErrorBoundary], or by calling [ErrorBoundaryComponent.reset].
+  ///
+  /// __DO NOT MODIFY THIS VALUE UNLESS YOU KNOW WHAT YOU ARE DOING.__
+  ///
+  /// > Default: `const Duration(seconds: 5)`
+  ///
+  /// <!-- Generated from [_$ErrorBoundaryProps.identicalErrorFrequencyTolerance] -->
+  @override
+  set identicalErrorFrequencyTolerance(Duration value) =>
+      props[_$key__identicalErrorFrequencyTolerance___$ErrorBoundaryProps] =
+          value;
   /* GENERATED CONSTANTS */
   static const PropDescriptor
       _$prop__onComponentDidCatch___$ErrorBoundaryProps =
       const PropDescriptor(_$key__onComponentDidCatch___$ErrorBoundaryProps);
+  static const PropDescriptor
+      _$prop__onComponentIsUnrecoverable___$ErrorBoundaryProps =
+      const PropDescriptor(
+          _$key__onComponentIsUnrecoverable___$ErrorBoundaryProps);
   static const PropDescriptor _$prop__fallbackUIRenderer___$ErrorBoundaryProps =
       const PropDescriptor(_$key__fallbackUIRenderer___$ErrorBoundaryProps);
+  static const PropDescriptor
+      _$prop__identicalErrorFrequencyTolerance___$ErrorBoundaryProps =
+      const PropDescriptor(
+          _$key__identicalErrorFrequencyTolerance___$ErrorBoundaryProps);
   static const String _$key__onComponentDidCatch___$ErrorBoundaryProps =
       'ErrorBoundaryProps.onComponentDidCatch';
+  static const String _$key__onComponentIsUnrecoverable___$ErrorBoundaryProps =
+      'ErrorBoundaryProps.onComponentIsUnrecoverable';
   static const String _$key__fallbackUIRenderer___$ErrorBoundaryProps =
       'ErrorBoundaryProps.fallbackUIRenderer';
+  static const String
+      _$key__identicalErrorFrequencyTolerance___$ErrorBoundaryProps =
+      'ErrorBoundaryProps.identicalErrorFrequencyTolerance';
 
   static const List<PropDescriptor> $props = const [
     _$prop__onComponentDidCatch___$ErrorBoundaryProps,
-    _$prop__fallbackUIRenderer___$ErrorBoundaryProps
+    _$prop__onComponentIsUnrecoverable___$ErrorBoundaryProps,
+    _$prop__fallbackUIRenderer___$ErrorBoundaryProps,
+    _$prop__identicalErrorFrequencyTolerance___$ErrorBoundaryProps
   ];
   static const List<String> $propKeys = const [
     _$key__onComponentDidCatch___$ErrorBoundaryProps,
-    _$key__fallbackUIRenderer___$ErrorBoundaryProps
+    _$key__onComponentIsUnrecoverable___$ErrorBoundaryProps,
+    _$key__fallbackUIRenderer___$ErrorBoundaryProps,
+    _$key__identicalErrorFrequencyTolerance___$ErrorBoundaryProps
   ];
 }
 
@@ -156,34 +282,75 @@ abstract class _$ErrorBoundaryStateAccessorsMixin
   @override
   Map get state;
 
-  /// Whether the tree that the [ErrorBoundary] is wrapping around threw an error.
+  /// Whether a component within the tree that the [ErrorBoundary] is wrapping around threw an error.
   ///
-  /// When `true`, fallback UI will be rendered using [ErrorBoundaryProps.fallbackUIRenderer].
+  /// * When `true`, and [ErrorBoundaryProps.fallbackUIRenderer] is set, the return value of that callback
+  ///   will be rendered instead.
+  /// * When `true`, and [ErrorBoundaryProps.fallbackUIRenderer] is not set, the [ErrorBoundary] will re-mount
+  ///   the tree to attempt to automatically recover from the error.
+  ///
+  ///   If an identical error is thrown from an identical component within the tree consecutively
+  ///   more frequently than [ErrorBoundaryProps.identicalErrorFrequencyTolerance], a non-interactive
+  ///   `String` representation of the DOM that was captured at the time of the error will be rendered.
+  ///   See: [ErrorBoundaryProps.onComponentIsUnrecoverable] for more information about this scenario.
   ///
   /// <!-- Generated from [_$ErrorBoundaryState.hasError] -->
   @override
   bool get hasError =>
       state[_$key__hasError___$ErrorBoundaryState] ??
       null; // Add ` ?? null` to workaround DDC bug: <https://github.com/dart-lang/sdk/issues/36052>;
-  /// Whether the tree that the [ErrorBoundary] is wrapping around threw an error.
+  /// Whether a component within the tree that the [ErrorBoundary] is wrapping around threw an error.
   ///
-  /// When `true`, fallback UI will be rendered using [ErrorBoundaryProps.fallbackUIRenderer].
+  /// * When `true`, and [ErrorBoundaryProps.fallbackUIRenderer] is set, the return value of that callback
+  ///   will be rendered instead.
+  /// * When `true`, and [ErrorBoundaryProps.fallbackUIRenderer] is not set, the [ErrorBoundary] will re-mount
+  ///   the tree to attempt to automatically recover from the error.
+  ///
+  ///   If an identical error is thrown from an identical component within the tree consecutively
+  ///   more frequently than [ErrorBoundaryProps.identicalErrorFrequencyTolerance], a non-interactive
+  ///   `String` representation of the DOM that was captured at the time of the error will be rendered.
+  ///   See: [ErrorBoundaryProps.onComponentIsUnrecoverable] for more information about this scenario.
   ///
   /// <!-- Generated from [_$ErrorBoundaryState.hasError] -->
   @override
   set hasError(bool value) =>
       state[_$key__hasError___$ErrorBoundaryState] = value;
+
+  /// Whether to show "fallback" UI when [hasError] is true.
+  ///
+  /// This value will always be true if [ErrorBoundaryProps.fallbackUIRenderer] is non-null.
+  ///
+  /// <!-- Generated from [_$ErrorBoundaryState.showFallbackUIOnError] -->
+  @override
+  bool get showFallbackUIOnError =>
+      state[_$key__showFallbackUIOnError___$ErrorBoundaryState] ??
+      null; // Add ` ?? null` to workaround DDC bug: <https://github.com/dart-lang/sdk/issues/36052>;
+  /// Whether to show "fallback" UI when [hasError] is true.
+  ///
+  /// This value will always be true if [ErrorBoundaryProps.fallbackUIRenderer] is non-null.
+  ///
+  /// <!-- Generated from [_$ErrorBoundaryState.showFallbackUIOnError] -->
+  @override
+  set showFallbackUIOnError(bool value) =>
+      state[_$key__showFallbackUIOnError___$ErrorBoundaryState] = value;
   /* GENERATED CONSTANTS */
   static const StateDescriptor _$prop__hasError___$ErrorBoundaryState =
       const StateDescriptor(_$key__hasError___$ErrorBoundaryState);
+  static const StateDescriptor
+      _$prop__showFallbackUIOnError___$ErrorBoundaryState =
+      const StateDescriptor(_$key__showFallbackUIOnError___$ErrorBoundaryState);
   static const String _$key__hasError___$ErrorBoundaryState =
       'ErrorBoundaryState.hasError';
+  static const String _$key__showFallbackUIOnError___$ErrorBoundaryState =
+      'ErrorBoundaryState.showFallbackUIOnError';
 
   static const List<StateDescriptor> $state = const [
-    _$prop__hasError___$ErrorBoundaryState
+    _$prop__hasError___$ErrorBoundaryState,
+    _$prop__showFallbackUIOnError___$ErrorBoundaryState
   ];
   static const List<String> $stateKeys = const [
-    _$key__hasError___$ErrorBoundaryState
+    _$key__hasError___$ErrorBoundaryState,
+    _$key__showFallbackUIOnError___$ErrorBoundaryState
   ];
 }
 
