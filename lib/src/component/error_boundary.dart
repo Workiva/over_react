078c84fe7ca0dd4d4053bbb0582e602791a90d8c
Aaron Lademann <aaron.lademann@workiva.com>
2019-10-08 08:26:43 -0700

    Use a managed timer

--- lib/src/component/error_boundary.dart
+++ lib/src/component/error_boundary.dart
@@ -361,7 +361,7 @@ class ErrorBoundaryComponent<T extends ErrorBoundaryProps, S extends ErrorBounda
   void _startIdenticalErrorTimer() {
     if (_identicalErrorTimer != null) return;
 
-    _identicalErrorTimer = Timer(props.identicalErrorFrequencyTolerance, _resetInternalErrorTracking);
+    _identicalErrorTimer = getManagedTimer(props.identicalErrorFrequencyTolerance, _resetInternalErrorTracking);
   }
 
   /// Resets all the internal fields used by [_handleErrorInComponentTree], and cancels
