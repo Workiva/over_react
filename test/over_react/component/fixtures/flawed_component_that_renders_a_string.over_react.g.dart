e8ced3b45c6a92843ddb1c38e30f2bb5d0c630fa
Keal Jones <keal.jones@workiva.com>
2019-10-10 15:17:16 -0700

    regenerate files

--- test/over_react/component/fixtures/flawed_component_that_renders_a_string.over_react.g.dart
+++ test/over_react/component/fixtures/flawed_component_that_renders_a_string.over_react.g.dart
@@ -43,8 +43,7 @@ class _$$FlawedWithStringChildProps extends _$FlawedWithStringChildProps
     with _$FlawedWithStringChildPropsAccessorsMixin
     implements FlawedWithStringChildProps {
   // This initializer of `_props` to an empty map, as well as the reassignment
-  // of `_props` in the constructor body is necessary to work around an unknown ddc issue.
-  // See <https://jira.atl.workiva.net/browse/CPLAT-4673> for more details
+  // of `_props` in the constructor body is necessary to work around a DDC bug: https://github.com/dart-lang/sdk/issues/36217
   _$$FlawedWithStringChildProps(Map backingMap) : this._props = {} {
     this._props = backingMap ?? {};
   }
@@ -61,7 +60,7 @@ class _$$FlawedWithStringChildProps extends _$FlawedWithStringChildProps
   /// The [ReactComponentFactory] associated with the component built by this class.
   @override
   ReactComponentFactoryProxy get componentFactory =>
-      $FlawedWithStringChildComponentFactory;
+      super.componentFactory ?? $FlawedWithStringChildComponentFactory;
 
   /// The default namespace for the prop getters/setters generated for this class.
   @override
