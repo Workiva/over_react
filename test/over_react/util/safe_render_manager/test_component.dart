35d900d68e9dd1ddba6790fe8198e017b1c2a9a6
Greg Littlefield <greg.littlefield@workiva.com>
2019-10-29 09:42:03 -0700

    Dart 1 compatibility fixes

--- test/over_react/util/safe_render_manager/test_component.dart
+++ test/over_react/util/safe_render_manager/test_component.dart
@@ -1,9 +1,12 @@
 import 'package:over_react/over_react.dart';
 
+// ignore: uri_has_not_been_generated
 part 'test_component.over_react.g.dart';
 
 @Factory()
-UiFactory<TestProps> Test = _$Test;
+UiFactory<TestProps> Test =
+    // ignore: undefined_identifier
+    _$Test;
 
 @Props()
 class _$TestProps extends UiProps {
@@ -49,3 +52,11 @@ class TestComponent extends UiComponent<TestProps> {
 }
 
 typedef void LifecycleCallback();
+
+class TestProps extends _$TestProps
+    with
+        // ignore: mixin_of_non_class, undefined_class
+        _$TestPropsAccessorsMixin {
+  // ignore: undefined_identifier, undefined_class, const_initialized_with_non_constant_value
+  static const PropsMeta meta = _$metaForTestProps;
+}
