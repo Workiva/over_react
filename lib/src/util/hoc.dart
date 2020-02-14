0a03ac59932018e9859438b75fb6c493531fd1d5
Joseph Bingham <joe.bingham@workiva.com>
2020-02-14 08:40:21 -0700

    Add hoc doc comment

--- lib/src/util/hoc.dart
+++ lib/src/util/hoc.dart
@@ -1,5 +1,28 @@
 
-// R doesn't have bounds of UiHoc since the analyzer doesn't handle that well.
+/// Returns a single HOC instance from multiple of the same instance.
+///
+/// When it is necessary to wrap a since `UiComponentFactory` in multiple HOCs,
+/// this can be used to declare those HOCs in a flat list, rather than nested with
+/// excessive indentation.
+///
+/// [functions]: A list of HOCs (usually UiFactories) that will be combined into
+/// a single HOC with the same function signature.
+///
+/// __EXAMPLE:__
+/// ```dart
+/// UiFactory<ReduxBigBlockProps> ConnectedReduxBigBlock = composeHocs([
+///   connect<RandomColorStore, ReduxBigBlockProps>(
+///     // `connect` implementation
+///   ),
+///   connect<LowLevelStore, ReduxBigBlockProps>(
+///     // `connect` implementation
+///   ),
+///   connect<AnotherColorStore, ReduxBigBlockProps>(
+///     // `connect` implementation
+///     // `connect` implementation
+///   ),
+/// ])(ReduxBigBlock);
+/// ```
 R Function(A) composeHocs<R, A extends R>(Iterable<R Function(A)> functions) {
   return functions.reduce((a, b) => (result) => a(b(result)));
 }
