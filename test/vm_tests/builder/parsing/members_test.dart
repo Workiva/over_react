e1b730607cf6a8612e6547e10b63245985df685f
Joseph Bingham <joe.bingham@workiva.com>
2020-03-11 16:48:14 -0700

    Clean up imports

--- test/vm_tests/builder/parsing/members_test.dart
+++ test/vm_tests/builder/parsing/members_test.dart
@@ -1,7 +1,5 @@
 @TestOn('vm')
 import 'package:analyzer/dart/analysis/utilities.dart';
-import 'package:analyzer/dart/ast/ast.dart';
-import 'package:meta/meta.dart';
 import 'package:over_react/src/builder/generation/parsing.dart';
 import 'package:source_span/source_span.dart';
 import 'package:test/test.dart';
