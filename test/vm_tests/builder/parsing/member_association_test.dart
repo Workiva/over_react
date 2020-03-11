f1d14bfd55f8fa7ee5125c5da0bdf5eca4a4d5b1
Joseph Bingham <joe.bingham@workiva.com>
2020-03-11 16:46:26 -0700

    Move mockComponentDeclarations into utils

--- test/vm_tests/builder/parsing/member_association_test.dart
+++ test/vm_tests/builder/parsing/member_association_test.dart
@@ -4,6 +4,8 @@ import 'package:over_react/src/builder/generation/parsing/member_association.dar
 import 'package:over_react/src/builder/generation/parsing.dart';
 import 'package:test/test.dart';
 
+import '../util.dart';
+
 main() {
   group('Member Association', () {
     BoilerplateMembers members;
@@ -44,8 +46,8 @@ main() {
       return stateMixins ??= (members ?? getAllExampleBoilerplateMembers()).stateMixins;
     }
 
-    Iterable<BoilerplateMember> getBoilerplateMembersFor(VersionOptions version) {
-      final unit = parseString(content: boilerplateStrings[version]).unit;
+    Iterable<BoilerplateMember> getBoilerplateMembersFor(BoilerplateVersions version) {
+      final unit = parseString(content: getBoilerplateString(version: version)).unit;
 
       return BoilerplateMembers.detect(unit).allMembers;
     }
@@ -75,39 +77,39 @@ main() {
       group('and the BoilerplateMember is a', () {
         test('BoilerplateComponent', () {
           final component =
-              getBoilerplateMembersFor(VersionOptions.v4).whereType<BoilerplateComponent>().first;
+              getBoilerplateMembersFor(BoilerplateVersions.v4).whereType<BoilerplateComponent>().first;
           expect(normalizeNameAndRemoveSuffix(component), 'Foo');
         });
 
         test('BoilerplateProps', () {
           final props =
-              getBoilerplateMembersFor(VersionOptions.v3).whereType<BoilerplateProps>().first;
+              getBoilerplateMembersFor(BoilerplateVersions.v3).whereType<BoilerplateProps>().first;
           expect(normalizeNameAndRemoveSuffix(props), 'Foo');
         });
 
         test('BoilerplatePropsMixin', () {
           final propsMixin =
-              getBoilerplateMembersFor(VersionOptions.v4).whereType<BoilerplatePropsMixin>().first;
+              getBoilerplateMembersFor(BoilerplateVersions.v4).whereType<BoilerplatePropsMixin>().first;
           expect(normalizeNameAndRemoveSuffix(propsMixin), 'Foo');
 
           final propsMixinWithMixinInName =
-              getBoilerplateMembersFor(VersionOptions.v5).whereType<BoilerplatePropsMixin>().first;
+              getBoilerplateMembersFor(BoilerplateVersions.v5).whereType<BoilerplatePropsMixin>().first;
           expect(normalizeNameAndRemoveSuffix(propsMixinWithMixinInName), 'Foo');
         });
 
         test('BoilerplateState', () {
           final state =
-              getBoilerplateMembersFor(VersionOptions.v3).whereType<BoilerplateState>().first;
+              getBoilerplateMembersFor(BoilerplateVersions.v3).whereType<BoilerplateState>().first;
           expect(normalizeNameAndRemoveSuffix(state), 'Foo');
         });
 
         test('BoilerplateStateMixin', () {
           final stateMixin =
-              getBoilerplateMembersFor(VersionOptions.v4).whereType<BoilerplateStateMixin>().first;
+              getBoilerplateMembersFor(BoilerplateVersions.v4).whereType<BoilerplateStateMixin>().first;
           expect(normalizeNameAndRemoveSuffix(stateMixin), 'Foo');
 
           final stateMixinWithMixinInName =
-              getBoilerplateMembersFor(VersionOptions.v5).whereType<BoilerplateStateMixin>().first;
+              getBoilerplateMembersFor(BoilerplateVersions.v5).whereType<BoilerplateStateMixin>().first;
           expect(normalizeNameAndRemoveSuffix(stateMixinWithMixinInName), 'Foo');
         });
       });
@@ -317,139 +319,3 @@ main() {
     });
   });
 }
-
-enum VersionOptions { v2, v3, v4, v5 }
-const boilerplateStrings = {
-  VersionOptions.v2: r'''
-          @Factory()
-          UiFactory<FooProps> Foo = _$Foo;
-
-          @Props()
-          class _$FooProps extends UiProps {}
-
-          class FooProps extends _$FooProps with _$FooPropsAccessorsMixin {}
-
-          @State()
-          class _$FooState extends UiState with _$FooStateAccessorsMixin {}
-
-          class FooState extends _$FooState with _$FooStateAccessorsMixin {}
-
-          @Component2()
-          class FooComponent extends UiStatefulComponent2<FooProps, FooState>{}
-      ''',
-  VersionOptions.v3: r'''
-          @Factory()
-          UiFactory<FooProps> Foo = _$Foo;
-
-          @Props()
-          class _$FooProps extends UiProps {}
-
-          @State()
-          class _$FooState extends _$FooState with _$FooStateAccessorsMixin {}
-
-          @Component2()
-          class FooComponent extends UiStatefulComponent2<FooProps, FooState>{}
-        ''',
-  VersionOptions.v4: r'''
-          UiFactory<FooProps> Foo = _$Foo;
-
-          mixin FooProps on UiProps {}
-
-          mixin FooState on UiState {}
-
-          class FooComponent extends UiStatefulComponent<FooProps, FooState>{}
-        ''',
-  VersionOptions.v5: r'''
-          UiFactory<FooProps> Foo = _$Foo;
-
-          mixin FooPropsMixin on UiProps {}
-
-          class FooProps = UiProps with FooPropsMixin;
-
-          mixin FooStateMixin on UiState {}
-          
-          class FooState = UiState with FooStateMixin;
-
-          class FooComponent extends UiStatefulComponent<FooProps, FooState>{}
-        ''',
-};
-
-const mockComponentDeclarations = r'''
-  // -------------------------- Version.v2_legacyBackwardsCompat --------------------------
-  @Factory()
-  UiFactory<FirstFooProps> FirstFoo = _$FirstFoo;
-
-  @Props()
-  class _$FirstFooProps extends UiProps {}
-
-  class FirstFooProps extends _$FirstFooProps with _$FirstFooPropsAccessorsMixin {}
-
-  @State()
-  class _$FirstFooState extends UiState with _$FirstFooStateAccessorsMixin {}
-
-  class FirstFooState extends _$FirstFooState with _$FirstFooStateAccessorsMixin {}
-
-  @Component2()
-  class FirstFooComponent extends UiStatefulComponent2<FirstFooProps, FooState>{}
-  
-  // -------------------------- Version.v3_legacyDart2Only --------------------------
-  @Factory()
-  UiFactory<SecondFooProps> SecondFoo = _$SecondFoo;
-
-  @Props()
-  class _$SecondFooProps extends UiProps {}
-
-  @State()
-  class _$SecondFooState extends _$SecondFooState with _$SecondFooStateAccessorsMixin {}
-
-  @Component2()
-  class SecondFooComponent extends UiStatefulComponent2<SecondFooProps, SecondFooState>{}
-  
-  // -------------------------- Version.v3_legacyDart2Only --------------------------
-  // But with dissassociated names to force grouping logic to utilize generics
-  @Factory()
-  UiFactory<HowAboutARandomNameForLegacyProps> ThisIsABadLegacyFactoryName = _$ThisIsABadLegacyFactoryName;
-
-  @Props()
-  class _$HowAboutARandomNameForLegacyProps extends UiProps {}
-
-  @State()
-  class _$AVeryRandomNameForLegacyState extends _$AVeryRandomNameForLegacyState with _$AVeryRandomNameForLegacyStateAccessorsMixin {}
-
-  @Component2()
-  class IsThisEvenAComponentNameLegacyComponent extends UiStatefulComponent2<HowAboutARandomNameForLegacyProps, AVeryRandomNameForLegacyState>{}
-  
-
-  // -------------------------- Version.v4_mixinBased --------------------------
-  UiFactory<ThirdFooProps> ThirdFoo = _$ThirdFoo;
-
-  mixin ThirdFooProps on UiProps {}
-
-  mixin ThirdFooState on UiState {}
-
-  class ThirdFooComponent extends UiStatefulComponent<ThirdFooProps, ThirdFooState>{}
-
-  // -------------------------- Version.v4_mixinBased --------------------------
-  // But with extended props / state aliase syntax
-  UiFactory<FourthFooProps> FourthFoo = _$FourthFoo;
-
-  mixin FourthFooPropsMixin on UiProps {}
-
-  class FourthFooProps = UiProps with FourthFooPropsMixin;
-
-  mixin FourthFooStateMixin on UiState {}
-  
-  class FourthFooState = UiState with FourthFooStateMixin;
-
-  class FourthFooComponent extends UiStatefulComponent<FourthFooProps, FourthFooState>{}
-  
-  // -------------------------- Version.v4_mixinBased --------------------------
-  // But with dissassociated names to force grouping logic to utilize generics
-  UiFactory<HowAboutARandomNameForProps> ThisIsABadFactoryName = _$IsThisEvenAComponentName;
-
-  mixin HowAboutARandomNameForProps on UiProps {}
-
-  mixin AVeryRandomNameForState on UiState {}
-
-  class IsThisEvenAComponentNameComponent extends UiStatefulComponent<HowAboutARandomNameForProps, AVeryRandomNameForState>{}
-''';
