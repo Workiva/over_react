08cea05425620cff9d1551789e9c186d3792b0d5
Greg Littlefield <greg.littlefield@workiva.com>
2020-04-15 11:10:02 -0700

    Add missing copyright notices

--- web/component2/src/demos/custom_error_boundary.dart
+++ web/component2/src/demos/custom_error_boundary.dart
@@ -1,3 +1,17 @@
+// Copyright 2020 Workiva Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 // ignore_for_file: deprecated_member_use_from_same_package
 import 'package:over_react/over_react.dart';
 import 'package:react/react_client/react_interop.dart';
