9850d5a19cd9a0ec08408e4567bf5bfde0226287
Sydney Jodon <sydney.jodon@workiva.com>
2020-06-16 15:44:34 -0700

    Connect comment to corresponding factory

--- over_react_analyzer_plugin/lib/src/diagnostic/incorrect_doc_comment_location.dart
+++ over_react_analyzer_plugin/lib/src/diagnostic/incorrect_doc_comment_location.dart
@@ -6,7 +6,7 @@ import 'package:over_react_analyzer_plugin/src/diagnostic_contributor.dart';
 class IncorrectDocCommentLocationDiagnostic extends DiagnosticContributor {
   static const code = DiagnosticCode(
     'over_react_incorrect_doc_comment_location',
-    "Doc comments should be above the factory declaration",
+    "Doc comments should be above the factory declaration.",
     AnalysisErrorSeverity.INFO,
     AnalysisErrorType.LINT,
   );
@@ -19,27 +19,40 @@ class IncorrectDocCommentLocationDiagnostic extends DiagnosticContributor {
 
   @override
   computeErrors(result, collector) async {
-    final visitor = ClassVisitor();
+    final visitor = CommentsVisitor();
 
     result.unit.accept(visitor);
 
-    final misplacedDocComments = visitor.misplacedDocComments;
+    final classesWithMisplacedDocComments = visitor.classesWithMisplacedDocComments;
     final factoryList = visitor.factoryList;
 
-    for (final comment in misplacedDocComments) {
-      await collector.addErrorWithFix(
-        code,
-        result.locationFor(comment),
-        fixKind: fixKind,
-        computeFix: () => buildFileEdit(result, (builder) {
-          builder.addDeletion(range.startOffsetEndOffset(comment.offset - 1, comment.end));
-          builder.addInsertion(factoryList.first.offset, (builder) {
-            for (final line in comment.childEntities) {
-              builder.write('${line.toString()}\n');
-            }
-          });
-        }),
-      );
+    for (final classWithComment in classesWithMisplacedDocComments) {
+      final docComment = classWithComment.documentationComment;
+      final componentFactory = getComponentFactory(classWithComment, factoryList);
+      if(componentFactory != null) {
+        await collector.addErrorWithFix(
+          code,
+          result.locationFor(docComment),
+          fixKind: fixKind,
+          computeFix: () => buildFileEdit(result, (builder) {
+            builder.addDeletion(range.startOffsetEndOffset(
+              // [offset - 1] to remove the extra line
+              docComment.offset - 1,
+              docComment.end,
+            ));
+            builder.addInsertion(componentFactory.offset, (builder) {
+              for (final line in docComment.childEntities) {
+                builder.write('${line.toString()}\n');
+              }
+            });
+          }),
+        );
+      } else {
+        collector.addError(
+          code,
+          result.locationFor(docComment),
+        );
+      }
     }
   }
 }
@@ -55,22 +68,64 @@ const overReactBaseComponentClasses = {
   'FluxUiStatefulComponent2',
 };
 
-bool isComponentFactory(TopLevelVariableDeclaration expr) {
-  return expr.variables.type.beginToken.toString() == 'UiFactory';
+const overReactClassEndings = {
+  'Component',
+  'State',
+  'StateMixin',
+  'Props',
+  'PropsMixin',
+};
+
+/// Returns the name of [factory].
+String getFactoryName(TopLevelVariableDeclaration factory) {
+  return factory.variables.variables.first.name.name;
 }
 
+/// Returns [className] with the `Component`, `Props`, `PropsMixin`, `State`,
+/// or `StateMixin` ending removed.
+///
+/// This should be the factory name (see [getComponentFactory]).
+String removeClassNameEnding(String className) {
+  if(overReactClassEndings.any((ending) => className.contains(ending))) {
+    final ending = overReactClassEndings.firstWhere((ending) {
+      return className.endsWith(ending);
+    });
+    return className.substring(0, className.lastIndexOf(ending));
+  }
+  return className;
+}
+
+/// Returns the component factory in [factoryList] that corresponds to the
+/// component class or props/state class or mixin [classDecl].
+TopLevelVariableDeclaration getComponentFactory(ClassOrMixinDeclaration classDecl, List<TopLevelVariableDeclaration> factoryList) {
+  if(factoryList.isEmpty) {
+    return null;
+  } else if(factoryList.length == 1) {
+    return factoryList.first;
+  }
+  return factoryList.firstWhere((factory) => removeClassNameEnding(classDecl.name.name) == getFactoryName(factory));
+}
+
+/// Returns whether or not [decl] is a `UiFactory`.
+bool isComponentFactory(TopLevelVariableDeclaration decl) {
+  return decl.variables.type.beginToken.toString() == 'UiFactory';
+}
+
+/// Returns whether or not [classDecl] is a props or state class.
 bool isPropsOrStateClass(ClassDeclaration classDecl) {
   return classDecl.declaredElement.allSupertypes.any((m) {
     return m.getDisplayString() == 'UiProps' || m.getDisplayString() == 'UiState';
   });
 }
 
+/// Returns whether or not [mixinDecl] is a props or state mixin.
 bool isPropsOrStateMixin(MixinDeclaration mixinDecl) {
   return mixinDecl.onClause.superclassConstraints.any((m) {
     return m.name.name == 'UiProps' || m.name.name == 'UiState';
   });
 }
 
+/// Returns whether or not [classDecl] is a `UiComponent` class.
 bool isComponentClass(ClassDeclaration classDecl) {
   return classDecl.declaredElement.allSupertypes.any((m) {
     return overReactBaseComponentClasses.any((c) {
@@ -79,8 +134,8 @@ bool isComponentClass(ClassDeclaration classDecl) {
   });
 }
 
-class ClassVisitor extends SimpleAstVisitor<void> {
-  List<Comment> misplacedDocComments = [];
+class CommentsVisitor extends SimpleAstVisitor<void> {
+  List<ClassOrMixinDeclaration> classesWithMisplacedDocComments = [];
   List<TopLevelVariableDeclaration> factoryList = [];
   @override
   void visitCompilationUnit(CompilationUnit node) {
@@ -96,17 +151,15 @@ class ClassVisitor extends SimpleAstVisitor<void> {
 
   @override
   void visitClassDeclaration(ClassDeclaration node) {
-    final docComment = node.documentationComment;
-    if (docComment != null && (isComponentClass(node) || isPropsOrStateClass(node))) {
-      misplacedDocComments.add(docComment);
+    if (node.documentationComment != null && (isComponentClass(node) || isPropsOrStateClass(node))) {
+      classesWithMisplacedDocComments.add(node);
     }
   }
 
   @override
   void visitMixinDeclaration(MixinDeclaration node) {
-    final docComment = node.documentationComment;
-    if (docComment != null && isPropsOrStateMixin(node)) {
-      misplacedDocComments.add(docComment);
+    if (node.documentationComment != null && isPropsOrStateMixin(node)) {
+      classesWithMisplacedDocComments.add(node);
     }
   }
 }
