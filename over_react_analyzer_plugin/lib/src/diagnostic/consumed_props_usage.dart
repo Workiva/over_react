c365ee339158eb907531c80c8aa764298cad8933
Sydney Jodon <sydney.jodon@workiva.com>
2020-06-18 10:49:56 -0700

    Initial implementation

---
+++ over_react_analyzer_plugin/lib/src/diagnostic/consumed_props_usage.dart
@@ -0,0 +1,80 @@
+import 'dart:math';
+
+import 'package:analyzer/dart/ast/ast.dart';
+import 'package:analyzer/dart/ast/visitor.dart';
+import 'package:over_react_analyzer_plugin/src/diagnostic_contributor.dart';
+import 'package:over_react_analyzer_plugin/src/util/react_types.dart';
+
+class ConsumedPropsUsageDiagnostic extends DiagnosticContributor {
+  static const code = DiagnosticCode(
+    'over_react_bool_prop_name_readability',
+    "{0}    **     {1}",
+    AnalysisErrorSeverity.INFO,
+    AnalysisErrorType.LINT,
+  );
+
+  static final fixKind = FixKind(
+    code.name,
+    200,
+    'Move comment above factory',
+  );
+
+  @override
+  computeErrors(result, collector) async {
+    final visitor = PropsVisitor();
+
+    result.unit.accept(visitor);
+
+    final returnMixins = visitor.returnMixins;
+
+    for (final r in returnMixins) {
+      final body = r.body;
+      if (body is ExpressionFunctionBody) {
+        final expression = body.expression;
+        if (expression is ListLiteral && expression.elements.isNotEmpty) {
+          final elements = expression.elements;
+          final shouldAddError = elements.every((element) =>
+              element is MethodInvocation &&
+              element.methodName.name == 'forMixin' &&
+              element.realTarget.toSource() == 'propsMeta');
+          if(shouldAddError) {
+              await collector.addErrorWithFix(
+                code,
+                result.locationFor(expression),
+                errorMessageArgs: [
+                  '',
+                  '',
+                ],
+                fixKind: fixKind,
+                computeFix: () => buildFileEdit(result, (builder) {
+
+                }),
+              );
+          }
+        }
+      }
+    }
+  }
+}
+
+class PropsVisitor extends SimpleAstVisitor<void> {
+  List<MethodDeclaration> returnMixins = [];
+
+  @override
+  void visitCompilationUnit(CompilationUnit node) {
+    node.visitChildren(this);
+  }
+
+  @override
+  void visitClassDeclaration(ClassDeclaration node) {
+    node.visitChildren(this);
+  }
+
+  @override
+  void visitMethodDeclaration(MethodDeclaration node) {
+    final classNode = node.thisOrAncestorOfType<ClassDeclaration>();
+    if (classNode.declaredElement.isComponentClass && node.name.name == 'consumedProps') {
+      returnMixins.add(node);
+    }
+  }
+}
--- playground/web/foo.dart
+++ playground/web/foo.dart
@@ -2,11 +2,44 @@ import 'package:over_react/over_react.dart';
 
 part 'foo.over_react.g.dart';
 
-UiFactory<FooProps> Foo = _$Foo; // ignore: undefined_identifier
+UiFactory<FooPropsMixin> Foo = _$Foo; // ignore: undefined_identifier
 
-mixin FooProps on UiProps {}
+mixin FooPropsMixin on UiProps {}
 
-class FooComponent extends UiComponent2<FooProps> {
+class FooComponent extends UiComponent2<FooPropsMixin> {
+  @override
+  Iterable<ConsumedProps> get consumedProps => [];
+
+  @override
+  render() {}
+}
+
+UiFactory<BarPropsMixin> Bar = _$Bar; // ignore: undefined_identifier
+
+mixin BarPropsMixin on UiProps {}
+
+class BarComponent extends UiComponent2<BarPropsMixin> {
+  @override
+  get consumedProps => [
+    ...propsMeta.forMixins({FooPropsMixin, BarPropsMixin}),
+    PropsMeta.forSimpleKey('_onChangePropKey'),
+  ];
+
+  @override
+  render() {}
+}
+
+UiFactory<Foo2PropsMixin> Foo2 = _$Foo2; // ignore: undefined_identifier
+
+mixin Foo2PropsMixin on UiProps {}
+
+class Foo2Component extends UiComponent2<Foo2PropsMixin> {
+  @override
+  Iterable<ConsumedProps> get consumedProps => [
+        propsMeta.forMixin(Foo2PropsMixin),
+        propsMeta.forMixin(FooPropsMixin),
+      ];
+
   @override
   render() {}
 }
