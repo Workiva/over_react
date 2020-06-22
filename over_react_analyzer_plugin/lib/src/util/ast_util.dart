23f1be84000b37ff800f9ce5b42585946bcba53e
Greg Littlefield <greg.littlefield@workiva.com>
2020-06-22 16:37:48 -0700

    Add common utility around getting return expressions

--- over_react_analyzer_plugin/lib/src/util/ast_util.dart
+++ over_react_analyzer_plugin/lib/src/util/ast_util.dart
@@ -7,9 +7,11 @@ import 'dart:collection';
 // be copied and pasted into this analyzer package (if still needed).
 import 'package:analyzer/analyzer.dart';
 import 'package:analyzer/dart/ast/ast.dart';
+import 'package:analyzer/dart/ast/visitor.dart';
 import 'package:analyzer/source/line_info.dart';
 
 export 'package:over_react/src/builder/parsing/ast_util.dart';
+export 'package:over_react/src/builder/parsing/util.dart';
 
 /// Returns a lazy iterable of all descendants of [node], in breadth-first order.
 Iterable<AstNode> allDescendants(AstNode node) sync* {
@@ -42,3 +44,53 @@ int nextLine(int offset, LineInfo lineInfo) {
 }
 
 bool isAConstantValue(Expression expr) => expr.accept(ConstantEvaluator()) != ConstantEvaluator.NOT_A_CONSTANT;
+
+extension FunctionBodyUtils on FunctionBody {
+  /// An of expressions representing:
+  ///
+  /// - for `ExpressionFunctionBody`: the single expression
+  /// - for `BlockFunctionBody`: the expressions within all `return` statements for that function,
+  ///   excluding return statements which have no value (`return;`) or belong to nested functions
+  ///
+  /// Useful when you want to get all of the potential return values, but don't care what
+  /// kind of function body you're dealing with.
+  Iterable<Expression> get returnExpressions sync* {
+    final self = this; // Need this for type promotion in is-checks.
+    if (self is ExpressionFunctionBody) {
+      yield self.expression;
+    } else if (self is BlockFunctionBody) {
+      for (final statement in self.returnStatements) {
+        // Expression is null for returns statements without values (`return;`). Skip those.
+        if (statement.expression != null) {
+          yield statement.expression;
+        }
+      }
+    }
+  }
+}
+
+extension BlockFunctionBodyUtils on BlockFunctionBody {
+  /// Returns all return statements for this function body.
+  ///
+  /// Includes return statements nested in `if`/`for`/other blocks,
+  /// and does not include return statements for nested functions.
+  List<ReturnStatement> get returnStatements {
+    final visitor = _ReturnStatementsForBodyVisitor();
+    accept(visitor);
+    return visitor.returnStatementsForBody;
+  }
+}
+
+class _ReturnStatementsForBodyVisitor extends RecursiveAstVisitor {
+  final returnStatementsForBody = <ReturnStatement>[];
+
+  @override
+  void visitReturnStatement(ReturnStatement node) {
+    returnStatementsForBody.add(node);
+  }
+
+  @override
+  void visitFunctionExpression(FunctionExpression node) {
+    // Don't call super so we don't traverse inside other functions.
+  }
+}
