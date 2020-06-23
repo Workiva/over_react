081b7753b5e32efe78399780b291abbbc5355283
Aaron Lademann <aaron.lademann@workiva.com>
2020-06-23 13:47:02 -0700

    Add docs for PropTypesReturnValueDiagnostic

--- tools/analyzer_plugin/lib/src/diagnostic/proptypes_return_value.dart
+++ tools/analyzer_plugin/lib/src/diagnostic/proptypes_return_value.dart
@@ -6,16 +6,64 @@ import 'package:over_react_analyzer_plugin/src/diagnostic_contributor.dart';
 import 'package:over_react_analyzer_plugin/src/util/ast_util.dart';
 import 'package:over_react_analyzer_plugin/src/util/react_types.dart';
 
+const _desc = r'Avoid throwing errors within propTypes.';
+// <editor-fold desc="Documentation Details">
+const _details = '''
+
+While any thrown errors/exceptions are caught by React and surfaced as prop warnings, throwing is not recommended.
+
+Return the error instead.
+
+**GOOD:**
+```
+class FooComponent extends UiComponent2<FooProps> {
+  @override
+  get propTypes => {
+    keyForProp((p) => p.prop1): (props, info) {
+      if (props.prop1 == null) {
+        return PropError.required(info.propName);
+      } else if (props.prop1 > 0) {
+        return PropError.value(props.prop1, info.propName);
+      }
+      return null;
+    },
+  };
+}
+```
+
+**BAD:**
+```
+class FooComponent extends UiComponent2<FooProps> {
+  @override
+  get propTypes => {
+    keyForProp((p) => p.prop1): (props, info) {
+      if (props.prop1 == null) {
+        throw PropError.required(info.propName);
+      } else if (props.prop1 > 0) {
+        throw PropError.value(props.prop1, info.propName);
+      }
+      return null;
+    },
+  };
+}
+```
+
+''';
+// </editor-fold>
+
 /// A warning that appears when `throw`ing a `PropError` instead of returning it within `UiComponent2.propTypes`.
 ///
 /// {@category Diagnostics}
 /// {@subCategory Warnings}
 class PropTypesReturnValueDiagnostic extends DiagnosticContributor {
+  @DocsMeta(_desc, details: _details)
   static final code = DiagnosticCode(
-      'over_react_proptypes_do_not_throw',
-      "propTypes errors are expected to be returned, not thrown. While any thrown errors/exceptions are caught by React and surfaced as prop warnings, throwing is not recommended.",
-      AnalysisErrorSeverity.WARNING,
-      AnalysisErrorType.STATIC_TYPE_WARNING);
+    'over_react_proptypes_do_not_throw',
+    "propTypes errors are expected to be returned, not thrown.",
+    AnalysisErrorSeverity.WARNING,
+    AnalysisErrorType.STATIC_TYPE_WARNING,
+    correction: 'Return the error instead of throwing it.',
+  );
 
   static final fixKind = FixKind(code.name, 200, 'Return the error');
 
