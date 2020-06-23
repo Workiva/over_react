74ea9df6f8909ef0ec6414d2aebd8a9a7b2c6829
Joe Bingham <joe.bingham@workiva.com>
2020-06-23 10:23:56 -0700

    Add CI for plugin

--- tools/analyzer_plugin/lib/src/util/react_types.dart
+++ tools/analyzer_plugin/lib/src/util/react_types.dart
@@ -23,7 +23,7 @@ extension on Element /*?*/ {
   }
 
   bool isTypeFromPackage(String typeName, String packageName) =>
-      this?.name == typeName && isDeclaredInPackage(packageName);
+      this?.name == typeName && isDeclaredInPackage(packageName); // ignore: unnecessary_this
 }
 
 extension on Element {
--- tools/analyzer_plugin/tool/dart_dev/config.dart
+++ tools/analyzer_plugin/tool/dart_dev/config.dart
@@ -1,6 +1,16 @@
 import 'package:dart_dev/dart_dev.dart';
+import 'package:glob/glob.dart';
 
 final config = {
   ...coreConfig,
-  'format': FormatTool()..formatterArgs = ['--line-length=120'],
+  'analyze': AnalyzeTool()
+    ..include = [
+      Glob('lib/**.dart'),
+      Glob('tool/**.dart'),
+    ],
+  'format': FormatTool()
+    ..formatterArgs = ['--line-length=120']
+    ..exclude = [
+      Glob('playground/**'),
+    ],
 };
