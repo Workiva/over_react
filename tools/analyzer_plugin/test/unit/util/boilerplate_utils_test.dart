c5051ef3139ad6732083a97beff54717a44b0735
Sydney Jodon <sydney.jodon@workiva.com>
2020-06-24 11:25:19 -0700

    Add tests for addOverReactGeneratedPartDirective

--- tools/analyzer_plugin/test/unit/util/boilerplate_utils_test.dart
+++ tools/analyzer_plugin/test/unit/util/boilerplate_utils_test.dart
@@ -1,5 +1,10 @@
+import 'package:analyzer/dart/analysis/results.dart';
 import 'package:analyzer/dart/ast/ast.dart';
+import 'package:analyzer_plugin/protocol/protocol_common.dart';
+import 'package:over_react_analyzer_plugin/src/diagnostic_contributor.dart';
+import 'package:over_react_analyzer_plugin/src/util/ast_util.dart';
 import 'package:over_react_analyzer_plugin/src/util/boilerplate_utils.dart';
+import 'package:over_react_analyzer_plugin/src/util/util.dart';
 import 'package:test/test.dart';
 
 import '../../test_util.dart';
@@ -34,6 +39,19 @@ const sourceWithInvalidPart = /*language=dart*/ r'''
   }
 ''';
 
+const sourceWithNoPart = /*language=dart*/ r'''
+  import 'package:over_react/over_react.dart';
+
+  UiFactory<FooProps> Foo = _$Foo; // ignore: undefined_identifier
+
+  mixin FooProps on UiProps {}
+
+  class FooComponent extends UiComponent2<FooProps> {
+    @override
+    render() {}
+  }
+''';
+
 main() {
   group('boilerplate_util', () {
     group('getOverReactGeneratedPartDirective', () {
@@ -51,18 +69,7 @@ main() {
         });
 
         test('there is no PartDirective', () {
-          final unit = parseAndGetUnit(/*language=dart*/ r'''
-          import 'package:over_react/over_react.dart';
-          
-          UiFactory<FooProps> Foo = _$Foo; // ignore: undefined_identifier
-          
-          mixin FooProps on UiProps {}
-          
-          class FooComponent extends UiComponent2<FooProps> {
-            @override
-            render() {}
-          }
-        ''');
+          final unit = parseAndGetUnit(sourceWithNoPart);
           expect(getOverReactGeneratedPartDirective(unit), isNull);
         });
       });
@@ -95,6 +102,83 @@ main() {
         });
       });
     });
+
+    group('addOverReactGeneratedPartDirective', () {
+      test('does nothing if there is already a valid part directive', () async {
+        final sourceFileEdits = await getSourceFileEdits(
+          sourceWithValidPart,
+          (builder, result) => addOverReactGeneratedPartDirective(builder, result.unit, result.uri),
+          path: 'foo.dart',
+          shouldThrowErrors: false,
+        );
+        expect(sourceFileEdits, isEmpty);
+      });
+
+      group('adds a valid part directive', () {
+        test('when there are no directives in the file', () async {
+          final sourceFileEdits = await getSourceFileEdits(
+            /*language=dart*/ r'''
+              UiFactory<FooProps> Foo = _$Foo; // ignore: undefined_identifier
+            
+              mixin FooProps on UiProps {}
+            
+              class FooComponent extends UiComponent2<FooProps> {
+                @override
+                render() {}
+              }
+            ''',
+            (builder, result) => addOverReactGeneratedPartDirective(builder, result.unit, result.uri),
+            path: 'foo.dart',
+            shouldThrowErrors: false,
+          );
+          final editList = sourceFileEdits.firstOrNull?.edits;
+
+          expect(editList, isNotNull);
+          expect(editList.length, 1, reason: 'there should be one edit in the file');
+          expect(editList.first.offset, 0);
+          expect(editList.first.replacement, 'part \'foo.over_react.g.dart\';\n\n');
+        });
+
+        test('when there are no part directives in the file', () async {
+          final result = await parseAndGetResolvedUnit(
+            sourceWithNoPart,
+            path: 'foo.dart',
+            shouldThrowErrors: false,
+          );
+          final sourceChange = await buildFileEdit(result, (builder) {
+            addOverReactGeneratedPartDirective(builder, result.unit, result.uri);
+          });
+          final editList = sourceChange.edits?.firstOrNull?.edits;
+
+          expect(editList, isNotNull);
+          expect(editList.length, 1, reason: 'there should be one edit in the file');
+
+          final offset = result.unit.lineInfo
+              .getOffsetOfLineAfter(nextLine(result.unit.directives.last.end, result.unit.lineInfo));
+          expect(editList.first.offset, offset, reason: 'should be new line between existing directives and new part');
+          expect(editList.first.replacement, 'part \'foo.over_react.g.dart\';\n\n');
+        });
+
+        test('when there is an existing part directive in the file', () async {
+          final result = await parseAndGetResolvedUnit(
+            sourceWithValidPart,
+            path: 'different_file_name.dart',
+            shouldThrowErrors: false,
+          );
+          final sourceChange = await buildFileEdit(result, (builder) {
+            addOverReactGeneratedPartDirective(builder, result.unit, result.uri);
+          });
+          final editList = sourceChange.edits?.firstOrNull?.edits;
+
+          expect(editList, isNotNull);
+          expect(editList.length, 1, reason: 'there should be one edit in the file');
+
+          final offset = result.unit.lineInfo.getOffsetOfLineAfter(result.unit.directives.last.end);
+          expect(editList.first.offset, offset, reason: 'new part should be on the line after existing part');
+          expect(editList.first.replacement, 'part \'different_file_name.over_react.g.dart\';\n\n');
+        });
+      });
+    });
   });
 }
 
@@ -112,4 +196,21 @@ Future<void> overReactGeneratedPartDirectiveIsValidTestHelper({
   expect(part, isNotNull);
   expect(result.uri.toFilePath(), endsWith('/$path'));
   expect(overReactGeneratedPartDirectiveIsValid(part, result.uri), expectedResult);
+}
+
+Future<List<SourceFileEdit>> getSourceFileEdits(
+  String dartSource,
+  void Function(DartFileEditBuilder builder, ResolvedUnitResult result) callUtilityFunction, {
+  String path = 'foo.dart',
+  bool shouldThrowErrors = true,
+}) async {
+  final result = await parseAndGetResolvedUnit(
+    dartSource,
+    path: path,
+    shouldThrowErrors: shouldThrowErrors,
+  );
+  final sourceChange = await buildFileEdit(result, (builder) {
+    callUtilityFunction(builder, result);
+  });
+  return sourceChange.edits;
 }
