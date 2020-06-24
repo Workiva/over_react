ab6c481b00f99a68b0a4737f4ee3953a3f23cbbc
Aaron Lademann <aaron.lademann@workiva.com>
2020-06-24 06:20:39 -0700

    Ignore playground directory when formatting

--- tools/analyzer_plugin/tool/dart_dev/config.dart
+++ tools/analyzer_plugin/tool/dart_dev/config.dart
@@ -1,6 +1,11 @@
 import 'package:dart_dev/dart_dev.dart';
+import 'package:glob/glob.dart';
 
 final config = {
   ...coreConfig,
-  'format': FormatTool()..formatterArgs = ['--line-length=120'],
+  'format': FormatTool()
+    ..formatterArgs = ['--line-length=120']
+    ..exclude = [
+      Glob('playground/**'),
+    ],
 };
