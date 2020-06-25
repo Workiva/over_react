638fe090d3adf658cd50df88f8434c254efb1110
Aaron Lademann <aaron.lademann@workiva.com>
2020-06-25 06:41:49 -0700

    Rever accidental change to playground file

--- tools/analyzer_plugin/playground/web/extract_component.dart
+++ tools/analyzer_plugin/playground/web/extract_component.dart
@@ -1,11 +1,9 @@
 import 'package:over_react/over_react.dart';
 
 ReactElement renderTheFoo() {
-  return component()(
-    Dom.div()(
-      'oh hai',
-      Dom.span()('again'),
-      Dom.em()(' wow this is a lot we should extract it into a component!'),
-    ),
+  return Dom.div()(
+    'oh hai',
+    Dom.span()('again'),
+    Dom.em()(' wow this is a lot we should extract it into a component!'),
   );
 }
