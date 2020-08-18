9ea249e4f2af5c18a4fe8496383c616e48ce0412
Joe Bingham <joe.bingham@workiva.com>
2020-08-18 16:04:27 -0700

    Experiment with theming the app

--- app/over_react_redux/todo_client/lib/src/components/app_bar/app_bar.dart
+++ app/over_react_redux/todo_client/lib/src/components/app_bar/app_bar.dart
@@ -3,36 +3,39 @@ import 'package:over_react/over_react.dart';
 import 'package:todo_client/src/components/app_bar/app_bar_local_storage_menu.dart';
 import 'package:react_material_ui/react_material_ui.dart';
 
+import '../../theme.dart';
+
 part 'app_bar.over_react.g.dart';
 
-UiFactory<TodoAppBarProps> TodoAppBar =
-    _$TodoAppBar; // ignore: undefined_identifier
+final useStyles = makeStyles(styleFunction: (theme) => MuiStyleMap()
+      ..addRule('app-bar', {
+        'backgroundColor': theme.palette.grey['900'],
+      }), options: MakeStylesOptions()..defaultTheme = simpleTheme);
 
 mixin TodoAppBarProps on UiProps {}
 
-class TodoAppBarComponent extends UiComponent2<TodoAppBarProps> {
-  @override
-  Iterable<ConsumedProps> get consumedProps => [];
+UiFactory<TodoAppBarProps> TodoAppBar = uiFunction((props) {
+  final classes = useStyles();
 
-  @override
-  render() {
-    return Fragment()(
-      (AppBar()
-          ..modifyProps(addUnconsumedProps)
-      )(
-        Toolbar()(
-          (Box()
-              ..flexGrow = 1
-          )(
-            (Typography()
-                ..variant = TypographyVariant.H6
-            )('OverReact Redux Todo Demo App'),
-          ),
-          AppBarLocalStorageMenu()(),
+  return Fragment()(
+    (AppBar()
+      ..className=classes['app-bar']
+      ..addProps(getPropsToForward(props))
+    )(
+      Toolbar()(
+        (Box()
+          ..flexGrow = 1
+        )(
+          (Typography()
+            ..variant = TypographyVariant.H6
+          )('OverReact Redux Todo Demo App'),
         ),
+        AppBarLocalStorageMenu()(),
       ),
-      Toolbar()(),
-    );
-  }
-}
+    ),
+    Toolbar()(),
+  );
+},
+  $TodoAppBarConfig, // ignore: undefined_identifier
+);
 
---
+++ app/over_react_redux/todo_client/lib/src/theme.dart
@@ -0,0 +1,27 @@
+import 'package:react_material_ui/react_material_ui.dart';
+
+final simpleTheme = MuiTheme({
+  'palette': {
+    'primary': {
+      'light': '#a3e066',
+      'main': '#66cc00',
+      'dark': '#85d633',
+    },
+    'secondary': {
+      'light': '#33a9ff',
+      'main': '#0094ff',
+      'dark': '#026dce',
+    },
+    'grey': {
+      '900': '#242424',
+      '800': '#333333',
+      '700': '#474747',
+      '600': '#616161',
+      '500': '#b3b3b3',
+      '400': '#c7c7c7',
+      '300': '#d6d6d6',
+      '200': '#e3e3e3',
+      '100': '#ededed',
+    }
+  }
+});
