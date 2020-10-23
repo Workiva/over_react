192f7b8381c447bb246d97608940ad9f8fb8adaa
Greg Littlefield <greg.littlefield@workiva.com>
2020-10-23 11:37:35 -0700

    Update event helpers to use raw SyntheticEvents

--- lib/src/util/event_helpers.dart
+++ lib/src/util/event_helpers.dart
@@ -17,69 +17,83 @@ library over_react.event_helpers;
 import 'dart:html';
 
 import 'package:over_react/over_react.dart';
+import 'package:react/react_client/js_interop_helpers.dart';
+
+// FIXME move these to react-dart and re-export here so the implementations don't get out of sync, possibly deprecate them?
 
 /// Helper util that wraps a native [KeyboardEvent] in a [SyntheticKeyboardEvent].
 ///
 /// Used where a native [KeyboardEvent] is given and a [SyntheticKeyboardEvent] is needed.
-SyntheticKeyboardEvent wrapNativeKeyboardEvent(KeyboardEvent nativeKeyboardEvent) {
-  return SyntheticKeyboardEvent(
-    nativeKeyboardEvent.bubbles,
-    nativeKeyboardEvent.cancelable,
-    nativeKeyboardEvent.currentTarget,
-    nativeKeyboardEvent.defaultPrevented,
-    nativeKeyboardEvent.preventDefault,
-    nativeKeyboardEvent.stopPropagation,
-    nativeKeyboardEvent.eventPhase,
-    null, // isTrusted
-    nativeKeyboardEvent,
-    nativeKeyboardEvent.target,
-    nativeKeyboardEvent.timeStamp,
-    nativeKeyboardEvent.type,
-    nativeKeyboardEvent.altKey,
-    nativeKeyboardEvent.charCode == null ? null : String.fromCharCode(nativeKeyboardEvent.charCode),
-    nativeKeyboardEvent.charCode,
-    nativeKeyboardEvent.ctrlKey,
-    null, // locale
-    nativeKeyboardEvent.location,
-    null, // key
-    nativeKeyboardEvent.keyCode,
-    nativeKeyboardEvent.metaKey,
-    nativeKeyboardEvent.repeat,
-    nativeKeyboardEvent.shiftKey);
+SyntheticKeyboardEvent wrapNativeKeyboardEvent(KeyboardEvent nativeEvent) {
+  return jsifyAndAllowInterop({
+    // SyntheticEvent fields
+    'bubbles': nativeEvent.bubbles,
+    'cancelable': nativeEvent.cancelable,
+    'currentTarget': nativeEvent.currentTarget,
+    'defaultPrevented': nativeEvent.defaultPrevented,
+    'eventPhase': nativeEvent.eventPhase,
+    'isTrusted': nativeEvent.isTrusted,
+    'nativeEvent': nativeEvent,
+    'target': nativeEvent.target,
+    'timeStamp': nativeEvent.timeStamp,
+    'type': nativeEvent.type,
+    // SyntheticEvent methods
+    'stopPropagation': nativeEvent.stopPropagation,
+    'preventDefault': nativeEvent.preventDefault,
+    'persist': () {},
+    'isPersistent': () => true,
+    // SyntheticKeyboardEvent fields
+    'altKey': nativeEvent.altKey,
+    'char': nativeEvent.charCode == null ? null : String.fromCharCode(nativeEvent.charCode),
+    'ctrlKey': nativeEvent.ctrlKey,
+    'locale': null,
+    'location': nativeEvent.location,
+    'key': nativeEvent.key,
+    'metaKey': nativeEvent.metaKey,
+    'repeat': nativeEvent.repeat,
+    'shiftKey': nativeEvent.shiftKey,
+    'keyCode': nativeEvent.keyCode,
+    'charCode': nativeEvent.charCode,
+  }) as SyntheticKeyboardEvent;
 }
 
 /// Helper util that wraps a native [MouseEvent] in a [SyntheticMouseEvent].
 ///
 /// Used where a native [MouseEvent] is given and a [SyntheticMouseEvent] is needed.
-SyntheticMouseEvent wrapNativeMouseEvent(MouseEvent nativeMouseEvent) {
-  return SyntheticMouseEvent(
-    nativeMouseEvent.bubbles,
-    nativeMouseEvent.cancelable,
-    nativeMouseEvent.currentTarget,
-    nativeMouseEvent.defaultPrevented,
-    nativeMouseEvent.preventDefault,
-    nativeMouseEvent.stopPropagation,
-    nativeMouseEvent.eventPhase,
-    null, // isTrusted
-    nativeMouseEvent,
-    nativeMouseEvent.target,
-    nativeMouseEvent.timeStamp,
-    nativeMouseEvent.type,
-    nativeMouseEvent.altKey,
-    nativeMouseEvent.button,
-    null, // buttons
-    null, // clientX
-    null, // clientY
-
-    nativeMouseEvent.ctrlKey,
-    null, // dataTransfer
-    nativeMouseEvent.metaKey,
-    null, // pageX
-    null, // pageY
-    nativeMouseEvent.relatedTarget,
-    null, // screenX
-    null, // screenY
-    nativeMouseEvent.shiftKey);
+SyntheticMouseEvent wrapNativeMouseEvent(MouseEvent nativeEvent) {
+  return jsifyAndAllowInterop({
+    // SyntheticEvent fields
+    'bubbles': nativeEvent.bubbles,
+    'cancelable': nativeEvent.cancelable,
+    'currentTarget': nativeEvent.currentTarget,
+    'defaultPrevented': nativeEvent.defaultPrevented,
+    'eventPhase': nativeEvent.eventPhase,
+    'isTrusted': nativeEvent.isTrusted,
+    'nativeEvent': nativeEvent,
+    'target': nativeEvent.target,
+    'timeStamp': nativeEvent.timeStamp,
+    'type': nativeEvent.type,
+    // SyntheticEvent methods
+    'stopPropagation': nativeEvent.stopPropagation,
+    'preventDefault': nativeEvent.preventDefault,
+    'persist': () {},
+    'isPersistent': () => true,
+    // SyntheticMouseEvent fields
+    'altKey': nativeEvent.altKey,
+    'button': nativeEvent.button,
+    'buttons': nativeEvent.buttons,
+    'clientX': nativeEvent.client.x,
+    'clientY': nativeEvent.client.y,
+    'ctrlKey': nativeEvent.ctrlKey,
+    'dataTransfer': nativeEvent.dataTransfer,
+    'metaKey': nativeEvent.metaKey,
+    'pageX': nativeEvent.page.x,
+    'pageY': nativeEvent.page.y,
+    'relatedTarget': nativeEvent.relatedTarget,
+    'screenX': nativeEvent.screen.x,
+    'screenY': nativeEvent.screen.y,
+    'shiftKey': nativeEvent.shiftKey,
+  }) as SyntheticMouseEvent;
 }
 
 /// If the consumer specifies a callback like `onChange` on one of our custom form components that are not *actually*
@@ -88,18 +102,22 @@ SyntheticMouseEvent wrapNativeMouseEvent(MouseEvent nativeMouseEvent) {
 /// This helper method generates a "fake" [SyntheticFormEvent], with nothing but the `target` set to [element],
 /// `type` set to [type] and `timeStamp` set to the current time. All other arguments are `noop`, `false` or `null`.
 SyntheticFormEvent fakeSyntheticFormEvent(Element element, String type) {
-  return SyntheticFormEvent(
-    false,
-    false,
-    element,
-    false,
-    () {},
-    () {},
-    Event.AT_TARGET,
-    false,
-    null,
-    element,
-    DateTime.now().millisecondsSinceEpoch,
-    type
-  );
+  return jsifyAndAllowInterop({
+    // SyntheticEvent fields
+    'bubbles': false,
+    'cancelable': false,
+    'currentTarget': element,
+    'defaultPrevented': false,
+    'eventPhase': Event.AT_TARGET,
+    'isTrusted': false,
+    'nativeEvent': null,
+    'target': element,
+    'timeStamp': DateTime.now().millisecondsSinceEpoch,
+    'type': type,
+    // SyntheticEvent methods
+    'stopPropagation': () {},
+    'preventDefault': () {},
+    'persist': () {},
+    'isPersistent': () => true,
+  }) as SyntheticFormEvent;
 }
