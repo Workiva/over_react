3f61e9e26457c234cb10ea58c4fd46550d761f29
Joe Bingham <joe.bingham@workiva.com>
2021-01-13 15:02:04 -0700

    Add 2.9.0 syntax examples

--- example/builder/main.dart
+++ example/builder/main.dart
@@ -15,10 +15,14 @@
 import 'dart:html';
 
 import 'package:over_react/over_react.dart';
+import 'package:over_react/over_react_flux.dart';
 import 'package:react/react_dom.dart' as react_dom;
+import 'package:redux/redux.dart';
 
 import './src/abstract_inheritance.dart';
 import './src/basic.dart';
+import './src/dart_two_nine_syntax/basic.dart' as dart_two_nine_basic;
+import './src/dart_two_nine_syntax/hoc.dart';
 import './src/basic_library.dart';
 import './src/generic_inheritance_sub.dart';
 import './src/generic_inheritance_super.dart';
@@ -26,8 +30,17 @@ import './src/function_component.dart' as function;
 import 'src/functional_consumed_props.dart';
 import 'src/new_class_consumed_props.dart';
 
+class ExampleState {
+  final String testValue;
+
+  ExampleState(this.testValue);
+}
+
+Store store = Store<ExampleState>((_, __) => ExampleState('foo'), initialState: ExampleState('foo'));
+
 main() {
   react_dom.render(
+      (ReduxProvider()..store = store)(
       Dom.div()(
         Dom.h3()('Components'),
         (Basic()
@@ -65,10 +78,22 @@ main() {
           ..aParentProp = 'parent'
           ..aPropToBePassed = 'passed'
         )(),
-          (SomeClassParent()
-            ..aParentProp = 'classParent'
-            ..aPropToBePassed = 'passed'
-          )()
+        (SomeClassParent()
+          ..aParentProp = 'classParent'
+          ..aPropToBePassed = 'passed'
+        )(),
+        Dom.h3()('Components (Dart >=2.9.0 syntax)'),
+        (dart_two_nine_basic.Basic()
+          ..basic1 = '<basic prop>'
+          ..id = 'the id of this component'
+        )(null, null, ['this is another child']),
+        (Hoc()
+          ..foo = 'HOC test'
+        )(),
+        (ConnectedHocWithTwoFactories()
+          ..foo = 'HOC test'
+        )(),
+      )
       ), querySelector('#content')
   );
 }
---
+++ example/builder/src/dart_two_nine_syntax/hoc.dart
@@ -0,0 +1,36 @@
+import 'package:over_react/over_react.dart';
+import 'package:over_react/over_react_redux.dart';
+
+import '../../main.dart';
+
+part 'hoc.over_react.g.dart';
+
+UiFactory<HocProps> Hoc = connect<ExampleState, HocProps>(
+  mapStateToPropsWithOwnProps: (state, props) => Hoc(),
+  mapDispatchToPropsWithOwnProps: (state, props) => Hoc(),
+)(castUiFactory(_$Hoc)); // ignore: undefined_identifier
+
+mixin HocProps on UiProps {
+  String foo;
+}
+
+class HocComponent extends UiComponent2<HocProps> {
+  @override
+  render() => Dom.div()(props.foo);
+}
+
+UiFactory<HocWithTwoFactoriesProps> HocWithTwoFactories = castUiFactory(_$HocWithTwoFactories); // ignore: undefined_identifier
+
+UiFactory<HocWithTwoFactoriesProps> ConnectedHocWithTwoFactories = connect<ExampleState, HocWithTwoFactoriesProps>(
+  mapStateToPropsWithOwnProps: (state, props) => HocWithTwoFactories(),
+  mapDispatchToPropsWithOwnProps: (state, props) => HocWithTwoFactories(),
+)(HocWithTwoFactories);
+
+mixin HocWithTwoFactoriesProps on UiProps {
+  String foo;
+}
+
+class HocWithTwoFactoriesComponent extends UiComponent2<HocWithTwoFactoriesProps> {
+  @override
+  render() => Dom.div()(props.foo);
+}
