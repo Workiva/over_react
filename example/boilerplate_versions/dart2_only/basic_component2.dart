a3e59fb7527261669116809d1125bac476e1fe73
Sydney Jodon <sydney.jodon@workiva.com>
2021-01-29 13:27:58 -0700

    Add examples for each boilerplate version

--- example/boilerplate_versions/dart2_only/basic_component2.dart
+++ example/boilerplate_versions/dart2_only/basic_component2.dart
@@ -12,13 +12,18 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// ignore_for_file: deprecated_member_use_from_same_package
+
 import 'package:over_react/over_react.dart';
 
-part 'basic.over_react.g.dart';
+part 'basic_component2.over_react.g.dart';
 
+@Factory()
 UiFactory<BasicProps> Basic = _$Basic; // ignore: undefined_identifier, invalid_assignment
 
-mixin BasicProps on UiProps {
+@Props()
+//// ignore: mixin_of_non_class,undefined_class
+class _$BasicProps extends UiProps {
   @Deprecated('This is deprecated')
   @requiredProp
   String basicProp;
@@ -30,20 +35,20 @@ mixin BasicProps on UiProps {
   String basic5;
 }
 
-class BasicComponent extends UiComponent2<BasicProps> {
+@Component2()
+class Basic2Component extends UiComponent2<BasicProps> {
   @override
-  get defaultProps => newProps()..id = 'basic component'
-      ..basicProp = 'defaultBasicProps'; // ignore: deprecated_member_use_from_same_package
+  Map get defaultProps => newProps()
+    ..id = 'basic 2 component'
+    ..basicProp = 'defaultBasic2Props';
 
   @override
   render() {
     return Dom.div()(
-        Dom.div()('prop id: ${props.id}'),
-        Dom.div()('default prop testing: ${props.basicProp}'), // ignore: deprecated_member_use_from_same_package
-        Dom.div()('default prop testing: ${props.basic1}'),
-        Dom.div()(null, props.basic4, 'children: ${props.children}' ),
+      Dom.div()('prop id: ${props.id}'),
+      Dom.div()('default prop testing: ${props.basicProp}'),
+      Dom.div()('default prop testing: ${props.basic1}'),
+      Dom.div()(null, props.basic4, 'children: ${props.children}'),
     );
   }
 }
-
-
