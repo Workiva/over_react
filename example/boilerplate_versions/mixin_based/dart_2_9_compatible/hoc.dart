250924153a5384fc5f28447b88146baa94d91310
Sydney Jodon <sydney.jodon@workiva.com>
2021-01-29 15:21:34 -0700

    Add examples of each boilerplate version

--- example/boilerplate_versions/mixin_based/dart_2_9_compatible/hoc.dart
+++ example/boilerplate_versions/mixin_based/dart_2_9_compatible/hoc.dart
@@ -15,14 +15,14 @@
 import 'package:over_react/over_react.dart';
 import 'package:over_react/over_react_redux.dart';
 
-import '../../main.dart';
+import '../../../builder/main.dart';
 
 part 'hoc.over_react.g.dart';
 
 UiFactory<HocProps> Hoc = connect<ExampleState, HocProps>(
   mapStateToPropsWithOwnProps: (state, props) => Hoc(),
   mapDispatchToPropsWithOwnProps: (state, props) => Hoc(),
-)(_$Hoc); // ignore: undefined_identifier, argument_type_not_assignable
+)(castUiFactory(_$Hoc)); // ignore: undefined_identifier
 
 mixin HocProps on UiProps {
   String foo;
@@ -33,9 +33,11 @@ class HocComponent extends UiComponent2<HocProps> {
   render() => Dom.div()(props.foo);
 }
 
-UiFactory<HocWithTwoFactoriesProps> HocWithTwoFactories = _$HocWithTwoFactories; // ignore: undefined_identifier, invalid_assignment
+UiFactory<HocWithTwoFactoriesProps> HocWithTwoFactories =
+    castUiFactory(_$HocWithTwoFactories); // ignore: undefined_identifier
 
-UiFactory<HocWithTwoFactoriesProps> ConnectedHocWithTwoFactories = connect<ExampleState, HocWithTwoFactoriesProps>(
+UiFactory<HocWithTwoFactoriesProps> ConnectedHocWithTwoFactories =
+    connect<ExampleState, HocWithTwoFactoriesProps>(
   mapStateToPropsWithOwnProps: (state, props) => HocWithTwoFactories(),
   mapDispatchToPropsWithOwnProps: (state, props) => HocWithTwoFactories(),
 )(HocWithTwoFactories);
