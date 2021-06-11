77d015e314633bbe6970c7b333f57d00a1a844ea
Greg Littlefield <greg.littlefield@workiva.com>
2021-06-11 11:47:11 -0700

    Remove unused import warning

--- test/over_react_redux/hooks/use_selector_test.dart
+++ test/over_react_redux/hooks/use_selector_test.dart
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import 'dart:developer';
-
 import 'package:over_react/over_react.dart';
 import 'package:over_react/over_react_redux.dart';
 import 'package:redux/redux.dart';
