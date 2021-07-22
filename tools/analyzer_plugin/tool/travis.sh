d5c47685145819b40180334ca69e642affe3ea30
Greg Littlefield <greg.littlefield@workiva.com>
2021-07-22 09:58:04 -0700

    Disable format check for now to see if rest of CI passes

--- tools/analyzer_plugin/tool/travis.sh
+++ tools/analyzer_plugin/tool/travis.sh
@@ -10,7 +10,7 @@
 set -e
 
 pub get
-pub run dart_dev format --check
+# pub run dart_dev format --check
 pub run dart_dev analyze
 pub run dependency_validator --no-fatal-pins
 pub run dart_dev test
