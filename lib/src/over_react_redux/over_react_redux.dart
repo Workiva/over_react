c01d310de1b5afdcae37817fea73eef339f60337
Greg Littlefield <greg.littlefield@workiva.com>
2022-02-02 12:20:13 -0700

    Reuse wrapped functions

--- lib/src/over_react_redux/over_react_redux.dart
+++ lib/src/over_react_redux/over_react_redux.dart
@@ -566,9 +566,14 @@ class DartValueWrapper {
 
   const DartValueWrapper(this.value);
 
+  static final _functionWrapperCache = Expando<DartValueWrapper>('_functionWrapperCache');
+
   static dynamic wrapIfNeeded(dynamic value) {
+    // This case should be fairly uncommon, since functions usually aren't used as
+    // a Redux store's state or the result of a connect or selector hook selector.
     if (value is Function && !identical(allowInterop(value), value)) {
-      return DartValueWrapper(value);
+      // Reuse wrappers for the same function so that they're identical to the JS.
+      return _functionWrapperCache[value] ??= DartValueWrapper(value);
     }
     return value;
   }
