c1d2790f630ae28eb02584e5cd1d93ce40a3b5c7
sydneesampson-wk <sydnee.sampson@workiva.com>
2022-05-06 19:50:10 -0700

    Create js_component

---
+++ lib/src/util/js_component.dart
@@ -0,0 +1,45 @@
+import 'package:over_react/over_react.dart';
+import 'package:react/react_client/component_factory.dart';
+
+/// Creates a Dart component factory that wraps a ReactJS [factoryProxy].
+///
+/// Example:
+/// ```dart
+/// UiFactory<ButtonProps> Button = uiJsComponent(
+///   ReactJsComponentFactoryProxy(MaterialUI.Button),
+///   _$ButtonConfig, // ignore: undefined_identifier
+/// );
+///
+/// @Props(keyNamespace: '')
+/// mixin ButtonProps on UiProps {}
+/// ```
+UiFactory<TProps> uiJsComponent<TProps extends UiProps>(
+  ReactJsComponentFactoryProxy factoryProxy,
+  dynamic _config,
+) {
+  ArgumentError.checkNotNull(_config, '_config');
+
+  if (_config is! UiFactoryConfig<TProps>) {
+    throw ArgumentError('_config should be a UiFactory<TProps>. Make sure you are '
+        r'using either the generated factory config (i.e. _$FooConfig) or manually '
+        'declaring your config correctly.');
+  }
+
+  // ignore: invalid_use_of_protected_member
+  final propsFactory = (_config as UiFactoryConfig<TProps>).propsFactory;
+  ArgumentError.checkNotNull(_config, '_config');
+
+  TProps _uiFactory([Map backingMap]) {
+    TProps builder;
+    if (backingMap == null) {
+      builder = propsFactory.jsMap(JsBackedMap());
+    } else if (backingMap is JsBackedMap) {
+      builder = propsFactory.jsMap(backingMap);
+    } else {
+      builder = propsFactory.map(backingMap);
+    }
+    return builder..componentFactory = factoryProxy;
+  }
+
+  return _uiFactory;
+}
