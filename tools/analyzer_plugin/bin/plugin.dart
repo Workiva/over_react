15ab28f95e6cb1883ba1e5ff46817eaa467d47f7
Greg Littlefield <greg.littlefield@workiva.com>
2022-05-11 12:08:08 -0700

    Fix analyzer plugin not starting

--- tools/analyzer_plugin/bin/plugin.dart
+++ tools/analyzer_plugin/bin/plugin.dart
@@ -1,3 +1,10 @@
+// @dart=2.9
+// ^ Without this version comment, the plugin will fail to start
+// if it imports any non-null-safe code (e.g., from over_react).
+//
+// Before removing, ensure all import_of_legacy_library_into_null_safe
+// analysis errors are fixed.
+
 import 'dart:isolate';
 
 import 'package:over_react_analyzer_plugin/plugin_starter.dart';
