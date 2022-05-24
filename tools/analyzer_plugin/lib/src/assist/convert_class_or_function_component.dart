b1b56ec53e8f7482eb192c28fbda0ebe75cc3807
Greg Littlefield <greg.littlefield@workiva.com>
2022-05-24 13:56:36 -0700

    Include class methods when converting to a function component

--- tools/analyzer_plugin/lib/src/assist/convert_class_or_function_component.dart
+++ tools/analyzer_plugin/lib/src/assist/convert_class_or_function_component.dart
@@ -72,8 +72,34 @@ class ConvertClassOrFunctionComponentAssistContributor extends AssistContributor
     final renderBody = render.body;
     if (renderBody is EmptyFunctionBody) return;
 
-    final renderSource = request.result.content!.substring(
-        renderBody.offset, renderBody.tryCast<ExpressionFunctionBody>()?.semicolon?.offset ?? renderBody.end);
+    final getSource = request.result.content!.substring;
+
+    const classMemberIndent = '  ';
+    const functionComponentBodyIndent = '    ';
+
+    late String newRenderSource;
+    if (renderBody is BlockFunctionBody) {
+      newRenderSource = getSource(renderBody.block.leftBracket.end, renderBody.block.rightBracket.offset);
+    } else if (renderBody is ExpressionFunctionBody) {
+      final bodyExpressionSource = getSource(renderBody.expression.offset, renderBody.expression.end);
+      newRenderSource = '\n${functionComponentBodyIndent}return $bodyExpressionSource;\n';
+    } else {
+      // This case is currently unreachable, but may become reachable if new function body types are ever added.
+      newRenderSource = getSource(renderBody.offset, renderBody.end);
+    }
+
+    final otherMethodsSources = closestClass.members
+        .where((member) => member != render)
+        // Add indentation since `.offset` won't include it.
+        // Assume it's two spaces since it should always be, and getting the actual indent is more effort.
+        .map((e) => classMemberIndent + getSource(e.offset, e.end))
+        // Indent one more level to match the new indentation of the function body.
+        .map((e) => e.split('\n').map((e) => '  $e').join('\n'))
+        .join('\n\n');
+
+    if (otherMethodsSources.isNotEmpty) {
+      newRenderSource = '\n$otherMethodsSources\n$newRenderSource';
+    }
 
     final changeBuilder = ChangeBuilder(session: request.result.session);
 
@@ -82,7 +108,7 @@ class ConvertClassOrFunctionComponentAssistContributor extends AssistContributor
       builder.addSimpleReplacement(
           range.node(factoryInitializer),
           'uiFunction(\n'
-          '  (props) $renderSource,\n'
+          '  (props) {$newRenderSource},\n'
           '  $generatedFactoryConfigName, // ignore:undefined_identifier\n'
           ')');
 
