b20ec362e2b32e6cd9b6cef200a2363b50527e20
kealjones-wk <41018730+kealjones-wk@users.noreply.github.com>
2023-08-09 15:19:36 -0700

    maybe fix test?

--- test/over_react/component_declaration/builder_integration_tests/new_boilerplate/function_component_test.dart
+++ test/over_react/component_declaration/builder_integration_tests/new_boilerplate/function_component_test.dart
@@ -413,7 +413,7 @@ testPropsToForward({UiFactory<TestProps> factory, bool modifyProps = false}) {
       test('which throws an error when not providing an exclude argument and the props class is NOT a mixin and `domOnly` is NOT `true`', () {
         debugger();
         expect(() => _propsToForward(exclude: null, props: initialProps, factory: factory, modifyProps: modifyProps),
-            throwsArgumentError);
+            throwsA(isA<AssertionError>()));
       }, tags: 'ddc');
     });
 }
