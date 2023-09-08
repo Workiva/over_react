eb9415f5ee19e65ab905e16debab6108a30f5f50
Greg Littlefield <greg.littlefield@workiva.com>
2023-09-08 10:22:14 -0700

    Omit types in StubServerPlugin overrides to clean up imports and remove some src imports

--- tools/analyzer_plugin/test/integration/stubs.dart
+++ tools/analyzer_plugin/test/integration/stubs.dart
@@ -1,19 +1,13 @@
 import 'package:analyzer/dart/analysis/analysis_context.dart';
-import 'package:analyzer/dart/analysis/analysis_context_collection.dart';
 import 'package:analyzer/dart/analysis/results.dart';
 import 'package:analyzer/file_system/overlay_file_system.dart';
-import 'package:analyzer/src/dart/analysis/byte_store.dart';
-import 'package:analyzer/src/dart/analysis/driver.dart';
 import 'package:analyzer_plugin/channel/channel.dart';
 import 'package:analyzer_plugin/plugin/navigation_mixin.dart';
 import 'package:analyzer_plugin/plugin/plugin.dart';
 import 'package:analyzer_plugin/protocol/protocol.dart';
-import 'package:analyzer_plugin/protocol/protocol_generated.dart';
-import 'package:analyzer_plugin/utilities/subscriptions/subscription_manager.dart';
 import 'package:over_react_analyzer_plugin/src/async_plugin_apis/assist.dart';
 import 'package:over_react_analyzer_plugin/src/async_plugin_apis/diagnostic.dart';
 import 'package:over_react_analyzer_plugin/src/plugin.dart';
-import 'package:pub_semver/src/version.dart';
 
 import 'test_bases/assist_test_base.dart';
 
@@ -31,155 +25,137 @@ class StubChannel implements PluginCommunicationChannel {
 
 class StubServerPlugin implements ServerPlugin {
   @override
-  PluginCommunicationChannel get channel => throw UnimplementedError();
+  get channel => throw UnimplementedError();
 
   @override
-  Set<String> get priorityPaths => throw UnimplementedError();
+  get priorityPaths => throw UnimplementedError();
 
   @override
-  set priorityPaths(Set<String> value) => throw UnimplementedError();
+  set priorityPaths(value) => throw UnimplementedError();
 
   @override
-  Future<void> afterNewContextCollection({required AnalysisContextCollection contextCollection}) =>
-      throw UnimplementedError();
+  afterNewContextCollection({required contextCollection}) => throw UnimplementedError();
 
   @override
-  Future<void> analyzeFile({required AnalysisContext analysisContext, required String path}) =>
-      throw UnimplementedError();
+  analyzeFile({required analysisContext, required path}) => throw UnimplementedError();
 
   @override
-  Future<void> analyzeFiles({required AnalysisContext analysisContext, required List<String> paths}) =>
-      throw UnimplementedError();
+  analyzeFiles({required analysisContext, required paths}) => throw UnimplementedError();
 
   @override
-  Future<void> beforeContextCollectionDispose({required AnalysisContextCollection contextCollection}) =>
-      throw UnimplementedError();
-
+  beforeContextCollectionDispose({required contextCollection}) => throw UnimplementedError();
 
   @override
-  String? get contactInfo => throw UnimplementedError();
+  get contactInfo => throw UnimplementedError();
 
   @override
-  Future<void> contentChanged(List<String> paths) => throw UnimplementedError();
+  contentChanged(paths) => throw UnimplementedError();
 
   @override
-  ByteStore createByteStore() => throw UnimplementedError();
+  createByteStore() => throw UnimplementedError();
 
   @override
-  List<String> get fileGlobsToAnalyze => throw UnimplementedError();
+  get fileGlobsToAnalyze => throw UnimplementedError();
 
   @override
-  Future<void> flushAnalysisState({bool elementModels = true}) => throw UnimplementedError();
+  flushAnalysisState({elementModels = true}) => throw UnimplementedError();
 
   @override
-  Future<ResolvedUnitResult> getResolvedUnitResult(String path) => throw UnimplementedError();
+  getResolvedUnitResult(path) => throw UnimplementedError();
 
   @override
-  Future<void> handleAffectedFiles({required AnalysisContext analysisContext, required List<String> paths}) =>
-      throw UnimplementedError();
+  handleAffectedFiles({required analysisContext, required paths}) => throw UnimplementedError();
 
   @override
-  Future<AnalysisGetNavigationResult> handleAnalysisGetNavigation(AnalysisGetNavigationParams parameters) =>
-      throw UnimplementedError();
+  handleAnalysisGetNavigation(parameters) => throw UnimplementedError();
 
   @override
-  Future<AnalysisHandleWatchEventsResult> handleAnalysisHandleWatchEvents(AnalysisHandleWatchEventsParams parameters) =>
-      throw UnimplementedError();
+  handleAnalysisHandleWatchEvents(parameters) => throw UnimplementedError();
 
   @override
-  Future<AnalysisSetContextRootsResult> handleAnalysisSetContextRoots(AnalysisSetContextRootsParams parameters) =>
-      throw UnimplementedError();
+  handleAnalysisSetContextRoots(parameters) => throw UnimplementedError();
 
   @override
-  Future<AnalysisSetPriorityFilesResult> handleAnalysisSetPriorityFiles(AnalysisSetPriorityFilesParams parameters) =>
-      throw UnimplementedError();
+  handleAnalysisSetPriorityFiles(parameters) => throw UnimplementedError();
 
   @override
-  Future<AnalysisSetSubscriptionsResult> handleAnalysisSetSubscriptions(AnalysisSetSubscriptionsParams parameters) =>
-      throw UnimplementedError();
+  handleAnalysisSetSubscriptions(parameters) => throw UnimplementedError();
 
   @override
-  Future<AnalysisUpdateContentResult> handleAnalysisUpdateContent(AnalysisUpdateContentParams parameters) =>
-      throw UnimplementedError();
+  handleAnalysisUpdateContent(parameters) => throw UnimplementedError();
 
   @override
-  Future<CompletionGetSuggestionsResult> handleCompletionGetSuggestions(CompletionGetSuggestionsParams parameters) =>
-      throw UnimplementedError();
+  handleCompletionGetSuggestions(parameters) => throw UnimplementedError();
 
   @override
-  Future<EditGetAssistsResult> handleEditGetAssists(EditGetAssistsParams parameters) => throw UnimplementedError();
+  handleEditGetAssists(parameters) => throw UnimplementedError();
 
   @override
-  Future<EditGetAvailableRefactoringsResult> handleEditGetAvailableRefactorings(
-          EditGetAvailableRefactoringsParams parameters) =>
-      throw UnimplementedError();
+  handleEditGetAvailableRefactorings(parameters) => throw UnimplementedError();
 
   @override
-  Future<EditGetFixesResult> handleEditGetFixes(EditGetFixesParams parameters) => throw UnimplementedError();
+  handleEditGetFixes(parameters) => throw UnimplementedError();
 
   @override
-  Future<EditGetRefactoringResult?> handleEditGetRefactoring(EditGetRefactoringParams parameters) =>
-      throw UnimplementedError();
+  handleEditGetRefactoring(parameters) => throw UnimplementedError();
 
   @override
-  Future<KytheGetKytheEntriesResult?> handleKytheGetKytheEntries(KytheGetKytheEntriesParams parameters) =>
-      throw UnimplementedError();
+  handleKytheGetKytheEntries(parameters) => throw UnimplementedError();
 
   @override
-  Future<PluginShutdownResult> handlePluginShutdown(PluginShutdownParams parameters) => throw UnimplementedError();
+  handlePluginShutdown(parameters) => throw UnimplementedError();
 
   @override
-  Future<PluginVersionCheckResult> handlePluginVersionCheck(PluginVersionCheckParams parameters) =>
-      throw UnimplementedError();
+  handlePluginVersionCheck(parameters) => throw UnimplementedError();
 
   @override
-  bool isCompatibleWith(Version serverVersion) => throw UnimplementedError();
+  isCompatibleWith(serverVersion) => throw UnimplementedError();
 
   @override
-  String get name => throw UnimplementedError();
+  get name => throw UnimplementedError();
 
   @override
   void onDone() {}
 
   @override
-  void onError(Object exception, StackTrace stackTrace) {}
+  void onError(exception, stackTrace) {}
 
   @override
-  OverlayResourceProvider get resourceProvider => throw UnimplementedError();
+  get resourceProvider => throw UnimplementedError();
 
   @override
-  Future<void> sendFoldingNotification(String path) => throw UnimplementedError();
+  sendFoldingNotification(path) => throw UnimplementedError();
 
   @override
-  Future<void> sendHighlightsNotification(String path) => throw UnimplementedError();
+  sendHighlightsNotification(path) => throw UnimplementedError();
 
   @override
-  Future<void> sendNavigationNotification(String path) => throw UnimplementedError();
+  sendNavigationNotification(path) => throw UnimplementedError();
 
   @override
-  void sendNotificationsForFile(String path) {}
+  void sendNotificationsForFile(path) {}
 
   @override
-  void sendNotificationsForSubscriptions(Map<String, List<AnalysisService>> subscriptions) {}
+  void sendNotificationsForSubscriptions(subscriptions) {}
 
   @override
-  Future<void> sendOccurrencesNotification(String path) => throw UnimplementedError();
+  sendOccurrencesNotification(path) => throw UnimplementedError();
 
   @override
-  Future<void> sendOutlineNotification(String path) => throw UnimplementedError();
+  sendOutlineNotification(path) => throw UnimplementedError();
 
   @override
-  void start(PluginCommunicationChannel channel) => throw UnimplementedError();
+  void start(channel) => throw UnimplementedError();
 
   @override
-  SubscriptionManager get subscriptionManager => throw UnimplementedError();
+  get subscriptionManager => throw UnimplementedError();
 
   @override
-  String get version => throw UnimplementedError();
+  get version => throw UnimplementedError();
 }
 
 /// A concrete [ServerPlugin] implementation designed for use in testing a
-/// single assist or diagnostic contributor against a real-ish [AnalysisDriver].
+/// single assist or diagnostic contributor against real-ish AnalysisContexts.
 ///
 /// Tests should not use, extend, or implement this class directly. Instead,
 /// extend the contributor-specific test base class, like [AssistTestBase].
@@ -195,8 +171,6 @@ class PluginForTest extends StubServerPlugin
 
   @override
   PluginCommunicationChannel get channel => _channel!;
-
-
 
   @override
   void start(PluginCommunicationChannel channel) {
@@ -218,7 +192,8 @@ class PluginForTest extends StubServerPlugin
   Future<void> contentChanged(List<String> paths) => throw UnimplementedError();
 
   @override
-  Future<ResolvedUnitResult> getResolvedUnitResult(String path) => handleGetResolvedUnitResult?.call(path) ?? (throw UnimplementedError());
+  Future<ResolvedUnitResult> getResolvedUnitResult(String path) =>
+      handleGetResolvedUnitResult?.call(path) ?? (throw UnimplementedError());
 
   Future<ResolvedUnitResult> Function(String path)? handleGetResolvedUnitResult;
 
