8508cd4b907d422af5983ca05d2dc40a5e64d668
Greg Littlefield <greg.littlefield@workiva.com>
2023-12-19 17:30:16 -0700

    Optimize required prop diagnostic

--- tools/analyzer_plugin/lib/src/util/get_all_props.dart
+++ tools/analyzer_plugin/lib/src/util/get_all_props.dart
@@ -1,14 +1,23 @@
 import 'package:analyzer/dart/element/element.dart';
-import 'package:over_react_analyzer_plugin/src/util/react_types.dart';
 import 'package:over_react_analyzer_plugin/src/util/util.dart';
 
-Iterable<FieldElement> getAllProps(InterfaceElement element) sync* {
+// Performance optimization notes:
+// [1] Building a list here is slightly more optimal than using a generator.
+//
+// [2] Ideally we'd check the library/package here,
+//     but for some reason accessing `.library` is pretty inefficient.
+//     Possibly due to the `LibraryElementImpl? get library => thisOrAncestorOfType();` impl,
+//     and an inefficient `thisOrAncestorOfType` impl: https://github.com/dart-lang/sdk/issues/53255
+
+List<FieldElement> getAllProps(InterfaceElement element) {
+  final allProps = <FieldElement>[]; // [1]
+
+  // final allInterfaces = element.thisAndSupertypes;
   final allInterfaces = element.thisAndSupertypes.toList();
 
   final uiPropsElement = allInterfaces.singleWhereOrNull(
       (i) => i.name == 'UiProps' && i.library.name == 'over_react.component_declaration.component_base');
-  if (uiPropsElement == null) return;
-
+  if (uiPropsElement == null) return allProps;
   final uiPropsInterfaces = uiPropsElement.thisAndSupertypes.toSet();
   for (final c in allInterfaces) {
     // Don't process UiProps or its supertypes (Object, Map, MapBase, MapViewMixin, PropsMapViewMixin, ReactPropsMixin, UbiquitousDomPropsMixin, CssClassPropsMixin, ...)
@@ -23,16 +32,21 @@ Iterable<FieldElement> getAllProps(InterfaceElement element) sync* {
       continue;
     }
 
-    late final isMixinBasedPropsMixin =
-        c is MixinElement && c.superclassConstraints.any((s) => s.element.name == 'UiProps');
-    late final isLegacyPropsOrPropsMixin = c.metadata.any((e) {
-      final element = e.element;
-      return element != null &&
-          element.library?.name == 'over_react.component_declaration.annotations' &&
-          const {'Props', 'PropsMixin'}.contains(element.tryCast<ConstructorElement>()?.enclosingElement.name);
-    });
+    switch (c.name) {
+      case 'DomProps':
+      case 'DomPropsMixin':
+      case 'SvgProps':
+      case 'SvgPropsMixin':
+        continue;
+    }
 
-    if (!isMixinBasedPropsMixin && !isLegacyPropsOrPropsMixin) continue;
+    final isMixinBasedPropsMixin = c is MixinElement && c.superclassConstraints.any((s) => s.element.name == 'UiProps');
+    late final isLegacyPropsOrPropsMixinConsumerClass =
+        c.name.startsWith(r'_$') && c.metadata.any(_isPropsOrPropsMixinAnnotation);
+
+    if (!isMixinBasedPropsMixin && !isLegacyPropsOrPropsMixinConsumerClass) {
+      continue;
+    }
 
     for (final field in c.fields) {
       if (field.isStatic) continue;
@@ -43,19 +57,29 @@ Iterable<FieldElement> getAllProps(InterfaceElement element) sync* {
           accessorAnnotation?.computeConstantValue()?.getField('doNotGenerate')?.toBoolValue() ?? false;
       if (isNoGenerate) continue;
 
-      yield field;
+      allProps.add(field);
     }
   }
+
+  return allProps;
+}
+
+bool _isPropsOrPropsMixinAnnotation(ElementAnnotation e) {
+  final element = e.element; // [2]
+  if (element is ConstructorElement) {
+    switch (element.enclosingElement.name) {
+      case 'Props':
+      case 'PropsMixin':
+        return true;
+    }
+  }
+  return false;
 }
 
 ElementAnnotation? _getAccessorAnnotation(List<ElementAnnotation> metadata) {
   return metadata.firstWhereOrNull((annotation) {
-    final element = annotation.element;
-    return element != null &&
-        element.isDeclaredInPackage('over_react') &&
-        // In our case, it's a constructor being referenced.
-        // We could also compute the constant first and then check its type, but that's probably less efficient.
-        element.tryCast<ConstructorElement>()?.enclosingElement.name == 'Accessor';
+    final element = annotation.element; // [2]
+    return element is ConstructorElement && element.enclosingElement.name == 'Accessor';
   });
 }
 
