b97d9ad712b65bfb983c017b96b6d22244a0e4c2
Greg Littlefield <greg.littlefield@workiva.com>
2024-01-22 16:47:56 -0700

    Update playground

--- tools/analyzer_plugin/playground/web/missing_required_props.dart
+++ tools/analyzer_plugin/playground/web/missing_required_props.dart
@@ -2,11 +2,14 @@ import 'package:over_react/over_react.dart';
 
 part 'missing_required_props.over_react.g.dart';
 
+UiFactory<UiProps> GenericFactory = uiFunction((_) {}, UiFactoryConfig());
+
 UiFactory<NoRequiredProps> NoRequired = uiFunction((_) {}, _$NoRequiredConfig);
 mixin NoRequiredProps on UiProps {
   String? optional1;
   String? optional2;
 }
+
 
 UiFactory<WithLateRequiredProps> WithLateRequired = uiFunction((_) {}, _$WithLateRequiredConfig);
 mixin WithLateRequiredProps on UiProps {
@@ -16,27 +19,40 @@ mixin WithLateRequiredProps on UiProps {
   String? optional2;
 }
 
+
 UiFactory<InheritsLateRequiredProps> InheritsLateRequired = uiFunction((_) {}, _$InheritsLateRequiredConfig);
 mixin InheritsLateRequiredPropsMixin on UiProps {
   late String requiredInSubclass;
 }
+class InheritsLateRequiredProps = UiProps with WithLateRequiredProps, InheritsLateRequiredPropsMixin;
 
 
-UiFactory<RequiredWithSameNameAsPrefixedProps> RequiredWithSameNameAsPrefixed = uiFunction((_) {},
-    _$RequiredWithSameNameAsPrefixedConfig);
+UiFactory<RequiredWithSameNameAsPrefixedProps> RequiredWithSameNameAsPrefixed = uiFunction((_) {}, _$RequiredWithSameNameAsPrefixedConfig);
 mixin RequiredWithSameNameAsPrefixedProps on UiProps {
   late bool hidden;
 }
 
 
-class InheritsLateRequiredProps = UiProps with WithLateRequiredProps, InheritsLateRequiredPropsMixin;
+UiFactory<DisableValidationProps> DisableValidation = uiFunction((_) {}, _$DisableValidationConfig);
+mixin DisableValidationProps on UiProps {
+  @disableRequiredPropValidation
+  late String requiredPropWithDisabledValidation;
+}
+
+
+UiFactory<IgnoresSomeRequiredProps> IgnoresSomeRequired = uiFunction((_) {}, _$IgnoresSomeRequiredConfig);
+mixin IgnoresSomeRequiredPropsMixin on UiProps {
+  late String requiredInSubclass1;
+  late String requiredInSubclass2;
+}
+@Props(ignoreRequiredProps: {'required1', 'requiredInSubclass1'})
+class IgnoresSomeRequiredProps = UiProps with WithLateRequiredProps, IgnoresSomeRequiredPropsMixin;
+
 
 UiFactory<WithAnnotationRequiredProps> WithAnnotationRequired = uiFunction((_) {}, _$WithAnnotationRequiredConfig);
 mixin WithAnnotationRequiredProps on UiProps {
-  @requiredProp
-  String? required1;
-  @requiredProp
-  String? required2;
+  @requiredProp String? required1;
+  @requiredProp String? required2;
   String? optional1;
   String? optional2;
 }
@@ -55,6 +71,10 @@ main() {
   InheritsLateRequired()();
 
   WithAnnotationRequired()();
+
+  DisableValidation()();
+
+  IgnoresSomeRequired()();
 
   // Make sure prefixed props aren't mistaken for the missing required prop.
   (RequiredWithSameNameAsPrefixed()..dom.hidden = true)();
