1a9c299e634253f7a5d645dec9688c788ee73db6
Greg Littlefield <greg.littlefield@workiva.com>
2024-02-02 12:42:15 -0700

    Update doc comment

--- lib/src/component_declaration/annotations.dart
+++ lib/src/component_declaration/annotations.dart
@@ -83,10 +83,36 @@ class Props implements TypedMap {
   ///```
   final Set<String>? disableRequiredPropValidation;
 
-  /// Whether to disable validation for all props defaulted in the class component associated
-  /// with this props class.
+  /// Whether to disable validation for props defaulted in the class component associated
+  /// with these props (defaults to `true`).
   ///
-  /// FIXME elaborate, add example
+  /// This allows the declaration of non-nullable props with defaults that aren't considered
+  /// required by runtime validation or the analyzer plugin.
+  ///
+  /// This only includes props defaulted directly in `defaultProps` or `getDefaultProps`,
+  /// and does not include any props added in other ways, such as super-calls or `addProps`/`addAll`.
+  ///
+  /// For example, given the following component:
+  /// ```dart
+  /// UiFactory<FooProps> Foo = castUiFactory(_$Foo);
+  ///
+  /// mixin FooProps on UiProps {
+  ///   late bool defaultedProp;
+  ///   late String requiredProp;
+  /// }
+  ///
+  /// class FooComponent extends UiComponent2<FooProps> {
+  ///   get defaultProps => (newProps()..defaultedProp = true);
+  ///
+  ///   render() {}
+  /// }
+  /// ```
+  ///
+  /// The prop `defaultedProp` is not considered required because it has a default:
+  /// ```dart
+  /// Foo()(); // Has static and runtime errors about missing `requiredProp`
+  /// (Foo()..requiredProp = true)(); // Has no errors.
+  /// ```
   final bool disableValidationForClassDefaultProps;
 
   const Props({
