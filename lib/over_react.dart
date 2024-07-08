5d5d2453a34ab636e17adf562ff056e51f08beb0
Sydney Jodon <sydney.jodon@workiva.com>
2024-07-08 13:26:43 -0700

    Remove entrypoint changes

--- lib/over_react.dart
+++ lib/over_react.dart
@@ -103,7 +103,6 @@ export 'src/util/key_constants.dart';
 export 'src/util/map_util.dart';
 export 'src/util/memo.dart';
 export 'src/util/pretty_print.dart';
-export 'src/util/prop_conversion.dart' show unjsifyMapProp, jsifyMapProp, unjsifyRefProp, jsifyRefProp;
 export 'src/util/prop_errors.dart';
 export 'src/util/prop_key_util.dart';
 export 'src/util/react_util.dart';
