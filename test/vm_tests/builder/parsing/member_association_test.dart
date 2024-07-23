120faa44fbda2c8745211f17dd9aa8921d460e9a
Aaron Lademann <aaron.lademann@workiva.com>
2024-07-23 08:18:30 -0700

    Add missing TestOn annotation

--- test/vm_tests/builder/parsing/member_association_test.dart
+++ test/vm_tests/builder/parsing/member_association_test.dart
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+@TestOn('vm')
+
 import 'package:over_react/src/builder/parsing.dart';
 import 'package:test/test.dart';
 
