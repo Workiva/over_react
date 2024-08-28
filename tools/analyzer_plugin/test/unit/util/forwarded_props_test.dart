bf0130a707efe4e3482bcaef87030297376e089c
Greg Littlefield <greg.littlefield@workiva.com>
2024-08-27 17:13:49 -0700

    Clean up test cases

--- tools/analyzer_plugin/test/unit/util/forwarded_props_test.dart
+++ tools/analyzer_plugin/test/unit/util/forwarded_props_test.dart
@@ -13,10 +13,8 @@
 // limitations under the License.
 
 import 'package:analyzer/dart/analysis/results.dart';
-import 'package:analyzer/dart/ast/ast.dart';
 import 'package:analyzer/dart/element/element.dart';
 import 'package:over_react_analyzer_plugin/src/diagnostic_contributor.dart';
-import 'package:over_react_analyzer_plugin/src/over_react_builder_parsing.dart';
 import 'package:over_react_analyzer_plugin/src/util/forwarded_props.dart';
 import 'package:test/test.dart';
 
@@ -30,137 +28,124 @@ void main() {
     setUpAll(sharedContext.warmUpAnalysis);
 
     group('correctly computes forwarded props for various cases:', () {
-      late ResolvedUnitResult result;
+      const sharedSourcePrefix = /*language=dart*/ r'''
+          // @dart=2.12
+          import 'package:over_react/over_react.dart';
+          
+          part '{{PART_PATH}}';
+          
+          mixin FooProps on UiProps {}
+          mixin BarProps on UiProps {}
+          mixin QuxProps on UiProps {}
+          
+          mixin UnrelatedProps on UiProps {}
+          
+          class TestProps = UiProps with FooProps, BarProps, QuxProps;
+          
+          UiFactory<TestProps> Test = castUiFactory(_$Test);
+      ''';
 
-      setUpAll(() async {
-        result = await resolveFileAndGeneratedPart(sharedContext, /*language=Dart*/ r'''
-// @dart=2.12
-import 'package:over_react/over_react.dart';
+      Future<ResolvedUnitResult> sharedResolveSource(String source) =>
+          resolveFileAndGeneratedPart(sharedContext, '$sharedSourcePrefix\n$source');
 
-part '{{PART_PATH}}';
+      test('..addProps(props)', () async {
+        final result = await sharedResolveSource(/*language=dart*/ r'''
+            UiFactory<TestProps> CaseRawAddProps = uiFunction((props) {
+              return (Test()
+                ..addProps(props)
+              )();
+            }, null);
+        ''');
+        final usage = getAllComponentUsages(result.unit).single;
 
-mixin FooProps on UiProps {}
-mixin BarProps on UiProps {}
-mixin QuxProps on UiProps {}
-
-mixin UnrelatedProps on UiProps {}
-
-class TestProps = UiProps with FooProps, BarProps, QuxProps;
-
-UiFactory<TestProps> Test = castUiFactory(_$Test);
-
-UiFactory<TestProps> TestRawAddProps = uiFunction((props) {
-  return (Test()
-    ..addProps(props)
-  )();
-}, null); 
-
-UiFactory<TestProps> TestRawAddAll = uiFunction((props) {
-  return (Test()
-    ..addAll(props)
-  )();
-}, null); 
-
-UiFactory<TestProps> TestGetPropsToForward = uiFunction((props) {
-  final consumedProps = props.staticMeta.forMixins({FooProps});
-  return (Test()
-    ..addProps(props.getPropsToForward(exclude: {FooProps}))
-  )();
-}, null);
-
-mixin TestGetPropsToForwardNoArgProps on UiProps {}
-UiFactory<TestGetPropsToForwardNoArgProps> TestGetPropsToForwardNoArg = uiFunction((props) {
-  return (Test()
-    ..addProps(props.getPropsToForward())
-  )();
-}, _$TestGetPropsToForwardNoArgConfig); 
-
-UiFactory<TestProps> TestConsumedPropsVariable = uiFunction((props) {
-  final consumedProps = props.staticMeta.forMixins({FooProps});
-  return (Test()
-    ..addUnconsumedProps(props, consumedProps)
-  )();
-}, null);
-
-''');
+        final f = getForwardedProps(usage)!;
+        expect(f.propsClassBeingForwarded, result.lookUpInterface('TestProps'));
+        expect(f.definitelyForwardsPropsFrom(result.lookUpInterface('FooProps')), isTrue);
+        expect(f.definitelyForwardsPropsFrom(result.lookUpInterface('BarProps')), isTrue);
+        expect(f.definitelyForwardsPropsFrom(result.lookUpInterface('QuxProps')), isTrue);
+        expect(f.definitelyForwardsPropsFrom(result.lookUpInterface('UnrelatedProps')), isFalse);
       });
 
-      FluentComponentUsage getSingleUsageWithinComponent(String componentName) {
-        final component = result.unit.declarations.singleWhere((e) =>
-            e is NamedCompilationUnitMember && e.name.lexeme == componentName ||
-            e is TopLevelVariableDeclaration && e.firstVariable.name.lexeme == componentName);
-        return getAllComponentUsages(component).single;
-      }
+      test('..addAll(props)', () async {
+        final result = await sharedResolveSource(/*language=dart*/ r'''
+            UiFactory<TestProps> _ = uiFunction<TestProps>((props) {
+              return (Test()
+                ..addAll(props)
+              )();
+            }, null);
+        ''');
+        final usage = getAllComponentUsages(result.unit).single;
 
-      late InterfaceElement testPropsElement;
-      late InterfaceElement fooPropsElement;
-      late InterfaceElement barPropsElement;
-      late InterfaceElement quxPropsElement;
-      late InterfaceElement unrelatedPropsElement;
-
-      setUp(() {
-        testPropsElement = getInterfaceElement(result, 'TestProps');
-        fooPropsElement = getInterfaceElement(result, 'FooProps');
-        barPropsElement = getInterfaceElement(result, 'BarProps');
-        quxPropsElement = getInterfaceElement(result, 'QuxProps');
-        unrelatedPropsElement = getInterfaceElement(result, 'UnrelatedProps');
+        final f = getForwardedProps(usage)!;
+        expect(f.propsClassBeingForwarded, result.lookUpInterface('TestProps'));
+        expect(f.definitelyForwardsPropsFrom(result.lookUpInterface('FooProps')), isTrue);
+        expect(f.definitelyForwardsPropsFrom(result.lookUpInterface('BarProps')), isTrue);
+        expect(f.definitelyForwardsPropsFrom(result.lookUpInterface('QuxProps')), isTrue);
+        expect(f.definitelyForwardsPropsFrom(result.lookUpInterface('UnrelatedProps')), isFalse);
       });
 
-      test('..addProps(props)', () {
-        final usage = getSingleUsageWithinComponent('TestRawAddProps');
-        final forwardedProps = getForwardedProps(usage);
-        expect(forwardedProps, isNotNull);
-        expect(forwardedProps!.propsClassBeingForwarded, testPropsElement);
-        expect(forwardedProps.definitelyForwardsPropsFrom(fooPropsElement), isTrue);
-        expect(forwardedProps.definitelyForwardsPropsFrom(barPropsElement), isTrue);
-        expect(forwardedProps.definitelyForwardsPropsFrom(quxPropsElement), isTrue);
-        expect(forwardedProps.definitelyForwardsPropsFrom(unrelatedPropsElement), isFalse);
+      test('..addUnconsumedProps(props, consumedPropsVariable)', () async {
+        final result = await sharedResolveSource(/*language=dart*/ r'''
+            UiFactory<TestProps> _ = uiFunction<TestProps>((props) {
+              final consumedProps = props.staticMeta.forMixins({FooProps});
+              return (Test()
+                ..addUnconsumedProps(props, consumedProps)
+              )();
+            }, null);
+        ''');
+        final usage = getAllComponentUsages(result.unit).single;
+
+        final f = getForwardedProps(usage)!;
+        expect(f.propsClassBeingForwarded, result.lookUpInterface('TestProps'));
+        expect(f.definitelyForwardsPropsFrom(result.lookUpInterface('FooProps')), isFalse);
+        expect(f.definitelyForwardsPropsFrom(result.lookUpInterface('BarProps')), isTrue);
+        expect(f.definitelyForwardsPropsFrom(result.lookUpInterface('QuxProps')), isTrue);
+        expect(f.definitelyForwardsPropsFrom(result.lookUpInterface('UnrelatedProps')), isFalse);
       });
 
-      test('..addAll(props)', () {
-        final usage = getSingleUsageWithinComponent('TestRawAddAll');
-        final forwardedProps = getForwardedProps(usage);
-        expect(forwardedProps, isNotNull);
-        expect(forwardedProps!.propsClassBeingForwarded, testPropsElement);
-        expect(forwardedProps.definitelyForwardsPropsFrom(fooPropsElement), isTrue);
-        expect(forwardedProps.definitelyForwardsPropsFrom(barPropsElement), isTrue);
-        expect(forwardedProps.definitelyForwardsPropsFrom(quxPropsElement), isTrue);
-        expect(forwardedProps.definitelyForwardsPropsFrom(unrelatedPropsElement), isFalse);
+      test('..addProps(props.getPropsToForward(exclude: {...}))', () async {
+        final result = await sharedResolveSource(/*language=dart*/ r'''
+            UiFactory<TestProps> _ = uiFunction((props) {
+              final consumedProps = props.staticMeta.forMixins({FooProps});
+              return (Test()
+                ..addProps(props.getPropsToForward(exclude: {FooProps}))
+              )();
+            }, null);
+        ''');
+        final usage = getAllComponentUsages(result.unit).single;
+
+        final f = getForwardedProps(usage);
+        expect(f, isNotNull);
+        expect(f!.propsClassBeingForwarded, result.lookUpInterface('TestProps'));
+        expect(f.definitelyForwardsPropsFrom(result.lookUpInterface('FooProps')), isFalse);
+        expect(f.definitelyForwardsPropsFrom(result.lookUpInterface('BarProps')), isTrue);
+        expect(f.definitelyForwardsPropsFrom(result.lookUpInterface('QuxProps')), isTrue);
+        expect(f.definitelyForwardsPropsFrom(result.lookUpInterface('UnrelatedProps')), isFalse);
       });
 
-      test('..addUnconsumedProps(props, consumedPropsVariable)', () {
-        final usage = getSingleUsageWithinComponent('TestConsumedPropsVariable');
-        final forwardedProps = getForwardedProps(usage);
-        expect(forwardedProps, isNotNull);
-        expect(forwardedProps!.propsClassBeingForwarded, testPropsElement);
-        expect(forwardedProps.definitelyForwardsPropsFrom(fooPropsElement), isFalse);
-        expect(forwardedProps.definitelyForwardsPropsFrom(barPropsElement), isTrue);
-        expect(forwardedProps.definitelyForwardsPropsFrom(quxPropsElement), isTrue);
-        expect(forwardedProps.definitelyForwardsPropsFrom(unrelatedPropsElement), isFalse);
-      });
+      test('..addProps(props.getPropsToForward()) - inferred from generic', () async {
+        final result = await sharedResolveSource(/*language=dart*/ r'''
+            mixin TestGetPropsToForwardNoArgProps on UiProps {}
+            UiFactory<TestGetPropsToForwardNoArgProps> TestGetPropsToForwardNoArg = uiFunction((props) {
+              return (Test()
+                ..addProps(props.getPropsToForward())
+              )();
+            }, _$TestGetPropsToForwardNoArgConfig); 
+        ''');
+        final usage = getAllComponentUsages(result.unit).single;
 
-      test('..addProps(props.getPropsToForward(exclude: {...}))', () {
-        final usage = getSingleUsageWithinComponent('TestGetPropsToForward');
-        final forwardedProps = getForwardedProps(usage);
-        expect(forwardedProps, isNotNull);
-        expect(forwardedProps!.propsClassBeingForwarded, testPropsElement);
-        expect(forwardedProps.definitelyForwardsPropsFrom(fooPropsElement), isFalse);
-        expect(forwardedProps.definitelyForwardsPropsFrom(barPropsElement), isTrue);
-        expect(forwardedProps.definitelyForwardsPropsFrom(quxPropsElement), isTrue);
-        expect(forwardedProps.definitelyForwardsPropsFrom(unrelatedPropsElement), isFalse);
-      });
+        final propsElement = result.lookUpInterface('TestGetPropsToForwardNoArgProps');
 
-      test('..addProps(props.getPropsToForward()) - inferred from generic', () {
-        final usage = getSingleUsageWithinComponent('TestGetPropsToForwardNoArg');
-        final forwardedProps = getForwardedProps(usage);
-        expect(forwardedProps, isNotNull);
-
-        final propsElement = getInterfaceElement(result, 'TestGetPropsToForwardNoArgProps');
-
-        expect(forwardedProps!.propsClassBeingForwarded, propsElement);
-        expect(forwardedProps.definitelyForwardsPropsFrom(propsElement), isFalse);
-        expect(forwardedProps.definitelyForwardsPropsFrom(unrelatedPropsElement), isFalse);
+        final f = getForwardedProps(usage);
+        expect(f!.propsClassBeingForwarded, propsElement);
+        expect(f.definitelyForwardsPropsFrom(propsElement), isFalse);
+        expect(f.definitelyForwardsPropsFrom(result.lookUpInterface('UnrelatedProps')), isFalse);
       });
     });
   });
+}
+
+extension on ResolvedUnitResult {
+  /// Extension form of [getInterfaceElement] to help make tests more concise.
+  InterfaceElement lookUpInterface(String name) => getInterfaceElement(this, name);
 }
