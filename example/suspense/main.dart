d3b00c7056ca73a8b42ea003f1e4e0f452250b3e
Keal Jones <keal.jones@workiva.com>
2024-09-16 14:22:45 -0700

    cleanup suspense example

--- example/suspense/main.dart
+++ example/suspense/main.dart
@@ -14,59 +14,12 @@
 
 import 'dart:html';
 
-import 'package:js/js.dart';
-import 'package:over_react/js_component.dart';
 import 'package:over_react/over_react.dart';
-import 'package:react/react_client/component_factory.dart';
-import 'package:react/react_client/react_interop.dart' hide lazy;
 import 'package:react/react_dom.dart' as react_dom;
 import 'counter_component.dart' deferred as lazy_component;
 import 'third_party_file.dart';
 
 part 'main.over_react.g.dart';
-
-@Props(keyNamespace: '')
-mixin TestJsProps on UiProps {
-  @Accessor(key: 'buttonProps')
-  JsMap? _$raw$buttonProps;
-
-  Map? get buttonProps => unjsifyMapProp(_$raw$buttonProps);
-  set buttonProps(Map? value) => _$raw$buttonProps = jsifyMapProp(value);
-
-  @Accessor(key: 'listOfProps')
-  List<dynamic>? _$raw$listOfProps;
-
-  List<Map?>? get listOfProps => unjsifyMapListProp(_$raw$listOfProps);
-  set listOfProps(List<Map?>? value) => _$raw$listOfProps = jsifyMapListProp(value);
-
-  @Accessor(key: 'inputRef')
-  dynamic _$raw$inputRef;
-
-  dynamic get inputRef => unjsifyRefProp(_$raw$inputRef);
-  set inputRef(dynamic value) => _$raw$inputRef = jsifyRefProp(value);
-
-  @Accessor(key: 'messageContext')
-  ReactContext? _$raw$messageContext;
-
-  Context<String?>? get messageContext => unjsifyContextProp(_$raw$messageContext);
-  set messageContext(Context<String?>? value) => _$raw$messageContext = jsifyContextProp(value);
-
-  dynamic /*ElementType*/ component;
-  dynamic /*ElementType*/ inputComponent;
-  dynamic /*ElementType*/ buttonComponent;
-}
-
-@JS('TestJsComponent')
-external ReactClass get _TestJs;
-
-UiFactory<TestJsProps> TestJs = uiJsComponent(
-  ReactJsComponentFactoryProxy(_TestJs),
-  _$TestJsConfig, // ignore: undefined_identifier
-);
-
-UiFactory<TestJsProps> LazyTestJs = lazy(() async => TestJs,
-_$TestJsConfig, // ignore: undefined_identifier
-);
 
 UiFactory<CounterPropsMixin> LazyCounter = lazy(() async {
   await Future.delayed(Duration(seconds: 5));
@@ -86,7 +39,7 @@ void main() {
             'I am a fallback UI that will show while we load the lazy component! The load time is artificially inflated to last an additional 5 seconds just to prove it\'s working!',
           )
         )(
-          (LazyTestJs())(
+          (LazyCounter())(
             (Dom.div()..id = 'Heyyy!')(),
           ),
         ),
