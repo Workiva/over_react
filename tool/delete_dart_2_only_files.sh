9d1cdd2b34a1b14d5b512efe96975f837f57e110
Greg Littlefield <greg.littlefield@workiva.com>
2024-10-15 18:38:58 -0700

    Fix Dart 3 CI trying to run unsound test file

--- tool/delete_dart_2_only_files.sh
+++ tool/delete_dart_2_only_files.sh
@@ -12,6 +12,14 @@ rm test/over_react/component_declaration/flux_component_test/component2/unsound_
 rm test/over_react/component_declaration/flux_component_test/unsound_flux_component_test.dart
 rm test/over_react_component_declaration_non_null_safe_test.dart
 
+# Remove the reference to the test file. Use a temporary file since we can't
+# redirect to/from the same file in the same command. See https://github.com/koalaman/shellcheck/wiki/SC2094
+mv dart_test.yaml dart_test.yaml.old
+grep over_react_component_declaration_non_null_safe_test.dart \
+   --fixed-strings \
+   --invert-match \
+   dart_test.yaml.old > dart_test.yaml
+
 rm -rf tools/analyzer_plugin/test/unit/util/non_null_safe
 rm -rf tools/analyzer_plugin/test/integration/assists/non_null_safe
 rm -rf tools/analyzer_plugin/test/integration/diagnostics/non_null_safe
