4b175320ae08cfe4c09d45dc31d9f5073a8e2fdf
Greg Littlefield <greg.littlefield@workiva.com>
2025-06-27 14:17:02 -0700

    Conditionally apply patch when using build_test 3
    Since Dart 3 can still resolve to 2.x when using older
    analyzer versions

--- tool/update_tests_for_dart_3.sh
+++ tool/update_tests_for_dart_3.sh
@@ -7,12 +7,13 @@
 
 set -e
 
-# Update over_react_builder_test.dart by applying a patch; see comment
-# in test file for more details on the changes.
-#
-# This patch is derived from fixing tests in Dart 3 and getting the diff with:
-#    git diff test/vm_tests/builder/over_react_builder_test.dart | pbcopy
-git apply << EndOfMessage
+# Update over_react_builder_test.dart by applying a patch in build_test_version v3 (Dart 3 only);
+# see comment in test file for more details on the changes.
+build_test_version=$(yq '.packages.build_test.version' pubspec.lock)
+if [[ "$build_test_version" == "3."* ]]; then
+  # This patch is derived from fixing tests  and getting the diff with:
+  #    git diff test/vm_tests/builder/over_react_builder_test.dart | pbcopy
+  git apply << EndOfMessage
 diff --git a/test/vm_tests/builder/over_react_builder_test.dart b/test/vm_tests/builder/over_react_builder_test.dart
 index 48c96b62..749db6e9 100644
 --- a/test/vm_tests/builder/over_react_builder_test.dart
@@ -39,3 +40,4 @@ index 48c96b62..749db6e9 100644
 
        logs = [];
 EndOfMessage
+fi
