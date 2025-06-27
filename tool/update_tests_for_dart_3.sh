49fc134d7a0a01f4f0d50e1eea6171b03757b0b4
Greg Littlefield <greg.littlefield@workiva.com>
2025-06-27 14:33:12 -0700

    Fix tests in build_test 3

--- tool/update_tests_for_dart_3.sh
+++ tool/update_tests_for_dart_3.sh
@@ -15,14 +15,25 @@ if [[ "$build_test_version" == "3."* ]]; then
   #    git diff test/vm_tests/builder/over_react_builder_test.dart | pbcopy
   git apply << EndOfMessage
 diff --git a/test/vm_tests/builder/over_react_builder_test.dart b/test/vm_tests/builder/over_react_builder_test.dart
-index 48c96b62..749db6e9 100644
+index 48c96b62..b9b60b55 100644
 --- a/test/vm_tests/builder/over_react_builder_test.dart
 +++ b/test/vm_tests/builder/over_react_builder_test.dart
-@@ -41,17 +41,17 @@ main() {
+@@ -20,6 +20,8 @@ import 'package:build/build.dart';
+ import 'package:build_resolvers/build_resolvers.dart';
+ import 'package:build_test/build_test.dart';
+ import 'package:collection/collection.dart' show IterableExtension;
++import 'package:glob/glob.dart';
++import 'package:glob/list_local_fs.dart';
+ import 'package:logging/logging.dart';
+ import 'package:over_react/src/builder/builder.dart';
+ import 'package:path/path.dart' as p;
+@@ -40,18 +42,27 @@ main() {
+     //     test 2.x and 3.x at the same time, and Dart 2 not being compatible with
      //     build_test 3.x.
 
-     late AssetReader reader;
+-    late AssetReader reader;
 -    late InMemoryAssetWriter writer; // [1]
++    late TestReaderWriter reader;
 +    late TestReaderWriter writer; // [1]
      late AssetWriterSpy writerSpy;
      late List<LogRecord> logs;
@@ -35,6 +46,15 @@ index 48c96b62..749db6e9 100644
        );
 
 -      writer = InMemoryAssetWriter(); // [1]
++      // Load test fixture assets from the filesystem into the test reader.
++      for (final file in Glob('test_fixtures/**').listSync().whereType<File>()) {
++        // Write via `testing` so it's not tracked as a builder output.
++        reader.testing.writeBytes(
++          makeAssetId('over_react|${file.path}'),
++          file.readAsBytesSync(),
++        );
++      }
++
 +      writer = TestReaderWriter(); // [1]
        writerSpy = AssetWriterSpy(writer);
 
