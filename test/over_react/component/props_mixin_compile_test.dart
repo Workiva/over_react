c2428334e1ae45db24df781281cce53faf3e80ca
Greg Littlefield <greg.littlefield@workiva.com>
2026-09-21 11:46:16 -0700

    Remove cases for props that are always mixed in

--- test/over_react/component/props_mixin_compile_test.dart
+++ test/over_react/component/props_mixin_compile_test.dart
@@ -11,11 +11,8 @@ void main() {
   group('props mixin compile test -', () {
     test('all non-generated prop mixins can be mixed into a generated props class', () {
       // Use all the components so they don't get tree-shaken out
-      expect(() => TestReact(), returnsNormally);
       expect(() => TestDom(), returnsNormally);
       expect(() => TestSvg(), returnsNormally);
-      expect(() => TestUbiquitousDom(), returnsNormally);
-      expect(() => TestCssClass(), returnsNormally);
       expect(() => TestAria(), returnsNormally);
       expect(() => TestConnectFlux(), returnsNormally);
     });
@@ -25,20 +22,11 @@ void main() {
 // Regression test: mixing these props mixins into generated props classes
 // ensures the $-companion classes compile correctly.
 
-class TestReactProps = UiProps with ReactPropsMixin;
-UiFactory<TestReactProps> TestReact = uiFunction((_) {}, _$TestReactConfig);
-
 class TestDomProps = UiProps with DomPropsMixin;
 UiFactory<TestDomProps> TestDom = uiFunction((_) {}, _$TestDomConfig);
 
 class TestSvgProps = UiProps with SvgPropsMixin;
 UiFactory<TestSvgProps> TestSvg = uiFunction((_) {}, _$TestSvgConfig);
-
-class TestUbiquitousDomProps = UiProps with UbiquitousDomPropsMixin;
-UiFactory<TestUbiquitousDomProps> TestUbiquitousDom = uiFunction((_) {}, _$TestUbiquitousDomConfig);
-
-class TestCssClassProps = UiProps with CssClassPropsMixin;
-UiFactory<TestCssClassProps> TestCssClass = uiFunction((_) {}, _$TestCssClassConfig);
 
 class TestAriaProps = UiProps with AriaPropsMixin;
 UiFactory<TestAriaProps> TestAria = uiFunction((_) {}, _$TestAriaConfig);
